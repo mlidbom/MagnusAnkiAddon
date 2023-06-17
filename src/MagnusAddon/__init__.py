fe81d62b9137496bd4d2a45b364327ab8cd28ea8
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-06-17 11:48:01 +0200

    Embry for integrating with wanikani.

--- src/MagnusAddon/__init__.py
+++ src/MagnusAddon/__init__.py
@@ -1,1 +1,1 @@
-from . import browser_search, wanikani, context_search, wani, refresh_media_references
+from . import browser_search, wanikani, context_search, wani, refresh_media_references, wanikani_api_client_test
---
+++ src/MagnusAddon/wanikani_api_client_test.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from wanikani_api.client import Client, models
+
+v2_api_key = "ebeda84c-2f6a-423e-bfc7-3068796ed50a"
+client = Client(v2_api_key)
+
+_all_vocabulary: List[models.Vocabulary] = None
+_all_radicals: List[models.Radical] = None
+_all_kanji: List[models.Kanji] = None
+
+
+def fetch_radicals() -> List[models.Radical]:
+    global _all_radicals
+    if _all_radicals is None:
+        _all_radicals = list(client.subjects(types="radical", fetch_all=True))
+    return _all_radicals
+
+
+def fetch_kanji() -> List[models.Kanji]:
+    global _all_kanji
+    if _all_kanji is None:
+        _all_kanji = list(client.subjects(types="kanji", fetch_all=True))
+    return _all_kanji
+
+
+def fetch_vocabulary() -> List[models.Vocabulary]:
+    global _all_vocabulary
+    if _all_vocabulary is None:
+        _all_vocabulary = list(client.subjects(types="vocabulary", fetch_all=True))
+    return _all_vocabulary
