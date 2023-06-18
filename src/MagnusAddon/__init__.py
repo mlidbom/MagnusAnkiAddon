8537605e727d4ccf5453b408a882dbe685a87fc6
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-06-18 15:29:10 +0200

    Ugly include of wanikani api as source.

--- src/MagnusAddon/__init__.py
+++ src/MagnusAddon/__init__.py
@@ -1,2 +1,7 @@
-from .magnus import wanikani, test_edit_note
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from magnus import wanikani, test_edit_note
+from wanikani_api import client, constants, models
 from . import browser_search,  context_search, refresh_media_references
