90a761b22b2ee4a66f6477b7daf6527a3fc8d0b1
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-06-18 11:32:44 +0200

    Minor optimization.

--- src/MagnusAddon/magnus/test_edit_note.py
+++ src/MagnusAddon/magnus/test_edit_note.py
@@ -3,16 +3,18 @@ from aqt import gui_hooks
 
 
 from .wanikani_note import *
+from .wanikani_api_client_test import WanikaniClient
 
+waniClient = WanikaniClient()
 
-def edit_note(kanji_note: WaniKanjiNote):
-    old_value = kanji_note.get_kanji_meaning()
-    new_value = old_value + " edited"
-    kanji_note.set_kanji_meaning(new_value)
+def get_wani_vocab(vocab_note: WaniVocabNote):
+    remote_vocab = waniClient.get_vocab(vocab_note.get_vocab())
+    remote_meaning = remote_vocab.meaning_mnemonic
+    # vocab_note.set_meaning_exp(remote_meaning)
 
 def setup_buttons(buttons, the_editor: aqt.editor.Editor):
-    btn = the_editor.addButton("", "Edit note",
-                               lambda local_editor: edit_note(WaniKanjiNote(local_editor.note)))
+    btn = the_editor.addButton("", "Update from wanikani",
+                               lambda local_editor: get_wani_vocab(WaniVocabNote(local_editor.note)))
     buttons.append(btn)
 
 
--- src/MagnusAddon/magnus/wanikani_api_client_test.py
+++ src/MagnusAddon/magnus/wanikani_api_client_test.py
@@ -2,30 +2,46 @@ from typing import List, Optional
 
 from wanikani_api.client import Client, models
 
-v2_api_key = "ebeda84c-2f6a-423e-bfc7-3068796ed50a"
-client = Client(v2_api_key)
 
-_all_vocabulary: Optional[List[models.Vocabulary]] = None
-_all_radicals: Optional[List[models.Radical]] = None
-_all_kanji: Optional[List[models.Kanji]] = None
+class WanikaniClient:
+    def __init__(self):
+        self.isInitialized = False
 
+    def _init(self):
+        if self.isInitialized is False:
+            self.v2_api_key = "ebeda84c-2f6a-423e-bfc7-3068796ed50a"
+            client = Client(self.v2_api_key)
 
-def fetch_radicals() -> List[models.Radical]:
-    global _all_radicals
-    if _all_radicals is None:
-        _all_radicals = list(client.subjects(types="radical", fetch_all=True))
-    return _all_radicals
+            self._radical_list: List[models.Radical] = list(client.subjects(types="radical", fetch_all=True))
+            self._kanji_list: List[models.Kanji] = list(client.subjects(types="kanji", fetch_all=True))
+            self._vocab_list: List[models.Vocabulary] = list(client.subjects(types="vocabulary", fetch_all=True))
 
+            self._radical_dictionary = {radical.characters: radical for radical in self._radical_list}
+            self._kanji_dictionary = {kanji.characters: kanji for kanji in self._kanji_list}
+            self._vocab_dictionary = {vocab.characters: vocab for vocab in self._vocab_list}
+            self.isInitialized = True
+            return self
 
-def fetch_kanji() -> List[models.Kanji]:
-    global _all_kanji
-    if _all_kanji is None:
-        _all_kanji = list(client.subjects(types="kanji", fetch_all=True))
-    return _all_kanji
+    def list_radicals(self) -> List[models.Radical]:
+        self._init()
+        return self._radical_list
 
+    def list_kanji(self) -> List[models.Kanji]:
+        self._init()
+        return self._kanji_list
 
-def fetch_vocabulary() -> List[models.Vocabulary]:
-    global _all_vocabulary
-    if _all_vocabulary is None:
-        _all_vocabulary = list(client.subjects(types="vocabulary", fetch_all=True))
-    return _all_vocabulary
+    def list_vocabulary(self) -> List[models.Vocabulary]:
+        self._init()
+        return self._vocab_list
+
+    def get_radical(self, radical_name: str) -> models.Radical:
+        self._init()
+        return self._radical_dictionary[radical_name]
+
+    def get_kanji(self, kanji_name: str) -> models.Kanji:
+        self._init()
+        return self._kanji_dictionary[kanji_name]
+
+    def get_vocab(self, vocab_name: str) -> models.Vocabulary:
+        self._init()
+        return self._vocab_dictionary[vocab_name]
