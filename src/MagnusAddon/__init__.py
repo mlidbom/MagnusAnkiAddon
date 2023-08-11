aed13c82d0cce9c0245f1413f596b9e978bc1ad5
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-11 21:55:38 +0200

    Environment improvement.

--- src/MagnusAddon/__init__.py
+++ src/MagnusAddon/__init__.py
@@ -2,9 +2,9 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from hooks import  wani_menu_manager, hooks_suppress_audio_on_typing, collection_search
+from hooks import  wani_menu_manager, hooks_suppress_audio_on_typing, right_click_menu
 from _addon_copies import refresh_media_references
 
 wani_menu_manager.setup()
 hooks_suppress_audio_on_typing.setup()
-collection_search.init()
+right_click_menu.init()
