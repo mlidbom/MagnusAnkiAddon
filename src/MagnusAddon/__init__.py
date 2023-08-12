f11dea915959c36e8c98b7bbb1dfb9e3a17b70cd
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-12 21:46:20 +0200

    Fix crash that lovely python exposes only when not running in the debugger.

--- src/MagnusAddon/__init__.py
+++ src/MagnusAddon/__init__.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-from hooks import wani_menu_manager, hooks_suppress_audio_on_typing, right_click_menu
-from _addon_copies import refresh_media_references
-
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from hooks import wani_menu_manager, hooks_suppress_audio_on_typing, right_click_menu
+from _addon_copies import refresh_media_references # noqa
 
 wani_menu_manager.setup()
 hooks_suppress_audio_on_typing.setup()
