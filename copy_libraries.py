cde086bf74e324a71f70f901448e3e31190d60f4
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-12 23:11:54 +0200

    Let's make managing libraries a bit less painful.

---
+++ copy_libraries.py
@@ -0,0 +1,29 @@
+import shutil
+import os
+
+# Get the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define the source directory (venv) and target directory (project) relative to the script location
+source_dir = os.path.join(script_dir, './venv/lib/site-packages')
+target_dir = os.path.join(script_dir, './src/_lib')
+
+# List the libraries you want to copy
+libraries_to_copy = ['dateutil', 'wanikani_api']
+
+# Create the target directory if it doesn't exist
+os.makedirs(target_dir, exist_ok=True)
+
+# Copy the libraries
+for library in libraries_to_copy:
+    src_path = os.path.join(source_dir, library)
+    dst_path = os.path.join(target_dir, library)
+
+    # Remove the existing directory if it exists
+    if os.path.exists(dst_path):
+        shutil.rmtree(dst_path)
+
+    # Copy the directory
+    shutil.copytree(src_path, dst_path)
+
+print("Libraries copied successfully!")
