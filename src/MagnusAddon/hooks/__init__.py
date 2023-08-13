605922864c36ba100e12709c44e6808b497366d5
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-13 17:57:35 +0200

    Checkpoint.

--- src/MagnusAddon/hooks/__init__.py
+++ src/MagnusAddon/hooks/__init__.py
@@ -1,2 +1,11 @@
-from . import browser_right_click_menu, copy_card_sort_field_to_clipboard, editor_buttons, right_click_menu, \
-    right_click_menu_utils, right_click_menu_search, right_click_menu_note, show_previewer, suppress_audio_on_typing, tools_menu
+from . import \
+    browser_right_click_menu, \
+    copy_card_sort_field_to_clipboard, \
+    editor_buttons, \
+    right_click_menu, \
+    right_click_menu_utils,\
+    right_click_menu_search,\
+    right_click_menu_note, \
+    show_previewer, \
+    suppress_audio_on_typing, \
+    tools_menu
