a06b08cf6c74cdeedadbc553a3caf2bc5345b047
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-17 12:52:29 +0200

    Various attempts to parse out words from text.

--- src/MagnusAddon/parsing/janomeutils.py
+++ src/MagnusAddon/parsing/janomeutils.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from janome.tokenizer import Tokenizer, Token
 
 from sysutils import kana_utils
@@ -11,6 +13,17 @@ class ParsedWord:
         self.parts_of_speech = parts_of_speech
 
     def is_kana_only(self) -> bool: return kana_utils.is_only_kana(self.word)
+
+    def __repr__(self) -> str:
+        return f"ParsedWord('{self.word}', '{self.parts_of_speech}')"
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, ParsedWord):
+            return self.word == other.word and self.parts_of_speech == other.parts_of_speech
+        return False
+
+    def __hash__(self) -> int:
+        return hash((self.word, self.parts_of_speech))
 
 _tokenizer = Tokenizer()
 
