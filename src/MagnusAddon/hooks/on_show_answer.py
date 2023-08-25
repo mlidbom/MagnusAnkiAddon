df54fa67b993e7300c92a9651d26cbe55e9f9617
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-08-25 12:53:18 +0200

    Fix some odd behavior.

--- src/MagnusAddon/hooks/on_show_answer.py
+++ src/MagnusAddon/hooks/on_show_answer.py
@@ -1,4 +1,5 @@
 from anki.cards import Card
+from anki.notes import Note
 from aqt import gui_hooks
 
 from ankiutils import search_utils
@@ -11,14 +12,13 @@ from sysutils.ui_utils import UIUtils
 recent_note_ids = RecentItems[int](2)
 
 def copy_previewer_sort_field_to_windows_clipboard(html:str, card: Card, type_of_display:str) -> str:
-    if type_of_display == 'previewAnswer':
-        copy_card_sort_field_to_clipboard(card)
+    if (type_of_display == 'previewAnswer'
+            and not UIUtils.is_edit_current_open()
+            and not recent_note_ids.is_recent(card.note().id)):
+        copy_card_sort_field_to_clipboard(card.note())
     return html
 
-def copy_card_sort_field_to_clipboard(card: Card) -> None:
-    note = card.note()
-    if UIUtils.is_edit_current_open() or recent_note_ids.is_recent(note.id): return
-
+def copy_card_sort_field_to_clipboard(note: Note) -> None:
     model = note.model()
     sort_field = model['sortf']
     sort_value = note.fields[sort_field]
@@ -26,10 +26,17 @@ def copy_card_sort_field_to_clipboard(card: Card) -> None:
     my_clipboard.set_text(clean_string)
 
 
-def show_dependencies_in_browser(card: Card) -> None:
-    search_utils.lookup_dependencies(MyNote.note_from_card(card))
+def show_dependencies_in_browser(note: Note) -> None:
+    search_utils.lookup_dependencies(MyNote.note_from_note(note))
+
+def on_reviewer_show_answer(card: Card) -> None:
+    note = card.note()
+    if UIUtils.is_edit_current_open() or recent_note_ids.is_recent(note.id):
+        return
+
+    show_dependencies_in_browser(note)
+    copy_card_sort_field_to_clipboard(note)
 
 def init() -> None:
-    gui_hooks.reviewer_did_show_answer.append(copy_card_sort_field_to_clipboard)
-    gui_hooks.reviewer_did_show_answer.append(show_dependencies_in_browser)
+    gui_hooks.reviewer_did_show_answer.append(on_reviewer_show_answer)
     gui_hooks.card_will_show.append(copy_previewer_sort_field_to_windows_clipboard)
