259c297b02ba3d0b0f3b686d36401b7a9f9436d8
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-02 17:57:10 +0200

    Disable suppression of audio for now.

--- src/MagnusAddon/hooks/suppress_audio_on_typing.py
+++ src/MagnusAddon/hooks/suppress_audio_on_typing.py
@@ -5,12 +5,13 @@ from hooks.timing_hacks import ugly_timing_hacks
 
 
 def will_play_tags(tags: list[AVTag], _something: str, _view: any):
-    if tags:
-        if isinstance(_view, BrowserPreviewer):
-            if ugly_timing_hacks.typed_in_editor_in_last_seconds(0.1):
-                tags.clear()
-            if ugly_timing_hacks.reviewer_just_showed_answer():
-                tags.clear()
+    something = 1
+    # if tags:
+    #     if isinstance(_view, BrowserPreviewer):
+    #         if ugly_timing_hacks.typed_in_editor_in_last_seconds(0.1):
+    #             tags.clear()
+    #         if ugly_timing_hacks.reviewer_just_showed_answer():
+    #             tags.clear()
 
 def init() -> None:
     gui_hooks.av_player_will_play_tags.append(will_play_tags)
