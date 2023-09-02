f6e77298e294ef8e570f816870c64e49aa6306d0
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-02 17:27:33 +0200

    Checkpoint

--- src/MagnusAddon/hooks/on_note_edited.py
+++ src/MagnusAddon/hooks/on_note_edited.py
@@ -9,6 +9,6 @@ def init() -> None:
     def handle_update(note: Note):
         # noinspection PyProtectedMember
         MyNote._on_note_edited(note)
-        UIUtils.refresh() #todo: this is a rather ugly hack. Surely anki should handle this itself if everything is working right?
+        #UIUtils.refresh() #todo: this is a rather ugly hack. Surely anki should handle this itself if everything is working right?
 
     gui_hooks.editor_did_fire_typing_timer.append(handle_update)
