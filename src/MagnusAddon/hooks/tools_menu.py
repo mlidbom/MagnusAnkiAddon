040d58253e12164e57680723cc35b633de28e351
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-02 17:56:47 +0200

    Fix "helpful" thing that pycharm did.

--- src/MagnusAddon/hooks/tools_menu.py
+++ src/MagnusAddon/hooks/tools_menu.py
@@ -11,7 +11,7 @@ from wanikani import wani_note_updater
 from wanikani.wani_downloader import WaniDownloader
 
 
-def add_menu_action(sub_menu: QMenu, heading: str, callback: Callable, shortcut: str):
+def add_menu_action(sub_menu: QMenu, heading: str, callback: Callable, shortcut: str = ""):
     action = QAction(heading, mw)
     if shortcut: action.setShortcut(shortcut)
     qconnect(action.triggered, callback)
@@ -20,7 +20,7 @@ def add_menu_action(sub_menu: QMenu, heading: str, callback: Callable, shortcut:
 def build_main_menu() -> None:
     my_menu = QMenu("Magnu&s", mw)
     tools_menu = mw.form.menuTools
-    add_menu_action(tools_menu, "Refresh UI", lambda: UIUtils.refresh(), "Ctrl+F5")
+    add_menu_action(tools_menu, "Refresh UI", lambda: UIUtils.refresh(), "F5")
 
     tools_menu.addMenu(my_menu)
 
@@ -33,24 +33,24 @@ def build_main_menu() -> None:
     build_wani_menu(wani_menu)
 
 def build_local_menu(sub_menu: QMenu) -> None:
-    add_menu_action(sub_menu, "Update &All", local_note_updater.update_all, "F5")
-    add_menu_action(sub_menu, "Update &Vocab", local_note_updater.update_vocab, "F5")
-    add_menu_action(sub_menu, "Update &Kanji", local_note_updater.update_kanji, "F5")
-    add_menu_action(sub_menu, "Update &Sentences", local_note_updater.update_sentences, "F5")
-    add_menu_action(sub_menu, "Set Vocab UK from dictionary", local_note_updater.set_vocab_uk_from_dictionary, "F5")
+    add_menu_action(sub_menu, "Update &All", local_note_updater.update_all)
+    add_menu_action(sub_menu, "Update &Vocab", local_note_updater.update_vocab)
+    add_menu_action(sub_menu, "Update &Kanji", local_note_updater.update_kanji)
+    add_menu_action(sub_menu, "Update &Sentences", local_note_updater.update_sentences)
+    add_menu_action(sub_menu, "Set Vocab UK from dictionary", local_note_updater.set_vocab_uk_from_dictionary)
 
 
 def build_wani_menu(sub_menu: QMenu) -> None:
-    add_menu_action(sub_menu, "Update Radicals", wani_note_updater.update_radical, "F5")
-    add_menu_action(sub_menu, "Update Kanji", wani_note_updater.update_kanji, "F5")
-    add_menu_action(sub_menu, "Update Vocabulary", wani_note_updater.update_vocab, "F5")
-    add_menu_action(sub_menu, "Import Missing Radicals", note_importer.import_missing_radicals, "F5")
-    add_menu_action(sub_menu, "Import Missing Kanji", note_importer.import_missing_kanji, "F5")
-    add_menu_action(sub_menu, "Import Missing Vocabulary", note_importer.import_missing_vocab, "F5")
-    add_menu_action(sub_menu, "Download Missing Vocabulary audio", WaniDownloader.fetch_missing_vocab_audio, "F5")
-    add_menu_action(sub_menu, "Delete Missing Radicals", wani_note_updater.delete_missing_radicals, "F5")
-    add_menu_action(sub_menu, "Delete Missing Kanji", wani_note_updater.delete_missing_kanji, "F5")
-    add_menu_action(sub_menu, "Delete Missing Vocabulary", wani_note_updater.delete_missing_vocab, "F5")
+    add_menu_action(sub_menu, "Update Radicals", wani_note_updater.update_radical)
+    add_menu_action(sub_menu, "Update Kanji", wani_note_updater.update_kanji)
+    add_menu_action(sub_menu, "Update Vocabulary", wani_note_updater.update_vocab)
+    add_menu_action(sub_menu, "Import Missing Radicals", note_importer.import_missing_radicals)
+    add_menu_action(sub_menu, "Import Missing Kanji", note_importer.import_missing_kanji)
+    add_menu_action(sub_menu, "Import Missing Vocabulary", note_importer.import_missing_vocab)
+    add_menu_action(sub_menu, "Download Missing Vocabulary audio", WaniDownloader.fetch_missing_vocab_audio)
+    add_menu_action(sub_menu, "Delete Missing Radicals", wani_note_updater.delete_missing_radicals)
+    add_menu_action(sub_menu, "Delete Missing Kanji", wani_note_updater.delete_missing_kanji)
+    add_menu_action(sub_menu, "Delete Missing Vocabulary", wani_note_updater.delete_missing_vocab)
 
 
 def init() -> None:
