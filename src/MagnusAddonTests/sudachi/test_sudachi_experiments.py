3d75a31a572ee14507e33d3171d4219d8bf0e687
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-20 09:36:04 +0200

    Play a bit more with sudachi.

--- src/MagnusAddonTests/sudachi/test_sudachi_experiments.py
+++ src/MagnusAddonTests/sudachi/test_sudachi_experiments.py
@@ -2,7 +2,7 @@
 # from sudachipy import Dictionary, SplitMode
 #
 # @pytest.mark.parametrize('text', [
-#     "声出したら駄目だからね",
+#     "いつまでも来ないと知らないからね",
 #     #"彼の日本語のレベルは私と同じ位だ"
 #     # "これをください",
 #     # "ハート形",
@@ -11,7 +11,11 @@
 # def test_sudachi(text:str) -> None:
 #     tokenizer = Dictionary(dict_type="full").create()
 #
-#     a = [mo.get_word_info() for mo in tokenizer.tokenize(text, SplitMode.A)]
-#     b = [mo.get_word_info() for mo in tokenizer.tokenize(text, SplitMode.B)]
-#     c = [mo.get_word_info() for mo in tokenizer.tokenize(text, SplitMode.C)]
+#     a = [mo for mo in (tokenizer.tokenize(text, SplitMode.A))]
+#     b = [mo for mo in tokenizer.tokenize(text, SplitMode.B)]
+#     c = [mo for mo in tokenizer.tokenize(text, SplitMode.C)]
+#
+#     a_info = [mo.get_word_info() for mo in a]
+#     b_info = [mo.get_word_info() for mo in b]
+#     c_info = [mo.get_word_info() for mo in c]
 #     something = 1
