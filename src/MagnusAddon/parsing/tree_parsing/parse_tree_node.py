be7a6ffd822ba555dc8e0a2dfa365653b6b28308
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-20 12:03:45 +0200

    Checkpoint.

--- src/MagnusAddon/parsing/tree_parsing/parse_tree_node.py
+++ src/MagnusAddon/parsing/tree_parsing/parse_tree_node.py
@@ -5,21 +5,23 @@ from parsing.tree_parsing import tree_parser
 
 class Node:
     _max_lookahead = 12
-    def __init__(self, tokens: list[TokenExt], children=None) -> None:
-        self.tokens = tokens
+    def __init__(self, surface: str, children=None) -> None:
+        self.surface = surface
         self.children = children if children else []
 
     def __repr__(self) -> str:
-        return f"""Node('{"".join([tok.surface for tok in self.tokens])}'{"," + str(self.children) if self.children else ""})"""
+        return f"""Node('{self.surface}'{"," + str(self.children) if self.children else ""})"""
 
     def __eq__(self, other: any) -> bool:
         return (isinstance(other, Node)
-                and self.tokens == other.tokens)
+                and self.surface == other.surface
+                and self.children == other.children)
 
     def __hash__(self) -> int:
-        return hash(self.tokens)
+        return hash(self.surface) + hash(self.children)
 
     @classmethod
     def create(cls, tokens: list[TokenExt]) -> 'Node':
         children = tree_parser.internal_parse(tokens) if len(tokens) > 1 else None
-        return Node(tokens, children)
+        surface = "".join(tok.surface for tok in tokens)
+        return Node(surface, children)
