c9697c4f9abe983b9c0c0620a19070c2ae9ec7ec
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-20 13:35:43 +0200

    Checkpoint.

--- src/MagnusAddon/parsing/tree_parsing/parse_tree_node.py
+++ src/MagnusAddon/parsing/tree_parsing/parse_tree_node.py
@@ -5,17 +5,17 @@ from parsing.tree_parsing import tree_parser
 
 class Node:
     _max_lookahead = 12
-    def __init__(self, surface: str, base: str, children=None) -> None:
-        self.surface = surface
+    def __init__(self, base: str, surface: str, children=None) -> None:
         self.base = base
+        self.surface = surface
         self.children = children if children else []
 
     def __repr__(self) -> str:
-        return f"""Node('{self.surface}','{self.base}'{"," + str(self.children) if self.children else ""})"""
+        return f"""Node('{self.base}','{self.surface}'{"," + str(self.children) if self.children else ""})"""
 
     def __eq__(self, other: any) -> bool:
         return (isinstance(other, Node)
-                and self.surface == other.surface
+                and self.base == other.base
                 and self.children == other.children)
 
     def __hash__(self) -> int:
@@ -26,5 +26,5 @@ class Node:
         children = tree_parser.internal_parse(tokens, excluded) if len(tokens) > 1 else None
         surface = "".join(tok.surface for tok in tokens)
         base = "".join(tok.surface for tok in tokens[:-1]) + tokens[-1].base_form
-        base = base if base != surface else ""
-        return Node(surface, base, children)
+        surface = surface if base != surface else ""
+        return Node(base, surface, children)
--- src/MagnusAddonTests/parsing/test_tree_parser.py
+++ src/MagnusAddonTests/parsing/test_tree_parser.py
@@ -5,20 +5,20 @@ from parsing.tree_parsing.parse_tree_node import Node
 
 
 @pytest.mark.parametrize('sentence, excluded, expected', [
-    ("知らない", set(), [Node('知らない','',[Node('知ら','知る'), Node('ない','')])]),
+    ("知らない", set(), [Node('知らない','',[Node('知る', '知ら'), Node('ない','')])]),
     ("いつまでも来ないと知らないからね", set(), [
         Node('いつまでも', '', [Node('いつまで', '', [Node('いつ', ''), Node('まで', '')]), Node('も', '')]),
-        Node('来', '来る'),
+        Node('来る', '来'),
         Node('ないと', '', [Node('ない', ''), Node('と', '')]),
-        Node('知らない', '', [Node('知ら', '知る'), Node('ない','')]),
+        Node('知らない', '', [Node('知る', '知ら'), Node('ない','')]),
         Node('から', ''),
         Node('ね', '')]),
     ("いつまでも来ないと知らないからね", {"ないと"}, [
         Node('いつまでも', '', [Node('いつまで', '', [Node('いつ', ''), Node('まで', '')]), Node('も', '')]),
-        Node('来', '来る'),
+        Node('来る', '来'),
         Node('ない', ''),
         Node('と', ''),
-        Node('知らない', '', [Node('知ら', '知る'), Node('ない', '')]),
+        Node('知らない', '', [Node('知る', '知ら'), Node('ない', '')]),
         Node('から', ''),
         Node('ね', '')]),
     ("ついに素晴らしい女性に逢えた。", set(), [
@@ -26,7 +26,7 @@ from parsing.tree_parsing.parse_tree_node import Node
         Node('素晴らしい',''),
         Node('女性',''),
         Node('に',''),
-        Node('逢え','逢える')]),
+        Node('逢える', '逢え')]),
     ("ついに素晴らしい女性に逢えた。", {"逢える"}, [
         Node('ついに', ''),
         Node('素晴らしい', ''),
