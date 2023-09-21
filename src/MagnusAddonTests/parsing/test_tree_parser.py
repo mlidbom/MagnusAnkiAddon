3aace369e8dba4d8a3c2a40e71a668a7a846fe6d
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-21 12:58:27 +0200

    Checkpoint.

--- src/MagnusAddonTests/parsing/test_tree_parser.py
+++ src/MagnusAddonTests/parsing/test_tree_parser.py
@@ -51,19 +51,20 @@ from parsing.tree_parsing.parse_tree_node import Node
         Node('来ないと', '', [Node('来る', '来'), Node('ないと', '', [Node('ない', ''), Node('と', '')])]),
         Node('知らない', '', [Node('知る', '知ら'), Node('ない', '')]),
         Node('から', ''),
-        Node('ね', '')])
-])
-def test_stuff(sentence: str, excluded:set[str], expected: list[Node]) -> None:
-    result = tree_parser.parse_tree(sentence, excluded)
-    assert result == expected
-
-@pytest.mark.parametrize('sentence, excluded, expected', [
+        Node('ね', '')]),
     ("ダメダメ私を殺して", {"殺し"}, [
         Node('ダメダメ',''),
         Node('私',''),
         Node('を',''),
         Node('殺して','',[Node('殺す','殺し'), Node('て','')])
     ])
+])
+def test_stuff(sentence: str, excluded:set[str], expected: list[Node]) -> None:
+    result = tree_parser.parse_tree(sentence, excluded)
+    assert result == expected
+
+@pytest.mark.parametrize('sentence, excluded, expected', [
+
 ])
 def test_temp(sentence: str, excluded:set[str], expected: list[Node]) -> None:
     result = tree_parser.parse_tree(sentence, excluded)
