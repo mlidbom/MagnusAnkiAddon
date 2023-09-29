4a4e24a3d70650266f993c2a9c609aa0f0daf015
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-29 12:06:44 +0200

    Try and get rid of clipboard exceptions once and for all.

--- src/MagnusAddon/sysutils/my_clipboard.py
+++ src/MagnusAddon/sysutils/my_clipboard.py
@@ -2,17 +2,27 @@ import win32clipboard
 
 
 def get_text() -> str:
-    win32clipboard.OpenClipboard()
-    clipboard_content = win32clipboard.GetClipboardData()
-    win32clipboard.CloseClipboard()
-    return clipboard_content
+    try:
+        win32clipboard.OpenClipboard()
+        try:
+            clipboard_content = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+            return clipboard_content
+        finally:
+            win32clipboard.CloseClipboard()
+    except: # noqa
+        pass  # Occasionally this code randomly fails. Let's not have that result in a crash of the addon OK?
+
+    return ""
 
 def set_text(text) -> None:
     try:
         win32clipboard.OpenClipboard()
-        win32clipboard.EmptyClipboard()
-        win32clipboard.SetClipboardText(text, win32clipboard.CF_UNICODETEXT)
-        win32clipboard.CloseClipboard()
-
+        try:
+            win32clipboard.EmptyClipboard()
+            win32clipboard.SetClipboardText(text, win32clipboard.CF_UNICODETEXT)
+            win32clipboard.CloseClipboard()
+        finally:
+            win32clipboard.CloseClipboard()
     except: # noqa
         pass  # Occasionally this code randomly fails. Let's not have that result in a crash of the addon OK?
