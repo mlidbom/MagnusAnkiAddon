272f3dd7bdb31e74785876d977bd06c1cdcc48c3
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-09-30 22:30:58 +0200

    Checkpoint.

--- src/MagnusAddon/parsing/unidic2ud/ud2ud_parsers.py
+++ src/MagnusAddon/parsing/unidic2ud/ud2ud_parsers.py
@@ -12,22 +12,27 @@ class UD2UDParser:
         return self._lazy_parser.instance()(text)
 
 
-gendai = UD2UDParser("gendai") # The leader so far, quite accurate
-spoken = UD2UDParser("spoken") # ??. Zero differences compared to gendai so far...
+gendai = UD2UDParser("gendai")  # The leader so far, quite accurate
+spoken = UD2UDParser("spoken")  # ??. Zero differences compared to gendai so far...
 
-#alternatives?
-kindai = UD2UDParser("kindai")  # Yes. 8 Differences. One clearly better and used in tests. 60+% of the times it differs from gendai, gendai is better.
-default = UD2UDParser("built-in")  # Maybe. 14 differences. 80-90% worse, but different in ways that may sometimes be what one wants...
-kinsei = UD2UDParser("kinsei") # Maybe. 6 differences. 1 better, 1 arguably better
+qkana = UD2UDParser("qkana")  # Maybe. 2 difference with gendai. One clearly better and used in tests(But would likely be caught by dictionary compounding that we want anyway.).
+kindai = UD2UDParser("kindai")  # Maybe. 8 Differences. One clearly better and used in tests(But would likely be caught by dictionary compounding that we want anyway.).
+default = UD2UDParser("built-in")  # Maybe. 14 differences. One clearly better and used in tests(But would likely be caught by dictionary compounding that we want anyway.).
+kinsei = UD2UDParser("kinsei")  # Maybe. 6 differences. One clearly better and used in tests(But would likely be caught by dictionary compounding that we want anyway.). 1 arguably better
 
-novel = UD2UDParser("novel") # NO. 1 difference with gendai and it was worse. Not worth the odds to try for the user.
-qkana = UD2UDParser("qkana") # NO. 1 difference with gendai and it was worse.
-
-
-
-kyogen = UD2UDParser("kyogen") # NO. 18 differences. Consistently strange. Did often pick up kanji 無い but that was the only upside.
-wakan = UD2UDParser("wakan") #NO. 27 differences. Consistently strange.
-wabun = UD2UDParser("wabun") #NO. 27 differences. Consistently strange.
-manyo = UD2UDParser("manyo") #NO. 25 differences. Consistently strange.
+# novel = UD2UDParser("novel") # NO. 2 difference with gendai. Both worse. Not worth the odds to try for the user.
+# kyogen = UD2UDParser("kyogen") # NO. 18 differences. Consistently strange. Did often pick up kanji 無い but that was the only upside.
+# wakan = UD2UDParser("wakan") #NO. 27 differences. Consistently strange.
+# wabun = UD2UDParser("wabun") #NO. 27 differences. Consistently strange.
+# manyo = UD2UDParser("manyo") #NO. 25 differences. Consistently strange.
 
 best = gendai
+
+all_parsers = [gendai,
+               spoken,
+               qkana,
+               kindai,
+               default,
+               kinsei,
+               # novel, kyogen, wakan, wabun, manyo
+               ]
--- src/MagnusAddonTests/test_parsing/test_u2ud_tree_parser.py
+++ src/MagnusAddonTests/test_parsing/test_u2ud_tree_parser.py
@@ -17,25 +17,32 @@ def only_string_params(param) -> str: return param if isinstance(param, str) els
     ("なかったかな", None, R(N('なかったかな', '', [N('なかっ', '無い'), N('た', ''), N('か', ''), N('な', '')]))),
     ("としたら", None,  R(N('としたら', 'とした', [N('と', ''), N('し', '為る'), N('たら', 'た')]))),
     ("離れていくよ", None,  R(N('離れていくよ', '', [N('離れ', '離れる'), N('て', ''), N('いく', '行く'), N('よ', '')]))),
-    ("ダメダメ私を助けて", ud2ud_parsers.kindai,  R(N('ダメダメ', ''),N('私を助けて', '', [N('私を', '', [N('私', ''), N('を', '')]), N('助けて', '', [N('助け', '助ける'), N('て', '')])]))),
+
+    # todo: ダメダメ
+    ("ダメダメ私を助けて", None,                  R(N('ダメ', '駄目'),    N('ダメ', '駄目'),       N('私を助けて', '',          [N('私を', '', [N('私', ''), N('を', '')]), N('助けて', '', [N('助け', '助ける'), N('て', '')])]))),
+    ("ダメダメ私を助けて", ud2ud_parsers.default, R(N('ダメダメ私を', '', [N('ダメダメ', 'ダメ駄目'), N('私を', '',                            [N('私', ''), N('を', '')])]),N('助けて', '', [N('助け', '助ける'), N('て', '')]))),
+    ("ダメダメ私を助けて", ud2ud_parsers.qkana,   R(N('ダメダメ私を', '', [N('ダメダメ',       ''),                             N('私を', '', [N('私', ''), N('を', '')])]),N('助けて', '', [N('助け', '助ける'), N('て', '')]))),
+    ("ダメダメ私を助けて", ud2ud_parsers.kindai,  R(N('ダメダメ', ''),                            N('私を助けて', '',          [N('私を', '', [N('私', ''), N('を', '')]),  N('助けて', '', [N('助け', '助ける'), N('て', '')])]))),
+    ("ダメダメ私を助けて", ud2ud_parsers.kinsei,  R(N('ダメダメ', ''),                            N('私を助けて', '',          [N('私を', '', [N('私', ''), N('を', '')]),  N('助けて', '', [N('助け', '助ける'), N('て', '')])]))),
+
     ("夢を見た", None, R(N('夢を', '', [N('夢', ''), N('を', '')]),N('見た', '', [N('見', '見る'), N('た', '')]))),
     ("言われるまで気づかなかった", None, R(N('言われるまで', '', [N('言わ', '言う'), N('れる', ''), N('まで', '')]),N('気づかなかった', '', [N('気づか', '気付く'), N('なかっ', 'ない'), N('た', '')]))),
     ("行きたい所全部行こう", None, R(N('行きたい所', '', [N('行きたい', '', [N('行き', '行く'), N('たい', '')]), N('所', '')]),N('全部行こう', '全部行く', [N('全部', ''), N('行こう', '行く')]))),
     ("当てられても", None, R(N('当てられても', '', [N('当て', '当てる'), N('られ', 'られる'), N('て', ''), N('も', '')]))),
-    # todo ても
+    #todo ても
     ("食べてもいいけど", None, R(N('食べても', '', [N('食べ', '食べる'), N('て', ''), N('も', '')]),N('いいけど', 'いいけれど', [N('いい', '良い'), N('けど', 'けれど')]))),
     ("逃げたり", None, R(N('逃げたり', '',[N('逃げ', '逃げる'), N('たり', '')]))),
-    # todo のに
+    #todo のに
     ("いるのにキス", None, R(N('いるのに', '', [N('いる', '居る'), N('の', ''), N('に', '')]),N('キス', ''))),
     ("するためでした", None, R(N('するためでした', '', [N('する', '為る'), N('ため', '為'), N('でし', 'です'), N('た', '')]))),
     ("ように言ったのも", None, R(N('ように', 'ようだ', [N('よう', '様'), N('に', 'だ')]),N('言ったのも', '', [N('言っ', '言う'), N('た', ''), N('の', ''), N('も', '')]))),
-    # todo: んです
+    #todo: んです
     ("探しているんですか", None, R(N('探しているんですか', '', [N('探し', '探す'), N('て', ''), N('いる', '居る'), N('ん', 'の'), N('です', ''), N('か', '')]))),
     ("一度聞いたことがある", None, R(N('一度', '', [N('一', ''), N('度', '')]),N('聞いたことが', '', [N('聞いた', '', [N('聞い', '聞く'), N('た', '')]), N('ことが', '', [N('こと', '事'), N('が', '')])]),N('ある', '有る'))),
     ("よかった", None, R(N('よかった', '', [N('よかっ', '良い'), N('た', '')]))),
-    # todo not sure about this one
+    #todo not sure about this one
     ("聞かなかったことにしてあげる", None, R(N('聞かなかった', '', [N('聞か', '聞く'), N('なかっ', 'ない'), N('た', '')]),N('ことにしてあげる', 'ことにして上げる', [N('こと', '事'), N('に', ''), N('し', '為る'), N('て', ''), N('あげる', '上げる')]))),
-    # todo　でも、 かも
+    #todo　でも、 かも
     ("今じゃ町は夜でも明るいしもう会うこともないかもな", None, R(N('今じゃ町は夜でも明るいし', '', [N('今じゃ', '今で', [N('今', ''), N('じゃ', 'で')]), N('町は', '', [N('町', ''), N('は', '')]), N('夜でも明るいし', '', [N('夜でも', '', [N('夜', ''), N('で', ''), N('も', '')]), N('明るいし', '', [N('明るい', ''), N('し', '')])])]),N('もう会う', '', [N('もう', ''), N('会う', '')]),N('こともないかもな', '', [N('ことも', '', [N('こと', '事'), N('も', '')]), N('ないかもな', '', [N('ない', '無い'), N('か', ''), N('も', ''), N('な', '')])]))),
     ("友達だから余計に気になっちゃうんだよ", None, R(N('友達だから余計に', '友達だから余計だ', [N('友達だから', '', [N('友達', ''), N('だ', ''), N('から', '')]), N('余計に', '余計だ', [N('余計', ''), N('に', 'だ')])]),N('気になっちゃうんだよ', '', [N('気に', '', [N('気', ''), N('に', '')]), N('なっちゃうんだよ', '', [N('なっ', '成る'), N('ちゃう', ''), N('ん', 'の'), N('だ', ''), N('よ', '')])]))),
 ], ids=only_string_params)
@@ -82,26 +89,17 @@ def run_tests(expected:UD2UDParseResult, parser: UD2UDParser, sentence:str) -> N
     print(repr(result))
     print("repr-single-line:")
     print(repr(result).replace("\n", "").replace("　", ""))
+
+    #These are neck and neck. Let's find out where and how they differ by catching it here.
+    assert ud2ud_tree_parser.parse(ud2ud_parsers.spoken, sentence) == ud2ud_tree_parser.parse(ud2ud_parsers.gendai, sentence)
     assert result == expected
 
 
 
-_parsers = [ud2ud_parsers.gendai,  # The leader so far
-            ud2ud_parsers.kindai,  # seems slightly less accurate than gendai.
-            ud2ud_parsers.default,  # As alternative? When differing from kindai, usually seems worse but significantly different. Polarity negative feature. Good for something?
-            ud2ud_parsers.spoken, # todo Recheck
-            ud2ud_parsers.kinsei, # todo Recheck
-            ud2ud_parsers.novel, # todo Recheck
-            ud2ud_parsers.qkana, # todo Recheck
-            ud2ud_parsers.kyogen, # todo Recheck
-            ud2ud_parsers.wakan, #No. wakan gives wack results
-            ud2ud_parsers.wabun, #No. oddness abounds
-            ud2ud_parsers.manyo #No. seems to usually give some truly strange results
-            ]
+
 
 
 @pytest.mark.parametrize("sentence", [
-    #"ダメダメ私を助けて"
     # "朝、近所をぶらぶらした。",
     # "そんなに気になるなら あの時俺も友達だって言えばよかったじゃん",
     # "普段どうやって日記読んでたんだ",
@@ -134,12 +132,12 @@ _parsers = [ud2ud_parsers.gendai,  # The leader so far
 def test_compare_parsers(sentence: str) -> None:
     print()
     print(sentence)
-    for parser in _parsers:
+    for parser in ud2ud_parsers.all_parsers:
         print(f"{parser.name} : {sentence}")
         print(parser.parse(sentence).to_tree())
         print()
 
-    for parser in _parsers:
+    for parser in ud2ud_parsers.all_parsers:
         print(f"{parser.name} : {sentence}")
         print(ud2ud_tree_parser.parse(parser, sentence))
         print()
