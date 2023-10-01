d23e71cea9cd48dc5bb4e665e6f7798184da7156
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-10-01 14:10:52 +0200

    Checkpoint.

--- src/MagnusAddon/parsing/universal_dependencies/universal_dependencies_parse_result.py
+++ src/MagnusAddon/parsing/universal_dependencies/universal_dependencies_parse_result.py
@@ -1,12 +1,24 @@
 from typing import Union
 
+from spacy.tokens import Doc, Token
 from unidic2ud import UniDic2UDEntry, UDPipeEntry
 
 from parsing.universal_dependencies.universal_dependencies_token import UDToken
 
 
 class UniversalDependenciesParseResult:
-    def __init__(self, wrapped: Union[UniDic2UDEntry]):
+    def __init__(self, wrapped: Union[UniDic2UDEntry, Doc]):
+
+        if isinstance(wrapped, Doc):
+            self._wrapped = wrapped
+
+            sents = [sent for sent in wrapped.sents]
+            if len(sents) != 1:
+                raise Exception(f"No idea how to handle {len(sents)} sents")
+
+            self._wrapped_tokens:list[Token] = [token for token in sents[0]]
+
+            self.tokens = [UDToken(token) for token in self._wrapped_tokens]
 
         if isinstance(wrapped, UniDic2UDEntry):
             self._wrapped = wrapped
@@ -14,7 +26,8 @@ class UniversalDependenciesParseResult:
             self.tokens = [UDToken(token) for token in self._wrapped_tokens]
 
         for token in self.tokens:
-            token.head = self.tokens[token.head_id]
+            # noinspection PyProtectedMember
+            token.head = self.tokens[token._head_id]
 
     def to_tree(self) -> str:
         if isinstance(self._wrapped, UniDic2UDEntry):
--- src/MagnusAddon/parsing/universal_dependencies/universal_dependencies_token.py
+++ src/MagnusAddon/parsing/universal_dependencies/universal_dependencies_token.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from spacy.tokens import Token
 from unidic2ud import UDPipeEntry
 
 from parsing.universal_dependencies import ud_japanese_part_of_speech_tag, ud_relationship_tag, ud_universal_part_of_speech_tag
@@ -11,7 +12,20 @@ def _head(token:UDPipeEntry) -> UDPipeEntry:
 _missing_value = "_"
 
 class UDToken:
-    def __init__(self, token: Union[UDPipeEntry]) -> None:
+    def __init__(self, token: Union[UDPipeEntry, Token]) -> None:
+
+        if isinstance(token, Token):
+            self.id = typed.int_(token.idx)
+            self.deps = typed.str_(token.dep_)
+            self.misc = "UNKNOWN_FIX_ME" # typed.str_(token.misc)
+            self.lemma = typed.str_(token.lemma_)
+            self.form = typed.str_(token.text)
+            self.upos = ud_universal_part_of_speech_tag.get_tag(typed.str_(token.pos_)) if token.pos_ != _missing_value else None
+            self.xpos = ud_japanese_part_of_speech_tag.get_tag(typed.str_(token.tag_)) if token.tag_ != _missing_value else None
+            self.deprel = ud_relationship_tag.get_tag(typed.str_(token.dep_)) if token.dep_ != _missing_value else None
+            self.feats = typed.str_(token.morph)
+            self._head_id = typed.int_(token.idx)
+            self.head = self  # ugly hack to get python typing working in spite of the recursive nature of this class. Will be replaced with correct value by parent object.
 
         if isinstance(token, UDPipeEntry):
             self.id = typed.int_(token.id)
@@ -23,5 +37,6 @@ class UDToken:
             self.xpos = ud_japanese_part_of_speech_tag.get_tag(typed.str_(token.xpos)) if token.xpos != _missing_value else None
             self.deprel = ud_relationship_tag.get_tag(typed.str_(token.deprel)) if token.deprel != _missing_value else None
             self.feats = typed.str_(token.feats)
-            self.head_id = typed.int_(_head(token).id)
+            self._head_id = typed.int_(_head(token).id)
             self.head = self #ugly hack to get python typing working in spite of the recursive nature of this class. Will be replaced with correct value by parent object.
+
