6ddd15cfde888aed4e84d82e2871a43085e7cd07
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-10-08 10:49:57 +0200

    Test cleanup.

--- src/MagnusAddonTests/tests/language_services_tests/universal_dependencies_tests/tree_building_tests/test_tree_builder_depth_0.py
+++ src/MagnusAddonTests/tests/language_services_tests/universal_dependencies_tests/tree_building_tests/test_tree_builder_depth_0.py
@@ -14,15 +14,13 @@ R = UDTreeSpec
 def only_string_params(param: Any) -> str: return param if isinstance(param, str) else ''
 
 @pytest.mark.parametrize('sentence, parser, expected', [
-    ("意外とかっこいいな", None, R(N('意外と', '', ''),N('かっこいいな', '', ''))),
-    ("としたら", ud_parsers.gendai, R()),
+    ("意外とかっこいいな", None, R(N('意外と', '', ''), N('かっこいいな', '', ''))),
 ], ids=only_string_params)
 def test_unsatisfied_dictionary_word_missing(sentence: str, parser: UDTokenizer | None, expected: R) -> None:
     run_tests(expected, parser if parser else ud_parsers.best, sentence)
 
 @pytest.mark.parametrize('sentence, parser, expected', [
-    ("良くない", None, R()),
-    ("行きたい所全部行こう", None, R(N('行きたい所', '', ''),N('全部', '', ''),N('行こう', '行く', ''))),
+    ("行きたい所全部行こう", None, R(N('行きたい所', '', ''), N('全部', '', ''), N('行こう', '行く', ''))),
 ], ids=only_string_params)
 def test_unsatisfied_sequential_identical_heads_not_compounded(sentence: str, parser: UDTokenizer | None, expected: R) -> None:
     run_tests(expected, parser if parser else ud_parsers.best, sentence)
@@ -39,13 +37,13 @@ def test_unsatisfied_sequential_identical_heads_not_compounded(sentence: str, pa
     ("一度夢を見た", None, R(N('一度', '', ''), N('夢を', '', ''), N('見た', '', ''))),
     ("友達だから余計に気になっちゃうんだよ", None, R(N('友達だから', '', ''), N('余計に', '', ''), N('気に', '', ''), N('なっちゃうんだよ', '', ''))),
     # todo いいよう
-    ("先生にいいように言って", None, R(N('先生に', '', ''),N('いいように言って', '', ''))),
+    ("先生にいいように言って", None, R(N('先生に', '', ''), N('いいように言って', '', ''))),
     # not a disaster, but I do miss 話の中に
-    ("あいつが話の中に出てくるのが", ud_parsers.gendai, R(N('あいつが', '', ''),N('話の', '', ''),N('中に', '', ''),N('出てくるのが', '', ''))),
+    ("あいつが話の中に出てくるのが", ud_parsers.gendai, R(N('あいつが', '', ''), N('話の', '', ''), N('中に', '', ''), N('出てくるのが', '', ''))),
     # not a disaster, but I want 藤宮さん compounded
-    ("ううん藤宮さんは日記を捨てるような人じゃない", ud_parsers.gendai, R(N('ううん', '', ''),N('藤宮さんは', '', ''),N('日記を', '', ''),N('捨てるような', '', ''),N('人じゃない', '', ''))),
+    ("ううん藤宮さんは日記を捨てるような人じゃない", ud_parsers.gendai, R(N('ううん', '', ''), N('藤宮さんは', '', ''), N('日記を', '', ''), N('捨てるような', '', ''), N('人じゃない', '', ''))),
     # 自分のこと
-    ("自分のことを知ってもらえてない人に", None, R(N('自分の', '', ''),N('ことを', '', ''),N('知ってもらえてない', '', ''),N('人に', '', ''))),
+    ("自分のことを知ってもらえてない人に", None, R(N('自分の', '', ''), N('ことを', '', ''), N('知ってもらえてない', '', ''), N('人に', '', ''))),
     # ように言った
     ("ように言ったのも", None, R(N('ように', '', ''), N('言ったのも', '', ''))),
 ])
@@ -68,7 +66,7 @@ def test_unsatisfied_dictionary_expression_missing(sentence: str, parser: UDToke
     # todo (夜)でも(case marking, 夜-head), かも(marker/case_marking, 会う-head)
     ("今じゃ町は夜でも明るいしもう会うこともないかもな", R(N('今じゃ', '', ''), N('町は', '', ''), N('夜でも', '', ''), N('明るいし', '', ''), N('もう', '', ''), N('会うこともないかもな', '', ''))),
     # todo ても, てもいい (も,いい: fixed_multiword_expression, て-head)
-    ("食べてもいいけど", R()),
+
     # todo (いる)のに(marker,case_marking: いる-head)
     ("いるのにキス", R(N('いるのに', '', ''), N('キス', '', ''))),
     # todo (聞か)なかった:ことにし(-て) (なかっ:auxiliary, た:auxiliary, こと:compound  聞か-head) (に,し fixed_multiword こと-head)
@@ -80,32 +78,38 @@ def test_sentences_we_are_unsatisfied_with(sentence: str, expected: R) -> None:
     run_tests(expected, ud_parsers.best, sentence)
 
 @pytest.mark.parametrize('sentence, expected', [
-    ("探しているんですか", R()),
     ("ダメダメ私を助けて", R(N('ダメダメ', '', ''), N('私を', '', ''), N('助けて', '', ''))),
-    ("知らない", R()),
     ("いつまでも来ないと知らないからね", R(N('いつまでも', '', ''), N('来ないと', '', ''), N('知らないからね', '', ''))),
     ("ついに素晴らしい女性に逢えた。", R(N('ついに', '', '遂に'), N('素晴らしい', '', ''), N('女性に', '', ''), N('逢えた。', '', ''))),
-    ("するためでした", R()),
-    ("なかったかな", R()),
-    ("離れていくよ", R()),
     ("言われるまで気づかなかった", R(N('言われるまで', '', ''), N('気づかなかった', '', ''))),
-    ("当てられても", R()),
-    ("逃げたり", R()),
     ("一度聞いたことがある", R(N('一度', '', ''), N('聞いたことがある', '', ''))),
-    ("よかった", R()),
-    ("良かった", R()),
-    ("良ければ", R()),
-    ("良かったら", R()),
-    ("よかったじゃん", R()),
     ("言えばよかった", R(N('言えば', '', ''), N('よかった', '', ''))),
     ("そっちへ行ったぞ", R(N('そっちへ', '', ''), N('行ったぞ', '', ''))),
-    ("だったら", R()),
-    ("だろう", R()),
-
 ], ids=only_string_params)
 def test_sentences_the_best_parser_does_well(sentence: str, expected: R) -> None:
     run_tests(expected, ud_parsers.best, sentence)
 
+@pytest.mark.parametrize('sentence', [
+    "としたら",
+    "良くない",
+    "食べてもいいけど",
+    "探しているんですか",
+    "知らない",
+    "するためでした",
+    "なかったかな",
+    "離れていくよ",
+    "よかった",
+    "良かった",
+    "良ければ",
+    "良かったら",
+    "よかったじゃん",
+    "だったら",
+    "だろう",
+    "当てられても",
+    "逃げたり",
+], ids=only_string_params)
+def test_sentences_swallowed_whole_at_this_level(sentence: str) -> None:
+    run_tests(R(), ud_parsers.best, sentence)
+
 def run_tests(expected: UDTreeSpec, parser: UDTokenizer, sentence: str) -> None:
     test_runner.run_tests(expected, parser, sentence, 0)
-
