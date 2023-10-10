27ec4ff7c413cb52863459911f56cc5e2e4cb32b
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-10-10 22:06:52 +0200

    Checkpoint.

--- src/MagnusAddon/language_services/universal_dependencies/shared/tree_building/compound_predicates.py
+++ src/MagnusAddon/language_services/universal_dependencies/shared/tree_building/compound_predicates.py
@@ -9,10 +9,16 @@ from language_services.universal_dependencies.shared.tokenizing.xpos import UdJa
 from language_services.universal_dependencies.shared.tree_building.compound_predicates_base import CompoundPredicatesBase
 
 class CompoundPredicates(CompoundPredicatesBase):
-    def next_is_dependent_of_compound(self, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
-        return lambda: self._next.head in self.compound.tokens and (not _deprel or self._next.deprel in _deprel )
-
     def true(self) -> bool: return True  # noqa
+
+    def compound_is_missing_token(self, token: UDToken) -> Callable[[], bool]:
+        return lambda: token not in self.compound.tokens
+
+    def compound_is_missing_head(self, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
+        return lambda: any(t for t in self._tokens_missing_heads() if not _deprel or t.deprel in _deprel)
+
+    def compound_is_missing_deprel_xpos_combo(self, *combo: tuple[UdRelationshipTag, UdJapanesePartOfSpeechTag]) -> Callable[[], bool]:
+        return lambda: any(t for t in self._tokens_missing_heads() if (t.deprel, t.xpos) in combo)
 
     def current_is_nominal_subject_or_oblique_nominal_of_next_that_is_adjective_i_bound(self) -> bool:
         return (self._current.deprel in {deprel.nominal_subject, deprel.oblique_nominal}
@@ -25,6 +31,12 @@ class CompoundPredicates(CompoundPredicatesBase):
     def current_is_last_sequential_deprel_xpos(self, combo: tuple[UdRelationshipTag, UdJapanesePartOfSpeechTag]) -> Callable[[], bool]:
         return lambda: ((self.compound.current.deprel, self.compound.current.xpos) == combo
                         and (self.compound.next.deprel, self.compound.next.xpos) != combo)
+
+    def next_is_dependent_of_compound(self, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
+        return lambda: self._next.head in self.compound.tokens and (not _deprel or self._next.deprel in _deprel)
+
+    def next_is_dependent_on(self, token: UDToken, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
+        return lambda: self.compound.next.head == token and (not _deprel or self._next.deprel in _deprel)
 
     def next_is_head_of_compound_token(self, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
         return lambda: self._next in set(token.head for token in self.compound.tokens) and (not _deprel or self._next.deprel in _deprel)
@@ -49,23 +61,11 @@ class CompoundPredicates(CompoundPredicatesBase):
     def next_is_first_deprel(self, _deprel: UdRelationshipTag) -> Callable[[], bool]:
         return lambda: self.compound.next.deprel == _deprel and self.compound.current.deprel != _deprel
 
-    def _tokens_missing_heads(self) -> list[UDToken]:
-        return [tok for tok in self.compound.tokens if tok.head.id > self.compound.current.id]
-
-    def missing_token(self, token: UDToken) -> Callable[[], bool]:
-        return lambda: token not in self.compound.tokens
-
-    def next_is_dependent_on(self, token: UDToken, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
-        return lambda: self.compound.next.head == token and (not _deprel or self._next.deprel in _deprel)
-
-    def compound_is_missing_head(self, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
-        return lambda: any(t for t in self._tokens_missing_heads() if not _deprel or t.deprel in _deprel)
-
-    def missing_deprel_xpos_combo(self, *combo: tuple[UdRelationshipTag, UdJapanesePartOfSpeechTag]) -> Callable[[], bool]:
-        return lambda: any(t for t in self._tokens_missing_heads() if (t.deprel, t.xpos) in combo)
-
-    def next_is_head_of_current(self, *_deprel:UdRelationshipTag) -> Callable[[], bool]:
+    def next_is_head_of_current(self, *_deprel: UdRelationshipTag) -> Callable[[], bool]:
         return lambda: self._current.head == self._next and (not _deprel or self._current.deprel in _deprel)
 
     def next_shares_head_and_xpos_with_current(self, _xpos: UdJapanesePartOfSpeechTag) -> Callable[[], bool]:
         return lambda: self._current.head == self._next.head and self._current.xpos == _xpos and self._next.xpos == _xpos
+
+    def _tokens_missing_heads(self) -> list[UDToken]:
+        return [tok for tok in self.compound.tokens if tok.head.id > self.compound.current.id]
