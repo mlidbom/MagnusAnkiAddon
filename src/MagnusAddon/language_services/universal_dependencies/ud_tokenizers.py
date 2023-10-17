cb70feb44a9554007941096eba67dd32b45b26cc
Magnus Lidbom <magnus.lidbom@gmail.com>
2023-10-17 10:35:47 +0200

    Maybe ginza is better after all...

--- src/MagnusAddon/language_services/universal_dependencies/ud_tokenizers.py
+++ src/MagnusAddon/language_services/universal_dependencies/ud_tokenizers.py
@@ -2,15 +2,15 @@ from language_services.universal_dependencies.ginza.ginza_tokenizer import Ginza
 from language_services.universal_dependencies.shared.tokenizing.ud_tokenizer import UDTokenizer
 from language_services.universal_dependencies.unidic2ud.unidic2ud_tokenizer import UD2UDTokenizer
 
-ginza: GinzaTokenizer = GinzaTokenizer()  # Yes. 15 Differences to gendai. 8 Better, 6 worse, one unclear.
-gendai: UD2UDTokenizer = UD2UDTokenizer("gendai")  # Yes. 15 Differences to ginza. 6 Better, 8 worse, one unclear.
-default: UD2UDTokenizer = gendai
-spoken: UD2UDTokenizer = UD2UDTokenizer("spoken")  # ??. Zero differences compared to gendai so far...
+ginza: UDTokenizer = GinzaTokenizer()  # Yes. 15 Differences to gendai. 8 Better, 6 worse, one unclear.
+gendai: UDTokenizer = UD2UDTokenizer("gendai")  # Yes. 15 Differences to ginza. 6 Better, 8 worse, one unclear.
+default: UDTokenizer = ginza
+spoken: UDTokenizer = UD2UDTokenizer("spoken")  # ??. Zero differences compared to gendai so far...
 
-qkana: UD2UDTokenizer = UD2UDTokenizer("qkana")  # Maybe. 2 difference with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.).
-kindai: UD2UDTokenizer = UD2UDTokenizer("kindai")  # Maybe. 8 Differences with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.).
-built_in: UD2UDTokenizer = UD2UDTokenizer("built-in")  # Maybe. 14 differences with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.).
-kinsei: UD2UDTokenizer = UD2UDTokenizer("kinsei")  # Maybe. 6 differences with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.). 1 arguably better
+qkana: UDTokenizer = UD2UDTokenizer("qkana")  # Maybe. 2 difference with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.).
+kindai: UDTokenizer = UD2UDTokenizer("kindai")  # Maybe. 8 Differences with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.).
+built_in: UDTokenizer = UD2UDTokenizer("built-in")  # Maybe. 14 differences with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.).
+kinsei: UDTokenizer = UD2UDTokenizer("kinsei")  # Maybe. 6 differences with gendai. One clearly better and used in tests(Not any more. Ginza, the current winner handled this case fine.). 1 arguably better
 
 # novel = UD2UDParser("novel") # NO. 2 difference with gendai. Both worse. Not worth the odds to try for the user.
 # kyogen = UD2UDParser("kyogen") # NO. 18 differences with gendai. Consistently strange. Did often pick up kanji 無い but that was the only upside.
