ba0a5b4160e830e44ae3b065b948c1a05fd2d3b1
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-05-06 03:30:58 +0200

    forgot this file...

--- src/MagnusAddon/sysutils/ex_sequence.py
+++ src/MagnusAddon/sysutils/ex_sequence.py
@@ -8,4 +8,10 @@ def flatten(this: Sequence[Sequence[T]]) -> list[T]:
     return [item for sub_list in this for item in sub_list]
 
 def remove_duplicates_while_retaining_order(sequence:Sequence[T]) -> list[T]:
-    return list(dict.fromkeys(sequence).keys())
+    seen = set()
+    result = []
+    for item in sequence:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
