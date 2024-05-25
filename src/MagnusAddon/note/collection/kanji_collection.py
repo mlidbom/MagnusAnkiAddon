ad344c7c8340d2bc1701c84c19f98b5f1ea4f674
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-05-25 23:17:23 +0200

    Make it possible to use kanji as radicals even if there is no such radical.

--- src/MagnusAddon/note/collection/kanji_collection.py
+++ src/MagnusAddon/note/collection/kanji_collection.py
@@ -53,9 +53,22 @@ class KanjiCollection:
                 for rad in self.dependencies_of(kanji_note)]
 
     def dependencies_of(self, kanji_note: KanjiNote) -> list[Union[RadicalNote, KanjiNote]]:
-        radical_dependency_notes = self.jp_collection.radicals.with_any_question_in(kanji_note.get_radicals())
-        radical_dependency_notes += self.jp_collection.radicals.with_any_answer_in(kanji_note.get_radical_dependencies_names())
-        radical_dependency_notes = ex_sequence.remove_duplicates_while_retaining_order(radical_dependency_notes)
-        dependency_notes: list[Union[RadicalNote, KanjiNote]] = [rad if not rad.is_replaced_by_kanji() else self.with_question(rad.get_question()) for rad in radical_dependency_notes]
+        dependency_notes: list[Union[RadicalNote, KanjiNote]] = list()
+
+        dependency_characters = kanji_note.get_radicals()
+        for character in dependency_characters:
+            radical_dependency = self.jp_collection.radicals.with_any_question_in([character])
+            if radical_dependency:
+                if not radical_dependency[0].is_replaced_by_kanji():
+                    dependency_notes.append(radical_dependency[0])
+                    continue
+
+            kanji_dependency = self.jp_collection.kanji.with_any_kanji_in([character])
+            if kanji_dependency:
+                dependency_notes.append(kanji_dependency[0])
+
+        dependency_radicals_by_name_with_no_question_character = [rad for rad in self.jp_collection.radicals.with_any_answer_in(kanji_note.get_radical_dependencies_names()) if not rad.get_question()]
+        dependency_notes += dependency_radicals_by_name_with_no_question_character
+        ex_sequence.remove_duplicates_while_retaining_order(dependency_notes)
 
         return dependency_notes
