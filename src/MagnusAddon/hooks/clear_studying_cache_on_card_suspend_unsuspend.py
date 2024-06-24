872846a82f9cf71462d6bb005c50ab9bda79d348
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-06-24 21:04:58 +0200

    This is ugly as sin, but for some reason it fixes the problem of the cache not being updated when cards are suspended or unsuspended. My best guess is that for some reason the scheduler is replaced....

--- src/MagnusAddon/hooks/clear_studying_cache_on_card_suspend_unsuspend.py
+++ src/MagnusAddon/hooks/clear_studying_cache_on_card_suspend_unsuspend.py
@@ -1,20 +1,15 @@
-from typing import Sequence
+from typing import Any, Sequence
 
 from anki.cards import CardId
 from anki.collection import OpChangesWithCount
-from aqt import gui_hooks
+from aqt import gui_hooks, mw
 
-from ankiutils import app
 from note import noteutils
 
 
-def _monkey_patch() -> None:
-    scheduler = app.col().anki_collection.sched
-    _real_suspend_cards = scheduler.suspend_cards
-    _real_unsuspend_cards = scheduler.unsuspend_cards
-
+def _monkey_patch(_something:Any) -> None:
     def remove_cards_from_cache(ids: Sequence[CardId]) -> None:
-        cards = [app.col().anki_collection.get_card(card_id) for card_id in ids]
+        cards = [mw.col.get_card(card_id) for card_id in ids]  # type: ignore
         for card in cards:
             noteutils.remove_from_studying_cache(card.note().id)
 
@@ -26,10 +21,19 @@ def _monkey_patch() -> None:
         remove_cards_from_cache(ids)
         return _real_unsuspend_cards(ids)
 
-    scheduler.suspend_cards = _monkey_patched_suspend_cards  # type: ignore
-    scheduler.unsuspend_cards = _monkey_patched_unsuspend_cards  # type: ignore
+    scheduler = mw.col.sched  # type: ignore
+
+    if not hasattr(scheduler, "is_patched_by_magnus_addon"):
+        _real_suspend_cards = scheduler.suspend_cards
+        _real_unsuspend_cards = scheduler.unsuspend_cards
+
+        scheduler.suspend_cards = _monkey_patched_suspend_cards  # type: ignore
+        scheduler.unsuspend_cards = _monkey_patched_unsuspend_cards  # type: ignore
+        scheduler.is_patched_by_magnus_addon = True # type: ignore
+
 
 
 
 def init() -> None:
-    gui_hooks.main_window_did_init.append(_monkey_patch)
+    gui_hooks.collection_did_load.append(_monkey_patch)
+    gui_hooks.browser_will_show.append(_monkey_patch)
