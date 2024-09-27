08d60c5c91f24d9868b992dee6808d75afbd2cdf
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-09-27 04:07:36 +0200

    always display 10 sentences if there are 10. Fall back to sentences being studied and then to any sentence.

--- src/MagnusAddon/hooks/note_content_building/vocab_highlighted_sentence_list.py
+++ src/MagnusAddon/hooks/note_content_building/vocab_highlighted_sentence_list.py
@@ -3,17 +3,13 @@ from aqt import gui_hooks
 
 from ankiutils import ui_utils
 from note.jpnote import JPNote
+from note.sentencenote import SentenceNote
 from note.vocabnote import VocabNote
 from sysutils import ex_sequence, ex_str, kana_utils
 from sysutils.ex_str import newline
 
 
 def generate_highlighted_sentences_html_list(_vocab_note: VocabNote) -> str:
-    sentences = _vocab_note.get_user_highlighted_sentences()
-
-    if not sentences:
-        return ""
-
     def format_sentence(html_sentence: str) -> str:
         clean_sentence = ex_str.strip_html_and_bracket_markup(html_sentence)
 
@@ -30,21 +26,47 @@ def generate_highlighted_sentences_html_list(_vocab_note: VocabNote) -> str:
 
         return clean_sentence
 
-    return f'''
-             <div id="highlightedSentencesSection" class="page_section">
-                <div class="page_section_title">highlighted sentences</div>
-                <div id="highlightedSentencesList">
-                    <div>
-                        {newline.join([f"""
-                        <div class="highlightedSentence">
-                            <div class="sentenceQuestion clipboard">{format_sentence(_sentence.get_question())}</div>
-                            <div class="sentenceAnswer"> {_sentence.get_answer()}</div>
+    def create_highlighted_sentences_html_list(sentences: list[SentenceNote], title:str) -> str:
+        return f'''
+                 <div id="highlightedSentencesSection" class="page_section">
+                    <div class="page_section_title">{title} sentences</div>
+                    <div id="highlightedSentencesList">
+                        <div>
+                            {newline.join([f"""
+                            <div class="highlightedSentence">
+                                <div class="sentenceQuestion clipboard">{format_sentence(_sentence.get_question())}</div>
+                                <div class="sentenceAnswer"> {_sentence.get_answer()}</div>
+                            </div>
+                            """ for _sentence in sentences])}
                         </div>
-                        """ for _sentence in sentences])}
                     </div>
                 </div>
-            </div>
-            '''
+                '''
+
+    highlighted_sentences = _vocab_note.get_user_highlighted_sentences()
+
+    html = ""
+    if highlighted_sentences:
+        html = create_highlighted_sentences_html_list(highlighted_sentences, "highlighted")
+
+    wanted_sentences = 10
+    displayed_sentences = len(highlighted_sentences)
+    if displayed_sentences < wanted_sentences:
+        studying_sentences = [sent for sent in _vocab_note.get_sentences_studying() if sent not in highlighted_sentences]
+        studying_sentences = studying_sentences[:(wanted_sentences - displayed_sentences)]
+        if studying_sentences:
+            html += create_highlighted_sentences_html_list(studying_sentences, "studying")
+            displayed_sentences += len(studying_sentences)
+
+        if displayed_sentences < wanted_sentences:
+            any_sentences = [sent for sent in _vocab_note.get_sentences() if sent not in highlighted_sentences and sent not in studying_sentences]
+            any_sentences = any_sentences[:(wanted_sentences - displayed_sentences)]
+            if any_sentences:
+                html += create_highlighted_sentences_html_list(any_sentences, "any")
+
+    return html
+
+
 
 def render_highlighted_sentence_list(html: str, card: Card, _type_of_display: str) -> str:
     vocab_note = JPNote.note_from_card(card)
