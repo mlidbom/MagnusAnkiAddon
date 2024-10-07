3bdf8a4eb574fef8cbd16961db9d48c8ba592f9c
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-07 19:04:06 +0200

    Change sorting

--- src/MagnusAddon/hooks/note_content_building/vocab_highlighted_sentence_list.py
+++ src/MagnusAddon/hooks/note_content_building/vocab_highlighted_sentence_list.py
@@ -65,9 +65,9 @@ def generate_highlighted_sentences_html_list(_vocab_note: VocabNote) -> str:
                                                  prefer_highlighted(x),
                                                  prefer_studying_read(x),
                                                  prefer_studying_listening(x),
-                                                 dislike_low_priority_tag(x),
                                                  prefer_high_priority_tag(x),
                                                  dislike_tts_sentences(x),
+                                                 dislike_low_priority_tag(x),
                                                  prefer_non_duplicates(x),
                                                  dislike_sentences_containing_secondary_form(x),
                                                  prefer_short_questions(x)))
