ec898e61a7e46b41e8e6ea315bb5e7ea467a804f
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-17 11:17:48 +0200

    Sort dependent kanji list by studying status.

--- src/MagnusAddon/hooks/note_content_building/radical_and_kanji_kanji_kanji_list.py
+++ src/MagnusAddon/hooks/note_content_building/radical_and_kanji_kanji_kanji_list.py
@@ -6,8 +6,7 @@ from note.jpnote import JPNote
 from note.kanjinote import KanjiNote
 from note.radicalnote import RadicalNote
 from sysutils import ex_str
-from viewmodels.kanji_list import sentence_kanji_list_viewmodel
-
+from viewmodels.kanji_list.sentence_kanji_viewmodel import KanjiViewModel
 
 def render_kanji_list(html:str, card: Card, _type_of_display:str) -> str:
     if not ui_utils.is_displaytype_displaying_answer(_type_of_display):
@@ -15,31 +14,36 @@ def render_kanji_list(html:str, card: Card, _type_of_display:str) -> str:
 
     from ankiutils import app
     note = JPNote.note_from_card(card)
-    kanjis:list[str] = []
+    kanjis:list[KanjiNote] = []
 
     if isinstance(note, KanjiNote):
-        kanjis = [n.get_question() for n in app.col().kanji.with_radical(note.get_question())]
+        kanjis = app.col().kanji.with_radical(note.get_question())
     elif isinstance(note, RadicalNote):
-        kanjis = [n.get_question() for n in app.col().kanji.with_radical(note.get_question())] if note.get_question() else []
+        kanjis = app.col().kanji.with_radical(note.get_question()) if note.get_question() else []
     else:
         return html
 
     if not kanjis:
         return html.replace("##KANJI_LIST##", "")
 
-    viewmodel = sentence_kanji_list_viewmodel.create(kanjis)
+    def prefer__studying_kanji(kan: KanjiNote) -> int:
+        return 0 if kan.is_studying() else 1
+
+    kanjis = sorted(kanjis, key= lambda kan: prefer__studying_kanji(kan))
+
+    viewmodels = [KanjiViewModel(kanji) for kanji in kanjis]
 
     list_html = f"""
 <div id="kanji_list" class="page_section">
     <div class="page_section_title">kanji</div>
 {ex_str.newline.join(f'''
-    <div class="kanji_item">
+    <div class="kanji_item {"studying" if kanji.kanji.is_studying() else ""}">
         <span class="kanji_kanji clipboard">{kanji.question()}</span>
         <span class="kanji_readings">{kanji.readings()}</span>
         <span class="kanji_answer">{kanji.answer()}</span>        
     </div>
     <div class="kanji_mnemonic">{kanji.mnemonic()}</div>
-''' for kanji in viewmodel.kanji_list)}
+''' for kanji in viewmodels)}
 </div>
         """
 
--- src/MagnusAddon/viewmodels/kanji_list/sentence_kanji_viewmodel.py
+++ src/MagnusAddon/viewmodels/kanji_list/sentence_kanji_viewmodel.py
@@ -5,19 +5,19 @@ from sysutils import ex_str
 
 class KanjiViewModel:
     def __init__(self, kanji: KanjiNote):
-        self._kanji = kanji
+        self.kanji = kanji
 
-    def question(self) -> str: return self._kanji.get_question()
-    def answer(self) -> str: return self._kanji.get_answer()
+    def question(self) -> str: return self.kanji.get_question()
+    def answer(self) -> str: return self.kanji.get_answer()
     def readings(self) -> str:
-        readings = f"""{kana_utils.to_katakana(self._kanji.get_reading_on())} <span class="readingsSeparator">|</span> {self._kanji.get_reading_kun()}"""
-        if self._kanji.get_reading_nan():
-            readings += f""" <span class="readingsSeparator">|</span> {self._kanji.get_reading_nan()}"""
+        readings = f"""{kana_utils.to_katakana(self.kanji.get_reading_on())} <span class="readingsSeparator">|</span> {self.kanji.get_reading_kun()}"""
+        if self.kanji.get_reading_nan():
+            readings += f""" <span class="readingsSeparator">|</span> {self.kanji.get_reading_nan()}"""
 
         return readings
 
     def mnemonic(self) -> str:
-        return self._kanji.get_active_mnemonic()
+        return self.kanji.get_active_mnemonic()
 
     def __str__(self) -> str:
         return f"{self.question()}      {ex_str.pad_to_length(self.answer(), 60)}: {self.readings()}"
