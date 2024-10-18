01e671fc8260e123b539d9be245b14a7afc4b57b
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-19 00:13:24 +0200

    Add a bunch of code for migrating the primary readings tags to the structure that I need.

--- src/MagnusAddon/batches/local_note_updater.py
+++ src/MagnusAddon/batches/local_note_updater.py
@@ -1,4 +1,5 @@
 import re
+from typing import Callable
 
 from anki.notes import NoteId
 
@@ -9,7 +10,7 @@ from note.note_constants import Mine
 from note.sentencenote import SentenceNote
 from note.vocabnote import VocabNote
 
-from sysutils import progress_display_runner
+from sysutils import ex_str, progress_display_runner
 
 def update_all() -> None:
     update_sentences()
@@ -92,7 +93,7 @@ def tag_kanji_metadata() -> None:
                 for reading in vocab.get_readings():
                     if reading not in primary_readings:
                         kanji.set_tag(Mine.Tags.kanji_with_single_kanji_vocab_with_different_reading)
-                        if vocab.is_studying(reading):
+                        if vocab.is_studying():
                             kanji.set_tag(Mine.Tags.kanji_with_studying_single_kanji_vocab_with_different_reading)
 
 
@@ -101,3 +102,51 @@ def tag_kanji_metadata() -> None:
     all_kanji = app.col().kanji.all()
     progress_display_runner.process_with_progress(all_kanji, tag_kanji, "Tagging kanji with studying metadata")
     progress_display_runner.process_with_progress(all_kanji, tag_has_single_kanji_vocab_with_reading_different_from_kanji_primary_reading, "Tagging kanji with single kanji vocab")
+
+
+# def backup_kanji_readings() -> None:
+#     for kanji in app.col().kanji.all():
+#         kanji.set_field("_on_backup", kanji.get_field(NoteFields.Kanji.Reading_On))
+#         kanji.set_field("_kun_backup", kanji.get_field(NoteFields.Kanji.Reading_Kun))
+
+def adjust_kanji_primary_readings() -> None:
+    primary_reading_pattern = re.compile(r'<primary>(.*?)</primary>')
+
+    def find_and_fix_reading(type:str, primary_reading: str, current_readings:str, setter:Callable[[str], None]) -> None:
+                new_text = re.sub(rf'\b{re.escape(primary_reading)}\b', f'<primary>{primary_reading}</primary>', current_readings)
+                print(f"""{type}:replacing 
+{current_readings} with 
+{new_text}
+
+""")
+                setter(new_text)
+
+
+    def adjust_kanji_readings(kanji: KanjiNote) -> None:
+        vocabs = app.col().vocab.with_kanji(kanji)
+        single_kanji_vocab = [v for v in vocabs if v.get_question() == kanji.get_question()]
+        if single_kanji_vocab:
+            primary_on_readings: list[str] = primary_reading_pattern.findall(kanji.get_reading_on())
+            primary_kun_readings: list[str] = primary_reading_pattern.findall(kanji.get_reading_kun())
+            primary_readings: set[str] = set(primary_on_readings + primary_kun_readings)
+
+            for vocab in single_kanji_vocab:
+                for reading in vocab.get_readings():
+                    if reading not in primary_readings:
+                        kanji.set_field("_primary_readings_tts_audio", "")
+                        kanji.set_tag("fixed_primary_readings")
+                        if reading in kanji.get_reading_on_list():
+                            find_and_fix_reading("ON", reading, kanji.get_reading_on(), kanji.set_reading_on)
+                        elif reading in kanji.get_reading_kun_list():
+                            find_and_fix_reading("KUN", reading, kanji.get_reading_kun(), kanji.set_reading_kun)
+                        elif reading in kanji.get_reading_nan_list():
+                            find_and_fix_reading("NAN", reading, kanji.get_reading_nan(), kanji.set_reading_nan)
+                        else:
+                            print(f"{kanji.get_question()}:WTF:{reading} #####################################################################################")
+
+
+    progress_display_runner.process_with_progress(app.col().kanji.all(), adjust_kanji_readings, "Adjusting kanji readings")
+
+def remove_kun_readings_html() -> None:
+    for kanji in app.col().kanji.all():
+        kanji.set_reading_kun(ex_str.strip_html_markup(kanji.get_reading_kun()))
