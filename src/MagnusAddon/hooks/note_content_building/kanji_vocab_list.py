2de76f8bbac41a931daa760a9126c3b84c21f7c6
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-20 16:56:49 +0200

    Just about the same bug again, for a vocab with no readings

--- src/MagnusAddon/hooks/note_content_building/kanji_vocab_list.py
+++ src/MagnusAddon/hooks/note_content_building/kanji_vocab_list.py
@@ -13,7 +13,7 @@ def _create_classes(_kanji: KanjiNote, _vocab: VocabNote) -> str:
     classes = " ".join([f"""common_ness_{prio}""" for prio in tags])
     classes += f""" {_vocab.priority_spec().priority_string}"""
     classes += " " + " ".join(_vocab.get_meta_tags())
-    if _vocab.get_question() in _kanji.get_primary_vocab() or _vocab.get_readings()[0] in _kanji.get_primary_vocab():
+    if _vocab.get_question() in _kanji.get_primary_vocab() or (_vocab.get_readings() and _vocab.get_readings()[0] in _kanji.get_primary_vocab()):
         classes += " primary_vocab"
 
     return classes
