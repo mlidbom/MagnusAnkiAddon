febd5e0a57c5b2c5120953181b1d18b6007584fe
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-22 16:40:16 +0200

    Add some stuff to try and figure out where the occational slowdown comes from

--- src/MagnusAddon/sysutils/timeutil.py
+++ src/MagnusAddon/sysutils/timeutil.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import time
 from typing import Callable
 
@@ -15,10 +16,28 @@ def time_execution(callback: Callable[[], None]) -> str:
     milliseconds = int((elapsed_time - seconds) * 1000)
     return f"{seconds}:{milliseconds:03}"
 
-def format_seconds_as_hh_mm_ss(estimated_remaining_time: float) -> str:
-    # Convert estimated remaining time to hours:minutes:seconds format
-    hours = int(estimated_remaining_time // 3600)
-    minutes = int((estimated_remaining_time % 3600) // 60)
-    seconds = int(estimated_remaining_time % 60)
+def format_seconds_as_hh_mm_ss(seconds: float) -> str:
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    sec = int(seconds % 60)
+    return f'{hours:02}:{minutes:02}:{sec:02}'
 
-    return f"{hours:02}:{minutes:02}:{seconds:02}"
+def format_seconds_as_hh_mm_ss_ttt(seconds: float) -> str:
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    sec = int(seconds % 60)
+    milliseconds = int((seconds - int(seconds)) * 1000)
+    return f'{hours:02}:{minutes:02}:{sec:02} {milliseconds:03}'
+
+def start_stop_watch() -> StopWatch:
+    return StopWatch()
+
+class StopWatch:
+    def __init__(self) -> None:
+        self.start_time = time.time()
+
+    def elapsed_seconds(self) -> float:
+        return time.time() - self.start_time
+
+    def elapsed_formatted(self) -> str:
+        return format_seconds_as_hh_mm_ss_ttt(time.time() - self.start_time)
