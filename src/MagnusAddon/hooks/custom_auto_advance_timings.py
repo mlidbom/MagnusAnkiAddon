10bae6be5b236acdd7b25963a932836c79db7c0d
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-30 21:31:11 +0100

    Only automatically handle auto advance for reading cards.

--- src/MagnusAddon/hooks/custom_auto_advance_timings.py
+++ src/MagnusAddon/hooks/custom_auto_advance_timings.py
@@ -3,8 +3,10 @@ from typing import Any
 from anki.cards import Card
 from aqt import gui_hooks, mw
 
+from note import cardutils
 from note.jpnote import JPNote
 from note.kanjinote import KanjiNote
+from note.note_constants import CardTypes
 from note.sentencenote import SentenceNote
 from note.vocabnote import VocabNote
 from sysutils import timeutil, typed
@@ -13,6 +15,9 @@ def _monkey_patch(html:str, _card:Any, _something_else_again:Any) -> str:
     reviewer = mw.reviewer
 
     def is_handled_card() -> bool:
+        if cardutils.card_type(typed.checked_cast(Card, reviewer.card)) != CardTypes.reading:
+            return False
+
         note = JPNote.note_from_card(typed.checked_cast(Card, reviewer.card))
         return isinstance(note, SentenceNote) or isinstance(note, VocabNote) or isinstance(note, KanjiNote)
 
