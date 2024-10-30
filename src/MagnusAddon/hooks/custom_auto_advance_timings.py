df93d059638120dec1c2e4e747f02ec05625e03d
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-10-30 19:38:52 +0100

    Also automatically start auto advance for kanji, but, for now at least, just use the configured time in the deck.

--- src/MagnusAddon/hooks/custom_auto_advance_timings.py
+++ src/MagnusAddon/hooks/custom_auto_advance_timings.py
@@ -4,6 +4,7 @@ from anki.cards import Card
 from aqt import gui_hooks, mw
 
 from note.jpnote import JPNote
+from note.kanjinote import KanjiNote
 from note.sentencenote import SentenceNote
 from note.vocabnote import VocabNote
 from sysutils import timeutil, typed
@@ -13,16 +14,19 @@ def _monkey_patch(html:str, _card:Any, _something_else_again:Any) -> str:
 
     def is_handled_card() -> bool:
         note = JPNote.note_from_card(typed.checked_cast(Card, reviewer.card))
-        return isinstance(note, SentenceNote) or isinstance(note, VocabNote)
+        return isinstance(note, SentenceNote) or isinstance(note, VocabNote) or isinstance(note, KanjiNote)
 
     def milliseconds_to_show_question() -> int:
-        note = JPNote.note_from_card(typed.checked_cast(Card, reviewer.card))
+        card = typed.checked_cast(Card, reviewer.card)
+        note = JPNote.note_from_card(card)
         if isinstance(note, SentenceNote):
             return note.get_allowed_read_review_time_milliseconds()
         elif isinstance(note, VocabNote):
             return note.get_allowed_read_review_time_milliseconds()
-        else:
-           raise Exception("This should never happen")
+        elif isinstance(note, KanjiNote):
+            return int(reviewer.mw.col.decks.config_dict_for_deck_id(card.current_deck_id())["secondsToShowQuestion"] * 1000)
+
+        raise Exception("We should never get here")
 
 
     def patched_auto_advance_to_answer_if_enabled() -> None:
