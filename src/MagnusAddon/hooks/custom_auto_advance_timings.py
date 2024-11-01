8dcfd6664e62a5897658ce14d5ccb5abc71f178c
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-01 19:00:51 +0100

    I was getting odd very short timeouts occasionally for the second card shown. I think this might have fixed it....

--- src/MagnusAddon/hooks/custom_auto_advance_timings.py
+++ src/MagnusAddon/hooks/custom_auto_advance_timings.py
@@ -12,24 +12,22 @@ from note.vocabnote import VocabNote
 from sysutils import timeutil, typed
 
 def _monkey_patch(html:str, _card:Any, _something_else_again:Any) -> str:
-    reviewer = mw.reviewer
-
     def is_handled_card() -> bool:
-        if cardutils.card_type(typed.checked_cast(Card, reviewer.card)) != CardTypes.reading:
+        if cardutils.card_type(typed.checked_cast(Card, mw.reviewer.card)) != CardTypes.reading:
             return False
 
-        note = JPNote.note_from_card(typed.checked_cast(Card, reviewer.card))
+        note = JPNote.note_from_card(typed.checked_cast(Card, mw.reviewer.card))
         return isinstance(note, SentenceNote) or isinstance(note, VocabNote) or isinstance(note, KanjiNote)
 
     def milliseconds_to_show_question() -> int:
-        card = typed.checked_cast(Card, reviewer.card)
+        card = typed.checked_cast(Card, mw.reviewer.card)
         note = JPNote.note_from_card(card)
         if isinstance(note, SentenceNote):
             return note.get_allowed_read_review_time_milliseconds()
         elif isinstance(note, VocabNote):
             return note.get_allowed_read_review_time_milliseconds()
         elif isinstance(note, KanjiNote):
-            return int(reviewer.mw.col.decks.config_dict_for_deck_id(card.current_deck_id())["secondsToShowQuestion"] * 1000)
+            return int(mw.reviewer.mw.col.decks.config_dict_for_deck_id(card.current_deck_id())["secondsToShowQuestion"] * 1000)
 
         raise Exception("We should never get here")
 
@@ -39,25 +37,25 @@ def _monkey_patch(html:str, _card:Any, _something_else_again:Any) -> str:
             _real_auto_advance_to_answer_if_enabled()
             return
 
-        reviewer._clear_auto_advance_timers() # noqa
+        mw.reviewer._clear_auto_advance_timers() # noqa
         allowed_milliseconds = milliseconds_to_show_question()
-        if not reviewer.auto_advance_enabled:
-            reviewer.toggle_auto_advance()
 
-        reviewer._show_answer_timer = reviewer.mw.progress.timer(
+        mw.reviewer._show_answer_timer = mw.reviewer.mw.progress.timer(
             allowed_milliseconds,
-            reviewer._on_show_answer_timeout, # noqa
+            mw.reviewer._on_show_answer_timeout, # noqa
             repeat=False,
-            parent=reviewer.mw,
+            parent=mw.reviewer.mw,
         )
 
         print(f"Set allowed time to: {timeutil.format_seconds_as_ss_ttt(allowed_milliseconds)}")
 
+    if is_handled_card() and not mw.reviewer.auto_advance_enabled:
+        mw.reviewer.toggle_auto_advance()
 
-    if not hasattr(reviewer, "is_patched_by_magnus_addon_for_auto_advance_timings"):
-        _real_auto_advance_to_answer_if_enabled = reviewer._auto_advance_to_answer_if_enabled # noqa
-        reviewer._auto_advance_to_answer_if_enabled = patched_auto_advance_to_answer_if_enabled # type: ignore
-        reviewer.is_patched_by_magnus_addon_for_auto_advance_timings = True # type: ignore
+    if not hasattr(mw.reviewer, "is_patched_by_magnus_addon_for_auto_advance_timings"):
+        _real_auto_advance_to_answer_if_enabled = mw.reviewer._auto_advance_to_answer_if_enabled # noqa
+        mw.reviewer._auto_advance_to_answer_if_enabled = patched_auto_advance_to_answer_if_enabled # type: ignore
+        mw.reviewer.is_patched_by_magnus_addon_for_auto_advance_timings = True # type: ignore
 
     return html
 
