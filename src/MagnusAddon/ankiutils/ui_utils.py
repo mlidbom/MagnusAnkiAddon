5057124340f0f9b38ab76aa2a079e247759d567e
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-06 21:58:48 +0100

    Fix UI refresh so that it works even if the previewer cannot tell that there has been a change.

--- src/MagnusAddon/ankiutils/ui_utils.py
+++ src/MagnusAddon/ankiutils/ui_utils.py
@@ -1,6 +1,7 @@
 from typing import Callable
 
 import aqt
+from anki.cards import Card
 from aqt import AnkiQt  # type: ignore
 from aqt.browser import Browser  # type: ignore
 from aqt.browser.previewer import Previewer
@@ -11,6 +12,7 @@ from aqt.utils import tooltip
 from ankiutils.audio_suppressor import audio_suppressor
 from ankiutils.ui_utils_interface import IUIUtils
 from sysutils import timeutil
+from sysutils.typed import checked_cast
 
 _ANSWER_DISPLAY_TYPES = {'reviewAnswer', 'previewAnswer', 'clayoutAnswer'}
 
@@ -36,22 +38,35 @@ class UIUtils(IUIUtils):
         self.refresh()
         tooltip(f"done in {time}")
 
+
     def refresh(self, refresh_browser:bool = True) -> None:
         from ankiutils import app
-        app.col().flush_cache_updates()
-        audio_suppressor.suppress_for_seconds(.3)
-        if self._mw.reviewer.card:
-            self._mw.reviewer._refresh_needed = RefreshNeeded.NOTE_TEXT
-            self._mw.reviewer.refresh_if_needed()
 
-        previewer: list[Previewer] = [window for window in self._mw.app.topLevelWidgets() if isinstance(window, Previewer)]
-        if len(previewer) > 0:
-            previewer[0].render_card()
+        def force_previewer_rerender() -> None:
+            previewers: list[Previewer] = [window for window in self._mw.app.topLevelWidgets() if isinstance(window, Previewer)]
+            if len(previewers) > 0:
+                previewer = previewers[0]
+                previewer._last_state = [previewer._state, checked_cast(Card, previewer.card()).id, 0]  # noqa
+                previewer.render_card()
 
-        if refresh_browser:
+        def force_reviewer_rerender() -> None:
+            if self._mw.reviewer.card:
+                self._mw.reviewer._refresh_needed = RefreshNeeded.NOTE_TEXT
+                self._mw.reviewer.refresh_if_needed()
+
+        def force_browser_rerender() -> None:
             browser: list[Browser] = [window for window in self._mw.app.topLevelWidgets() if isinstance(window, Browser)]
             if len(browser) > 0:
                 browser[0].onSearchActivated()
+
+        app.col().flush_cache_updates()
+        audio_suppressor.suppress_for_seconds(.3)
+        force_reviewer_rerender()
+        force_previewer_rerender()
+
+        if refresh_browser:
+            force_browser_rerender()
+
 
     def show_current_review_in_preview(self) -> None:
         self._mw.onBrowse()
