4f18d3c8c854c700efa07e590a3ecca3233cb909
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-09 06:26:25 +0100

    Unbelieably, the performance issue I've been seeing turns out to be caused by the clipboard integration. So switch to pyperclip which solves the issue.

--- src/MagnusAddon/hooks/copy_sort_field_to_clipboard.py
+++ src/MagnusAddon/hooks/copy_sort_field_to_clipboard.py
@@ -1,31 +1,27 @@
-from typing import cast
-
+import pyperclip
 from anki.cards import Card
-from anki.models import NotetypeDict
-from anki.notes import Note
 from aqt import gui_hooks
 
+from ankiutils import app
 from ankiutils.app import ui_utils
-from sysutils import my_clipboard
+from note.jpnote import JPNote
 from sysutils.collections.recent_items import RecentItems
 from sysutils import ex_str
 
 recent_previewer_cards = RecentItems[int](2)
 
-def copy_card_sort_field_to_clipboard(note: Note) -> None:
-    model = cast(NotetypeDict, note.note_type())
-    sort_field = model['sortf']
-    sort_value = note.fields[sort_field]
-    clean_string = ex_str.strip_html_and_bracket_markup_and_noise_characters(sort_value)
-    my_clipboard.set_text(clean_string)
+def copy_card_sort_field_to_clipboard(note: JPNote) -> None:
+    clean_string = ex_str.strip_html_and_bracket_markup_and_noise_characters(note.get_question())
+    pyperclip.copy(clean_string)
 
 recent_review_answers = RecentItems[int](1)
 def on_reviewer_show_answer(card: Card) -> None:
-    note = card.note()
-    if ui_utils().is_edit_current_open() or recent_review_answers.is_recent(note.id):
-        return
+    note = app.col().note_by_id(card.nid)
+    if note:
+        if ui_utils().is_edit_current_open() or recent_review_answers.is_recent(note.get_id()):
+            return
 
-    copy_card_sort_field_to_clipboard(note)
+        copy_card_sort_field_to_clipboard(note)
 
 def init() -> None:
     gui_hooks.reviewer_did_show_answer.append(on_reviewer_show_answer)
--- src/MagnusAddon/note/collection/jp_collection.py
+++ src/MagnusAddon/note/collection/jp_collection.py
@@ -48,3 +48,7 @@ class JPCollection:
     def flush_cache_updates(self) -> None: self.cache_manager.flush_updates()
     def pause_cache_updates(self) -> None: self.cache_manager.pause_data_generation()
     def resume_cache_updates(self) -> None: self.cache_manager.resume_data_generation()
+    def note_by_id(self, id: NoteId) -> JPNote | None:
+        return self.vocab.with_id(id) or self.kanji.with_id(id) or self.sentences.with_id(id) or None
+
+
