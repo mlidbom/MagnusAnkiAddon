6540a3e7285cbe251b82391576bc46cdbe823807
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-09 06:42:42 +0100

    Fix typing warning.

--- src/MagnusAddon/note/collection/jp_collection.py
+++ src/MagnusAddon/note/collection/jp_collection.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from anki.collection import Collection
 from anki.notes import NoteId
 
@@ -48,7 +50,7 @@ class JPCollection:
     def flush_cache_updates(self) -> None: self.cache_manager.flush_updates()
     def pause_cache_updates(self) -> None: self.cache_manager.pause_data_generation()
     def resume_cache_updates(self) -> None: self.cache_manager.resume_data_generation()
-    def note_by_id(self, id: NoteId) -> JPNote | None:
+    def note_by_id(self, id: NoteId) -> Optional[JPNote]:
         return self.vocab.with_id(id) or self.kanji.with_id(id) or self.sentences.with_id(id) or None
 
 
