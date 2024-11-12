c12938d070ed5b4873124d258926904b88ee12ec
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-12 05:59:27 +0100

    Start getting rid of the obsolete, massively time-expensive, failed attempt to make python code figure out everything about a sentence.

--- src/MagnusAddon/hooks/note_content_building/ud_sentence_breakdown.py
+++ src/MagnusAddon/hooks/note_content_building/ud_sentence_breakdown.py
@@ -3,57 +3,9 @@ from aqt import gui_hooks
 from ankiutils import app
 from hooks.note_content_building.content_renderer import PrerenderingAnswerContentRenderer
 from language_services.shared import priorities
-from language_services.universal_dependencies import ud_tokenizers
-from language_services.universal_dependencies.shared.tree_building import ud_tree_builder
 from note.sentencenote import SentenceNote
 from note.vocabnote import VocabNote
 from sysutils import kana_utils
-from viewmodels.sentence_breakdown import sentence_breakdown_viewmodel
-from viewmodels.sentence_breakdown.sentence_breakdown_viewmodel import NodeViewModel, VocabHit
-
-def _node_html(node: NodeViewModel, excluded: set[str], highlighted: set[str], depth: int) -> str:
-    def priority_class(question: str) -> str:
-        return "word_priority_" + node.get_priority_class(question, highlighted)
-
-    html = ""
-
-    vocab_hits:list[VocabHit] = node.surface_vocab_hits + node.base_vocab_hits
-    vocab_hits = [voc for voc in vocab_hits if voc.hit_form not in excluded]
-    exact_match = [voc for voc in vocab_hits if voc.is_exact_match()]
-    if exact_match:
-        vocab_hits = [exact_match[0]]
-
-    if vocab_hits:
-        for vocab_entry in vocab_hits:
-            needs_reading = True #I need to practice katakana #kana_utils.contains_kanji(vocab_entry.lookup_form) and (not vocab_entry.hit_form or kana_utils.contains_kanji(vocab_entry.hit_form))
-            readings = ", ".join(vocab_entry.readings) if needs_reading else ""
-            readings = kana_utils.to_katakana(readings)
-            html += f"""
-            <li class="sentenceVocabEntry depth{depth} {vocab_entry.meta_tags} {priority_class(vocab_entry.lookup_form if vocab_entry.lookup_form else vocab_entry.surface_form)}">
-                <div class="sentenceVocabEntryDiv">
-                    <span class="vocabQuestion clipboard">{vocab_entry.surface_form}</span>
-                    {f'''<span class="vocabLookupForm {'' if vocab_entry.hit_form else 'vocabHitForm' } clipboard">{vocab_entry.lookup_form}</span>''' if vocab_entry.lookup_form else ""}
-                    {f'''<span class="vocabHitForm clipboard">{vocab_entry.hit_form}</span>''' if vocab_entry.hit_form else ""}
-                    {f'''<span class="vocabHitReadings clipboard">{readings}</span>''' if readings else ""}
-                    {vocab_entry.meta_tags_html}
-                    
-                    
-                    <span class="vocabAnswer">{vocab_entry.answer}</span>
-                </div>
-                {_create_html_from_nodes(node.children, excluded, highlighted, depth + 1)}
-            </li>
-            """
-    else:
-        html += f"""
-            <li class="sentenceVocabEntry depth{depth} {priority_class(node.surface)}">
-                <div class="sentenceVocabEntryDiv">
-                    <span class="vocabQuestion clipboard">{node.surface}</span>
-                </div>
-                {_create_html_from_nodes(node.children, excluded, highlighted, depth + 1)}
-            </li>
-            """
-
-    return html
 
 def _build_vocab_list(word_to_show: list[str], excluded_words:set[str], title:str, include_extended_sentence_statistics:bool = False) -> str:
     html = f"""
@@ -73,9 +25,8 @@ def _build_vocab_list(word_to_show: list[str], excluded_words:set[str], title:st
         if vocabs:
             for vocab in vocabs:
                 hit_form = vocab.get_question() if vocab.get_question() != word else ""
-                needs_reading = True #I need to practice katakana # kana_utils.contains_kanji(word) and (not hit_form or kana_utils.contains_kanji(hit_form))
+                needs_reading = kana_utils.contains_kanji(word) and (not hit_form or kana_utils.contains_kanji(hit_form))
                 readings = ", ".join(vocab.get_readings()) if needs_reading else ""
-                readings = kana_utils.to_katakana(readings)
                 html += f"""
                         <li class="sentenceVocabEntry depth1 word_priority_{priorities.very_high} {" ".join(vocab.get_meta_tags())}">
                             <div class="sentenceVocabEntryDiv">
@@ -104,44 +55,6 @@ def _build_vocab_list(word_to_show: list[str], excluded_words:set[str], title:st
     """
     return html
 
-def _create_html_from_nodes(nodes: list[NodeViewModel], excluded: set[str], extra: set[str], depth: int) -> str:
-    if not nodes:
-        return ""
-
-    html = f"""<ul class="sentenceVocabList depth{depth}">\n"""
-
-    for node in nodes:
-        html += _node_html(node, excluded, extra, depth)
-
-    html += "</ul>\n"
-
-    return html
-
-def print_debug_information_for_analysis(sentence: str) -> str:
-    html = f"""<div id="debug_output">\n"""
-
-    for parser in ud_tokenizers.all_tokenizers:
-        html += f"""{parser.name} : {sentence}
-{parser.tokenize(sentence).str_()}
-
-"""
-
-    for parser in ud_tokenizers.all_tokenizers:
-        html += f"""{parser.name} : {sentence}
-{parser.tokenize(sentence).to_tree()}
-
-"""
-
-    for parser in ud_tokenizers.all_tokenizers:
-        html += f"""{parser.name} : {sentence}")
-{ud_tree_builder.build_tree(parser, sentence)}
-
-"""
-
-    html += """</div>\n"""
-
-    return html
-
 def render_parsed_words(note: SentenceNote, replacements:dict[str, str]) ->None:
     replacements["##PARSED_WORDS##"] = _build_vocab_list(note.get_parsed_words(), note.get_user_excluded_vocab(), "parsed words")
 
@@ -151,25 +64,6 @@ def render_excluded_words(note: SentenceNote, replacements:dict[str, str]) ->Non
 def render_user_extra_list(note: SentenceNote, replacements:dict[str, str]) ->None:
     replacements["##USER_EXTRA_VOCAB##"] = _build_vocab_list(note.get_user_highlighted_vocab(), note.get_user_excluded_vocab(), "highlighted words", include_extended_sentence_statistics=True) if note.get_user_highlighted_vocab() else ""
 
-def render_breakdown(note: SentenceNote, replacements:dict[str, str]) ->None:
-    question = note.get_question()
-    tree = ud_tree_builder.build_tree(ud_tokenizers.default, question)
-    view_model = sentence_breakdown_viewmodel.create(tree, app.col())
-
-    user_higlighted = set(note.get_user_highlighted_vocab())
-
-    breakdown_html = f"""
-    <div class="breakdown page_section">
-    <div class="page_section_title">breakdown</div>
-    {_create_html_from_nodes(view_model.nodes, note.get_user_excluded_vocab(), user_higlighted, 1)}
-    </div>
-    """
-    # todo: restore this or remove the related code
-    # html += f"""
-    #
-    # {print_debug_information_for_analysis(sentence.get_question())}
-    # """
-    replacements["##VOCAB_BREAKDOWN##"] = breakdown_html
 
 def render(note: SentenceNote) -> dict[str, str]:
     replacements:dict[str, str] = {}
@@ -177,7 +71,6 @@ def render(note: SentenceNote) -> dict[str, str]:
     render_parsed_words(note, replacements)
     render_user_extra_list(note, replacements)
     render_excluded_words(note, replacements)
-    render_breakdown(note, replacements)
 
 
     return replacements
