6036e027e5dd4845402d83ddbd928c8719e59f6d
Magnus Lidbom <magnus.lidbom@gmail.com>
2024-11-16 02:18:08 +0100

    Make sure the short term scheduling works in filtered decks.

--- src/MagnusAddon/anki_extentions/sheduling_states_ex.py
+++ src/MagnusAddon/anki_extentions/sheduling_states_ex.py
@@ -6,11 +6,17 @@ class SchedulingStatesEx:
 
     def is_relearning(self) -> bool: return self.states.again.normal.relearning.learning.scheduled_secs > 0
     def is_learning(self) -> bool: return self.states.again.normal.learning.scheduled_secs > 0
+    def is_filtered_learning(self) -> bool: return self.states.again.filtered.rescheduling.original_state.learning.scheduled_secs > 0
+    def is_filtered_relearning(self) -> bool: return self.states.again.filtered.rescheduling.original_state.relearning.learning.scheduled_secs > 0
 
     def set_again_seconds(self, seconds:int) -> None:
         if self.is_relearning():
             self.states.again.normal.relearning.learning.scheduled_secs = seconds
         elif self.is_learning():
             self.states.again.normal.learning.scheduled_secs = seconds
+        elif self.is_filtered_relearning():
+            self.states.again.filtered.rescheduling.original_state.relearning.learning.scheduled_secs = seconds
+        elif self.is_filtered_learning():
+            self.states.again.filtered.rescheduling.original_state.learning.scheduled_secs = seconds
         else:
             raise Exception("May only be called on relearning or learning cards")
