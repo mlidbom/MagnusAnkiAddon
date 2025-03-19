d77de4a5bb6f5f4f8e0778ffeddecb2aa34b5866
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-03-19 20:53:00 +0100

    Fix immersionkit search.

--- src/MagnusAddon/hooks/right_click_menu_search.py
+++ src/MagnusAddon/hooks/right_click_menu_search.py
@@ -35,7 +35,7 @@ def setup_web_search_menu(string_menu:QMenu, menu_string:str) -> None:
 
     add_web_lookup(search_web_menu, shortcutfinger.home1("English: Merriam Webster"), u"https://www.merriam-webster.com/dictionary/%s", menu_string)
 
-    add_web_lookup(search_web_menu, shortcutfinger.home2("Sentences: Immersion Kit"), u"https://www.immersionkit.com/dictionary?exact=true&sort=shortness&keyword=%s", menu_string)
+    add_web_lookup(search_web_menu, shortcutfinger.home2("Sentences: Immersion Kit"), u"https://www.immersionkit.com/dictionary?exact=true&sort=sentence_length%%3Aasc&keyword=%s", menu_string)
     add_web_lookup(search_web_menu, shortcutfinger.home3("Sentences: Tatoeba"), u"https://tatoeba.org/en/sentences/search?from=jpn&to=eng&query=%s", menu_string)
 
     kanji_lookup_menu = non_optional(search_web_menu.addMenu(shortcutfinger.home4("Kanji")))
