e90aef249f2d94a6699d2e23107140dc2f8464ca
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-04-26 01:42:09 +0200

    also cache possible names.

--- src/MagnusAddon/language_services/jamdict_ex/dict_lookup.py
+++ src/MagnusAddon/language_services/jamdict_ex/dict_lookup.py
@@ -4,6 +4,7 @@ from functools import lru_cache
 from typing import TYPE_CHECKING
 
 from jamdict.jmdict import JMDEntry
+from puchikarui import ExecutionContext
 
 from language_services.jamdict_ex.priority_spec import PrioritySpec
 from sysutils.lazy import BackgroundInitialingLazy
@@ -18,6 +19,7 @@ from language_services.jamdict_ex.dict_entry import DictEntry
 from sysutils import ex_iterable, kana_utils
 
 _jamdict = Jamdict(reuse_ctx=False)
+
 def _find_all_words() -> set[str]:
     kanji_forms: set[str] = set()
     kana_forms: set[str] = set()
@@ -34,7 +36,24 @@ def _find_all_words() -> set[str]:
 
     return kanji_forms | kana_forms
 
+def _find_all_names() -> set[str]:
+    kanji_forms: set[str] = set()
+    kana_forms: set[str] = set()
+
+    with _jamdict.jmdict.ctx() as ctx:
+        for batch in ctx.conn.execute("SELECT distinct text FROM NEKanji"):
+            for row in batch:
+                kanji_forms.add(str_(row))
+
+    with _jamdict.jmdict.ctx() as ctx:
+        for batch in ctx.conn.execute("SELECT distinct text FROM NEKana"):
+            for row in batch:
+                kana_forms.add(str_(row))
+
+    return kanji_forms | kana_forms
+
 _all_word_forms = BackgroundInitialingLazy(_find_all_words)
+_all_name_forms = BackgroundInitialingLazy(_find_all_names)
 
 class DictLookup:
     def __init__(self, entries: list[DictEntry], lookup_word: str, lookup_reading: list[str]):
@@ -67,6 +86,8 @@ class DictLookup:
     @classmethod
     @lru_cache(maxsize=None)
     def _try_lookup_word_or_name(cls, word: str, readings: tuple[str, ...]) -> DictLookup:
+        if not cls.might_be_entry(word): return DictLookup([], word,[])
+
         def kanji_form_matches() -> list[DictEntry]:
             return [ent for ent in lookup
                     if any(ent.has_matching_kana_form(reading) for reading in readings)
@@ -103,6 +124,7 @@ class DictLookup:
     @classmethod
     @lru_cache(maxsize=None)  # _lookup_word_shallow.cache_clear(), _lookup_word_shallow.cache_info()
     def _lookup_name(cls, word: str) -> list[JMDEntry]:
+        if not cls.might_be_name(word): return []
         return list(_jamdict.lookup(word, lookup_chars=False).names)
 
     @staticmethod
@@ -114,6 +136,14 @@ class DictLookup:
     @classmethod
     def might_be_word(cls, word:str) -> bool:
         return word in _all_word_forms.instance()
+
+    @classmethod
+    def might_be_name(cls, word:str) -> bool:
+        return word in _all_name_forms.instance()
+
+    @classmethod
+    def might_be_entry(cls, word:str) -> bool:
+        return cls.might_be_word(word) or cls.might_be_name(word)
 
     @classmethod
     def is_word(cls, word:str) -> bool:
