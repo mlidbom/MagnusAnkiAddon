aea8f19032971df1a39f6d4ee023ae165addefff
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-04-28 19:15:46 +0200

    cruft

--- src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_word_extractor_jn.py
+++ src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_word_extractor_jn.py
@@ -131,7 +131,7 @@ def insert_custom_words(custom_words:list[str]) -> None:
     ("怖くなくなったの", [], [], ['怖い', 'なくなる', 'た', 'の']),
     ("落ちてないかな", [], [], ['落ちる', 'てる', 'ないか', 'な']),
     ("分かってたら", [], [], ['分かる', 'てたら']),
-    ("頑張れたというか", [], [], ['頑張れる', 'た', 'というか']),
+    ("頑張れたというか", [], [], ['頑張れる', 'た', 'というか'])
 ])
 def test_hierarchical_extraction(sentence: str, custom_words:list[str], excluded:list[WordExclusion], expected_output: list[str]) -> None:
     insert_custom_words(custom_words)
