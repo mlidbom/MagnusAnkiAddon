8f0276580581dabbeffca0ecea9d75a5c5d126d2
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-01 03:42:29 +0200

    ensure spaces are converted.

--- src/MagnusAddonTests/tests/language_services_tests/test_kana_utils.py
+++ src/MagnusAddonTests/tests/language_services_tests/test_kana_utils.py
@@ -4,7 +4,8 @@ from sysutils import kana_utils
 
 @pytest.mark.parametrize("text,expected", [
     ("かな", "kana"),
-    ("かく", "kaku")
+    ("かく", "kaku"),
+    ("かく はく", "kaku haku")
 ])
 def test_romanization(text: str, expected: str) -> None:
     assert expected == kana_utils.romanize(text)
