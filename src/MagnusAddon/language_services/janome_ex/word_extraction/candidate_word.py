44c579fee1f1082da6e97e814c4ac2423fe6dd2c
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-05 14:00:56 +0200

    checkpoint

--- src/MagnusAddon/language_services/janome_ex/word_extraction/candidate_word.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/candidate_word.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 from language_services.janome_ex.word_extraction.display_word import BaseCandidateForm, CandidateForm, SurfaceCandidateForm
 
 if TYPE_CHECKING:
+    from language_services.janome_ex.word_extraction.text_analysis import TextAnalysis
     from language_services.janome_ex.word_extraction.text_location import TextLocation
     from note.vocabnote import VocabNote
 
@@ -12,9 +13,9 @@ class CandidateWord:
     def __init__(self, locations: list[TextLocation]):
         from ankiutils import app
         from language_services.jamdict_ex.dict_lookup import DictLookup
-        self.analysis = locations[0].analysis
-        self.locations = locations
-        self.is_custom_compound = len(locations) > 1
+        self.analysis:TextAnalysis = locations[0].analysis
+        self.locations:list[TextLocation] = locations
+        self.is_custom_compound:bool = len(locations) > 1
 
         self.surface_str:str = "".join([t.surface for t in locations]) + ""
         self.base_str:str = "".join([t.surface for t in locations[:-1]]) + locations[-1].base
@@ -24,33 +25,31 @@ class CandidateWord:
         self.surface_vocabs:list[VocabNote] = app.col().vocab.with_form(self.surface_str)
         self.base_vocabs:list[VocabNote] = app.col().vocab.with_form(self.base_str)
 
-        self.surface_is_word = self.surface_dict_lookup.found_words() or len(self.surface_vocabs) > 0
-        self.base_is_word = self.base_dict_lookup.found_words() or len(self.base_vocabs) > 0
-        self.is_word = self.surface_is_word or self.base_is_word
-        self.surface_is_excluded_by_config = self.surface_str in self.analysis.exclusions
-        self.base_is_excluded_by_config = self.base_str in self.analysis.exclusions
+        self.surface_is_word:bool = self.surface_dict_lookup.found_words() or len(self.surface_vocabs) > 0
+        self.base_is_word:bool = self.base_dict_lookup.found_words() or len(self.base_vocabs) > 0
+        self.is_word:bool = self.surface_is_word or self.base_is_word
+        self.surface_is_excluded_by_config:bool = self.surface_str in self.analysis.exclusions
+        self.base_is_excluded_by_config:bool = self.base_str in self.analysis.exclusions
 
-        self.surface = SurfaceCandidateForm(self)
-        self.base = BaseCandidateForm(self)
+        self.surface:SurfaceCandidateForm = SurfaceCandidateForm(self)
+        self.base:BaseCandidateForm = BaseCandidateForm(self)
 
         self.display_words:list[CandidateForm] = []
-        if self.base_is_valid_candidate():
-            self.display_words.append(CandidateForm(self, False))
-        if self.surface_is_valid_candidate():
-            self.display_words.append(CandidateForm(self, True))
+        if self.base.is_valid_candidate():
+            self.display_words.append(self.base)
+        if self.surface.is_valid_candidate():
+            self.display_words.append(self.surface)
 
     def surface_has_vocab(self) -> bool: return len(self.surface_vocabs) > 0
     def base_has_vocab(self) -> bool: return len(self.base_vocabs) > 0
 
-    def base_is_valid_candidate(self) -> bool: return self.base_is_word and not self.base_is_excluded_by_config
-    def surface_is_valid_candidate(self) -> bool: return self.surface_is_word and not self.surface_is_excluded_by_config
-    def has_valid_candidates(self) -> bool: return self.base_is_valid_candidate() or self.surface_is_valid_candidate()
+    def has_valid_candidates(self) -> bool: return self.base.is_valid_candidate() or self.surface.is_valid_candidate()
 
     def __repr__(self) -> str: return f"""
 CandidateWord({self.surface_str} | {self.base_str},
 hvc:{self.has_valid_candidates()},  
-bivc:{self.base_is_valid_candidate()}, 
-sivc:{self.surface_is_valid_candidate()}, 
+bivc:{self.base.is_valid_candidate()}, 
+sivc:{self.surface.is_valid_candidate()}, 
 iw:{self.is_word} 
 siw:{self.surface_is_word}, 
 biw:{self.base_is_word} 
--- src/MagnusAddon/language_services/janome_ex/word_extraction/display_word.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/display_word.py
@@ -3,26 +3,34 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from language_services.janome_ex.word_extraction.candidate_word import CandidateWord
+    from note.vocabnote import VocabNote
 
 class CandidateForm:
-    def __init__(self, candidate: CandidateWord, is_surface: bool):
+    def __init__(self, candidate: CandidateWord, is_surface: bool, form:str):
+        from ankiutils import app
+        from language_services.jamdict_ex.dict_lookup import DictLookup
+
+        self.exclusions = candidate.analysis.exclusions
         self.candidate = candidate
         self.is_surface = is_surface
-        if is_surface:
-            self.word = candidate.surface_str
-            self.vocabs = candidate.surface_vocabs
-        else:
-            self.word = candidate.base_str
-            self.vocabs = candidate.base_vocabs
+        self.form = form
+
+        self.dict_lookup:DictLookup = DictLookup.lookup_word_shallow(form)
+        self.vocabs:list[VocabNote] = app.col().vocab.with_form(form)
+
+        self.is_word:bool = self.dict_lookup.found_words() or len(self.vocabs) > 0
+        self.is_excluded_by_config:bool = form in self.exclusions
+
+    def is_valid_candidate(self) -> bool: return self.is_word and not self.is_excluded_by_config
 
     def __repr__(self) -> str:
-        return f"""{self.word}"""
+        return f"""{self.form}"""
 
 
 class SurfaceCandidateForm(CandidateForm):
     def __init__(self, candidate: CandidateWord):
-        super().__init__(candidate, True)
+        super().__init__(candidate, True, "".join([t.surface for t in candidate.locations]) + "")
 
 class BaseCandidateForm(CandidateForm):
     def __init__(self, candidate: CandidateWord):
-        super().__init__(candidate, False)
+        super().__init__(candidate, False, "".join([t.surface for t in candidate.locations[:-1]]) + candidate.locations[-1].base)
