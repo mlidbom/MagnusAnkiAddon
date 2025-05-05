f25e972a985c4970fb906dd6a2ed43dc9dbc21d0
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-05 15:48:21 +0200

    first passing test

--- src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_word_extractor_jn.py
+++ src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_word_extractor_jn.py
@@ -4,6 +4,7 @@ import pytest
 
 from fixtures.collection_factory import inject_anki_collection_with_select_data
 from language_services.janome_ex.tokenizing.jn_tokenizer import JNTokenizer
+from language_services.janome_ex.word_extraction.text_analysis import TextAnalysis
 from language_services.janome_ex.word_extraction.word_extractor import jn_extractor
 from language_services.janome_ex.word_extraction.word_exclusion import WordExclusion
 from note.vocabnote import VocabNote
@@ -81,67 +82,72 @@ def insert_custom_words(custom_words: list[str]) -> None:
         VocabNote.create(custom_word, "", [""])
     app.col().flush_cache_updates()
 
-@pytest.mark.parametrize('sentence, custom_words, excluded, expected_output', [
-    ("厳密に言えば　俺一人が友達だけど", [],
+@pytest.mark.parametrize('run_text_analysis_test_version, sentence, custom_words, excluded, expected_output', [
+    (False, "厳密に言えば　俺一人が友達だけど", [],
      [],
      ["厳密に言えば", "俺", "一人", "が", "友達", "だけど"]),
-    ("厳密に言えば　俺一人が友達だけど", [],
+    (False, "厳密に言えば　俺一人が友達だけど", [],
      [WordExclusion("厳密に言えば"), WordExclusion("言え"), WordExclusion('だけど')],
      ['厳密', 'に', '言う', 'ば', '俺', '一人', 'が', '友達', 'だ', 'けど']),
-    ("厳密に言えば　俺一人が友達だけどだけど", [],
+    (False, "厳密に言えば　俺一人が友達だけどだけど", [],
      [],
      ['厳密に言えば', '俺', '一人', 'が', '友達', 'だけど', 'だけど']),
-    ("厳密に言えばだけど俺一人が友達だけど", [],
+    (False, "厳密に言えばだけど俺一人が友達だけど", [],
      [WordExclusion("だけど", 6)],
      ['厳密に言えば', '俺', '一人', 'が', '友達', 'だけど']),
-    ("幼すぎて よく覚えていないけど", [],
+    (False, "幼すぎて よく覚えていないけど", [],
      [],
      ['幼い', 'すぎる', 'て', 'よく', '覚える', 'て', 'いる', 'ない', 'けど']
      ),
-    ("私は毎日ジョギングをすることを習慣にしています。",
+    (False, "私は毎日ジョギングをすることを習慣にしています。",
      ["してい", "ている", "にする"],
      [WordExclusion("にして", 17), WordExclusion("にし", 17), WordExclusion("して", 18), WordExclusion("し", 18), WordExclusion("してい", 18), WordExclusion("い", 12), WordExclusion("にする")],
      ['私', 'は', '毎日', 'ジョギング', 'を', 'する', 'こと', 'を', '習慣', 'に', 'する', 'ている', 'ます']),
-    ("私は毎日ジョギングをすることを習慣にしています。",
+    (False, "私は毎日ジョギングをすることを習慣にしています。",
      ["してい", "ている", "にする"],
      [WordExclusion("にして", 17), WordExclusion("にし", 17), WordExclusion("して", 18), WordExclusion("し", 18), WordExclusion("してい", 18)],
      ['私', 'は', '毎日', 'ジョギング', 'を', 'する', 'こと', 'を', '習慣', 'にする', 'ている', 'ます']),
-    ("ばら撒かれるなんて死んでもいやだ",
+    (False, "ばら撒かれるなんて死んでもいやだ",
      [],
      [],
      ['ばら撒く', 'れる', 'なんて', '死んでも', 'いや', 'だ']),
-    ("ああもう　だったら普通に金貸せって言えよ",
+    (False, "ああもう　だったら普通に金貸せって言えよ",
      [],
      [],
      ['ああ', 'もう', 'だったら', '普通に', '金貸', 'せる', 'て', '言える', '言えよ']),
-    ("お前も色々考えてるんだなぁ",
+    (False, "お前も色々考えてるんだなぁ",
      [],
      [],
      ['お前', 'も', '色々', '考える', 'てる', 'んだ', 'なぁ']),
-    ("この夏は　たくさん思い出を作れたなぁ",
+    (False, "この夏は　たくさん思い出を作れたなぁ",
      [],
      [],
      ['この', '夏', 'は', 'たくさん', '思い出', 'を', '作れる', 'た', 'なぁ']),
-    ("教科書落ちちゃうから",
+    (False, "教科書落ちちゃうから",
      [],
      [],
      ['教科書', '落ちる', 'ちゃう', 'から']),
-    ("待ってました", [], [], ['待つ', 'て', 'ます', 'た']),
-    ("怖くなくなったの", [], [], ['怖い', 'なくなる', 'た', 'の']),
-    ("落ちてないかな", [], [], ['落ちる', 'てる', 'ないか', 'な']),
-    ("分かってたら", [], [], ['分かる', 'てたら']),
-    ("頑張れたというか", [], [], ['頑張れる', 'た', 'というか']),
-    ("思い出せそうな気がする", [], [], ['思い出せる', 'そう', 'な', '気がする']),
-    ("私が頼んだの", [], [], ['私', 'が', '頼む', '頼ん', 'だ', 'の']),
-    ("いらっしゃいません", [], [WordExclusion('いらっしゃいませ')], ['いらっしゃいます', 'ん'])
+    (True, "待ってました", [], [], ['待つ', 'て', 'ます', 'た']),
+    (False, "怖くなくなったの", [], [], ['怖い', 'なくなる', 'た', 'の']),
+    (False, "落ちてないかな", [], [], ['落ちる', 'てる', 'ないか', 'な']),
+    (False, "分かってたら", [], [], ['分かる', 'てたら']),
+    (False, "頑張れたというか", [], [], ['頑張れる', 'た', 'というか']),
+    (False, "思い出せそうな気がする", [], [], ['思い出せる', 'そう', 'な', '気がする']),
+    (False, "私が頼んだの", [], [], ['私', 'が', '頼む', '頼ん', 'だ', 'の']),
+    (False, "いらっしゃいません", [], [WordExclusion('いらっしゃいませ')], ['いらっしゃいます', 'ん'])
 ])
-def test_hierarchical_extraction(sentence: str, custom_words: list[str], excluded: list[WordExclusion], expected_output: list[str]) -> None:
+def test_hierarchical_extraction(run_text_analysis_test_version:bool, sentence: str, custom_words: list[str], excluded: list[WordExclusion], expected_output: list[str]) -> None:
     print("")
     print("###### running test")
     insert_custom_words(custom_words)
     hierarchical = word_extractor.extract_words_hierarchical(sentence, excluded)
     root_words = [w.word.word for w in hierarchical]
     assert root_words == expected_output
+
+    if run_text_analysis_test_version:
+        analysis = TextAnalysis(sentence, excluded)
+        root_words = [w.form for w in analysis.display_words]
+        assert root_words == expected_output
 
 def insert_custom_words_with_excluded_forms(custom_words: list[list[str]]) -> None:
     from ankiutils import app
