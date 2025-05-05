c731bdbababa73403c9f90155691471f07d18263
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-05 19:12:56 +0200

    restore confused change

--- src/MagnusAddon/language_services/janome_ex/word_extraction/text_location.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/text_location.py
@@ -50,9 +50,7 @@ TextLocation('{self.start_index}-{self.end_index}, {self.surface} | {self.base} 
 
     def run_analysis(self) -> None:
         lookahead_max = min(_max_lookahead, len(self.forward_list(_max_lookahead)))
-        self.all_candidates = []
-        for index in range(0, lookahead_max):
-            self.all_candidates.append(CandidateWord(self.forward_list(index)))
+        self.all_candidates = [CandidateWord(self.forward_list(index)) for index in range(lookahead_max - 1, -1, -1)]
 
         self.all_candidates.sort(key=lambda cand: cand.length, reverse=True)#we need the shortest parts initialized first, that's why the reverse afterwards.
         self.word_candidates = [word for word in self.all_candidates if word.is_word]
