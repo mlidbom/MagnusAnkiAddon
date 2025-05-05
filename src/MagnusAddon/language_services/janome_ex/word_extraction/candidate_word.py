d450550b32ac25a28115a453b5e2f9692cee98d2
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-05 14:11:15 +0200

    checkpoint

--- src/MagnusAddon/language_services/janome_ex/word_extraction/candidate_word.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/candidate_word.py
@@ -5,36 +5,21 @@ from language_services.janome_ex.word_extraction.display_word import BaseCandida
 if TYPE_CHECKING:
     from language_services.janome_ex.word_extraction.text_analysis import TextAnalysis
     from language_services.janome_ex.word_extraction.text_location import TextLocation
-    from note.vocabnote import VocabNote
 
 from sysutils.ex_str import newline
 
 class CandidateWord:
     def __init__(self, locations: list[TextLocation]):
-        from ankiutils import app
-        from language_services.jamdict_ex.dict_lookup import DictLookup
-        self.analysis:TextAnalysis = locations[0].analysis
-        self.locations:list[TextLocation] = locations
-        self.is_custom_compound:bool = len(locations) > 1
+        self.analysis: TextAnalysis = locations[0].analysis
+        self.locations: list[TextLocation] = locations
+        self.is_custom_compound: bool = len(locations) > 1
 
-        self.surface_str:str = "".join([t.surface for t in locations]) + ""
-        self.base_str:str = "".join([t.surface for t in locations[:-1]]) + locations[-1].base
+        self.surface: SurfaceCandidateForm = SurfaceCandidateForm(self)
+        self.base: BaseCandidateForm = BaseCandidateForm(self)
 
-        self.surface_dict_lookup:DictLookup = DictLookup.lookup_word_shallow(self.surface_str)
-        self.base_dict_lookup:DictLookup = DictLookup.lookup_word_shallow(self.base_str)
-        self.surface_vocabs:list[VocabNote] = app.col().vocab.with_form(self.surface_str)
-        self.base_vocabs:list[VocabNote] = app.col().vocab.with_form(self.base_str)
+        self.is_word: bool = self.surface.is_word or self.base.is_word
 
-        self.surface_is_word:bool = self.surface_dict_lookup.found_words() or len(self.surface_vocabs) > 0
-        self.base_is_word:bool = self.base_dict_lookup.found_words() or len(self.base_vocabs) > 0
-        self.is_word:bool = self.surface_is_word or self.base_is_word
-        self.surface_is_excluded_by_config:bool = self.surface_str in self.analysis.exclusions
-        self.base_is_excluded_by_config:bool = self.base_str in self.analysis.exclusions
-
-        self.surface:SurfaceCandidateForm = SurfaceCandidateForm(self)
-        self.base:BaseCandidateForm = BaseCandidateForm(self)
-
-        self.display_words:list[CandidateForm] = []
+        self.display_words: list[CandidateForm] = []
         if self.base.is_valid_candidate():
             self.display_words.append(self.base)
         if self.surface.is_valid_candidate():
@@ -43,13 +28,7 @@ class CandidateWord:
     def has_valid_candidates(self) -> bool: return self.base.is_valid_candidate() or self.surface.is_valid_candidate()
 
     def __repr__(self) -> str: return f"""
-CandidateWord({self.surface_str} | {self.base_str},
+surface: {self.surface.__repr__()} | base:{self.base.__repr__()},
 hvc:{self.has_valid_candidates()},  
-bivc:{self.base.is_valid_candidate()}, 
-sivc:{self.surface.is_valid_candidate()}, 
 iw:{self.is_word} 
-siw:{self.surface.is_word}, 
-biw:{self.base.is_word} 
-sie:{self.surface.is_excluded_by_config}, 
-bie:{self.base.is_excluded_by_config} 
 icc:{self.is_custom_compound})""".replace(newline, "")
--- src/MagnusAddon/language_services/janome_ex/word_extraction/display_word.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/display_word.py
@@ -5,8 +5,10 @@ if TYPE_CHECKING:
     from language_services.janome_ex.word_extraction.candidate_word import CandidateWord
     from note.vocabnote import VocabNote
 
+from sysutils.ex_str import newline
+
 class CandidateForm:
-    def __init__(self, candidate: CandidateWord, is_surface: bool, form:str):
+    def __init__(self, candidate: CandidateWord, is_surface: bool, form: str):
         from ankiutils import app
         from language_services.jamdict_ex.dict_lookup import DictLookup
 
@@ -15,18 +17,17 @@ class CandidateForm:
         self.is_surface = is_surface
         self.form = form
 
-        self.dict_lookup:DictLookup = DictLookup.lookup_word_shallow(form)
-        self.vocabs:list[VocabNote] = app.col().vocab.with_form(form)
+        self.dict_lookup: DictLookup = DictLookup.lookup_word_shallow(form)
+        self.vocabs: list[VocabNote] = app.col().vocab.with_form(form)
 
-        self.is_word:bool = self.dict_lookup.found_words() or len(self.vocabs) > 0
-        self.is_excluded_by_config:bool = form in self.exclusions
+        self.is_word: bool = self.dict_lookup.found_words() or len(self.vocabs) > 0
+        self.is_excluded_by_config: bool = form in self.exclusions
 
     def is_valid_candidate(self) -> bool: return self.is_word and not self.is_excluded_by_config
     def has_vocabs(self) -> bool: return len(self.vocabs) > 0
 
     def __repr__(self) -> str:
-        return f"""{self.form}"""
-
+        return f"""CandidateForm: {self.form}, ivc:{self.is_valid_candidate()}, iw:{self.is_word} ie:{self.is_excluded_by_config}""".replace(newline, "")
 
 class SurfaceCandidateForm(CandidateForm):
     def __init__(self, candidate: CandidateWord):
