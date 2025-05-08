3f8289452409a527d851a7dce547eec9fa82806c
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-08 10:16:11 +0200

    polish

--- src/MagnusAddon/note/kanjinote_mnemonic_maker.py
+++ src/MagnusAddon/note/kanjinote_mnemonic_maker.py
@@ -20,21 +20,21 @@ def create_default_mnemonic(kanji_note:KanjiNote) -> str:
             return read if read.count("<read>") <= 1 else f"""<compound-reading>{read}</compound-reading>"""
 
         def try_combine_framentary_matches_into_one_reading() -> str:
-            segments_by_start_index: list[list[str]] = list()
+            segments_with_mapped_readings_by_start_index: list[list[str]] = list()
             for segment_start_index in range(reading_length):
                 candidates = [reading[segment_start_index:sub_string_length] for sub_string_length in range(segment_start_index + 1, reading_length + 1)]
-                segments_by_start_index.append([cand for cand in candidates if cand in readings_mappings])
+                segments_with_mapped_readings_by_start_index.append([cand for cand in candidates if cand in readings_mappings])
 
             def remove_dead_end_paths() -> None:
                 matches_removed = True
                 while matches_removed:
                     matches_removed = False
                     for path_index in range(reading_length):
-                        for match in segments_by_start_index[path_index]:
+                        for match in segments_with_mapped_readings_by_start_index[path_index]:
                             if not path_index + len(match) == reading_length: #this match brings us to the end of the reading
-                                if not segments_by_start_index[path_index + len(match)]: #There's nowhere to go after the end of this fragment
+                                if not segments_with_mapped_readings_by_start_index[path_index + len(match)]: #There's nowhere to go after the end of this fragment
                                     matches_removed = True
-                                    segments_by_start_index[path_index].remove(match)
+                                    segments_with_mapped_readings_by_start_index[path_index].remove(match)
 
             def find_path_with_fewest_segments() -> list[str]:
                 shortest_paths_to_position: dict[int, list[str]] = {0: []}  # Start with an empty path at position 0
@@ -49,7 +49,7 @@ def create_default_mnemonic(kanji_note:KanjiNote) -> str:
                 for index in range(reading_length):
                     if not current_index_is_reachable(): continue
 
-                    segments_starting_at_current_index = segments_by_start_index[index]
+                    segments_starting_at_current_index = segments_with_mapped_readings_by_start_index[index]
                     for segment in segments_starting_at_current_index:
                         position_after_segment:int = index + len(segment)
                         if segment_is_shortest_path_to_position_after_segment():
