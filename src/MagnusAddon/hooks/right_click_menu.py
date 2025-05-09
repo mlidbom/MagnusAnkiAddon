19ce728b0c8e468cd7f434ac343856997b87c6a2
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-09 16:04:04 +0200

    checkpoint

--- src/MagnusAddon/hooks/right_click_menu.py
+++ src/MagnusAddon/hooks/right_click_menu.py
@@ -78,9 +78,9 @@ def build_string_menu(menu: QMenu, string: str, string_note_menu_factory: Callab
                 build_universal_note_actions_menu(non_optional(note_menu.addMenu(shortcutfinger.home3("Kanji Actions"))), kanjis[0])
 
     string_note_menu_factory(non_optional(menu.addMenu(shortcutfinger.home1("Current note actions"))), string)
-    build_matching_note_menu(shortcutfinger.home2("Exactly matching notes"), menu, string)
-    build_open_in_anki_menu(non_optional(menu.addMenu(shortcutfinger.home3("Open in Anki"))), lambda menu_string_=string: menu_string_)  # type: ignore
-    build_web_search_menu(non_optional(menu.addMenu(shortcutfinger.home4("Search Web"))), lambda menu_string_=string: menu_string_)  # type: ignore
+    build_open_in_anki_menu(non_optional(menu.addMenu(shortcutfinger.home2("Open in Anki"))), lambda menu_string_=string: menu_string_)  # type: ignore
+    build_web_search_menu(non_optional(menu.addMenu(shortcutfinger.home3("Search Web"))), lambda menu_string_=string: menu_string_)  # type: ignore
+    build_matching_note_menu(shortcutfinger.home4("Exactly matching notes"), menu, string)
     build_create_note_menu(non_optional(menu.addMenu(shortcutfinger.up1(f"Create: {string[:40]}"))), string)
     add_ui_action(menu, shortcutfinger.down1("remove from sentence exclusions"), lambda _string=string: local_note_updater.clean_sentence_excluded_word(_string))  # type: ignore
 
