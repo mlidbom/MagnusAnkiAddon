2a9e27c67ff7b166182e6f0301ada3cf10bf98a7
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-09 15:45:38 +0200

    checkpoint

--- src/MagnusAddon/hooks/right_click_menu.py
+++ src/MagnusAddon/hooks/right_click_menu.py
@@ -79,12 +79,12 @@ def build_string_menu(menu: QMenu, string: str, string_note_menu_factory: Callab
             if any(kanjis):
                 build_universal_note_actions_menu(non_optional(note_menu.addMenu(shortcutfinger.home3("Kanji Actions"))), kanjis[0])
 
-    string_note_menu_factory(non_optional(menu.addMenu(shortcutfinger.home2("Open in Anki"))), string)
-    build_matching_note_menu(shortcutfinger.home1("Exactly matching notes"), menu, string)
-    build_open_in_anki_menu(non_optional(menu.addMenu(shortcutfinger.home2("Open in Anki"))), lambda menu_string_=string: menu_string_)  # type: ignore
-    build_web_search_menu(non_optional(menu.addMenu(shortcutfinger.home3("Search Web"))), lambda menu_string_=string: menu_string_)  # type: ignore
-    build_create_note_menu(non_optional(menu.addMenu(shortcutfinger.home4(f"Create: {string}"))), string)
-    add_ui_action(menu, shortcutfinger.up1("remove from sentence exclusions"), lambda _string=string: local_note_updater.clean_sentence_excluded_word(_string))  # type: ignore
+    string_note_menu_factory(non_optional(menu.addMenu(shortcutfinger.home2("Note actions"))), string)
+    build_matching_note_menu(shortcutfinger.home2("Exactly matching notes"), menu, string)
+    build_open_in_anki_menu(non_optional(menu.addMenu(shortcutfinger.home3("Open in Anki"))), lambda menu_string_=string: menu_string_)  # type: ignore
+    build_web_search_menu(non_optional(menu.addMenu(shortcutfinger.home4("Search Web"))), lambda menu_string_=string: menu_string_)  # type: ignore
+    build_create_note_menu(non_optional(menu.addMenu(shortcutfinger.up1(f"Create: {string}"))), string)
+    add_ui_action(menu, shortcutfinger.down1("remove from sentence exclusions"), lambda _string=string: local_note_updater.clean_sentence_excluded_word(_string))  # type: ignore
 
 def build_universal_note_actions_menu(note_actions_menu: QMenu, note: JPNote) -> None:
     note_actions_menu.addAction(shortcutfinger.home1("Open"), search_executor.lookup_promise(lambda: query_builder.notes_lookup([note])))
