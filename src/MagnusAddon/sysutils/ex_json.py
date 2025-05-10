74e2a34bfa3b1d5d3df1414feb817423eb27c1eb
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-10 05:13:46 +0200

    checkpoint

--- src/MagnusAddon/sysutils/ex_json.py
+++ src/MagnusAddon/sysutils/ex_json.py
@@ -6,7 +6,7 @@ from typing import Any, Optional
 from sysutils import typed
 
 def dict_to_json(object_dict: dict[str, Any]) -> str:
-    return json.dumps(object_dict, sort_keys=True, indent=3, ensure_ascii=False)
+    return json.dumps(object_dict, ensure_ascii=False)
 
 def json_to_dict(json_str: str) -> JsonDictReader:
     return JsonDictReader(typed.checked_cast_generics(dict[str, Any], json.loads(json_str)))
