8ee6d303832fc4fae5b4e1f05aca7506e36886b9
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-13 15:43:44 +0200

    Use on global anki imports in app.py, thus making it possible to import it globally easily in other files, reducing boilerplate

--- src/MagnusAddon/ankiutils/app.py
+++ src/MagnusAddon/ankiutils/app.py
@@ -3,24 +3,18 @@ from __future__ import annotations
 import gc
 from typing import TYPE_CHECKING, Callable, Optional
 
-import anki
-from anki.collection import Collection
-from anki.scheduler.v3 import Scheduler
-from anki_extentions.config_manager_ex import ConfigManagerEx
-from ankiutils import anki_module_import_issues_fix_just_import_this_module_before_any_other_anki_modules  # noqa
-from ankiutils.ui_utils import UIUtils
-from aqt import AnkiQt, gui_hooks, mw  # type: ignore
-from note.collection.jp_collection import JPCollection
 from sysutils.lazy import BackgroundInitialingLazy
-from sysutils.timeutil import StopWatch
 from sysutils.typed import checked_cast, non_optional
 
 if TYPE_CHECKING:
+    from anki.collection import Collection
     from anki.dbproxy import DBProxy
+    from anki.scheduler.v3 import Scheduler
+    from anki_extentions.config_manager_ex import ConfigManagerEx
     from ankiutils.ui_utils_interface import IUIUtils
+    from aqt import AnkiQt
     from configuration.configuration_value import JapaneseConfig
-
-
+    from note.collection.jp_collection import JPCollection
 
 _collection: Optional[BackgroundInitialingLazy[JPCollection]] = None
 
@@ -41,6 +35,8 @@ def config() -> JapaneseConfig:
 
 
 def _init(_col:Optional[object] = None, delay_seconds: float = 0) -> None:
+    from aqt import mw
+    from note.collection.jp_collection import JPCollection
     global _collection
     if _collection and not _collection.try_cancel_scheduled_init():
         return
@@ -71,6 +67,7 @@ def _collection_is_being_invalidated(_col:Optional[object] = None) -> None:
     reset(delay_seconds=9999) #Unless forced by the user we don't actually want to run an initialization here
 
 def _profile_closing() -> None:
+    from aqt import gui_hooks
     gui_hooks.sync_will_start.remove(_collection_is_being_invalidated)
     _collection_closed_hooks.remove(_destruct)
     gui_hooks.sync_did_finish.remove(_init)
@@ -78,33 +75,52 @@ def _profile_closing() -> None:
     _destruct()
 
 def _profile_opened() -> None:
+    from aqt import gui_hooks
     gui_hooks.sync_will_start.append(_collection_is_being_invalidated)
     _collection_closed_hooks.add(_destruct)
     gui_hooks.sync_did_finish.append(_init)
     gui_hooks.collection_did_load.append(_reset)
     _init(delay_seconds=1.0)
 
-def wait_for_initialization() -> None:
-    col()
-
 def anki_config() -> ConfigManagerEx:
+    from anki_extentions.config_manager_ex import ConfigManagerEx
+    from aqt import mw
     return ConfigManagerEx(mw.col.conf)
 
 def col() -> JPCollection:
+    from sysutils.timeutil import StopWatch
     with StopWatch.log_warning_if_slower_than(0.01, "waiting_for_initialization"):
         assert _collection
         return _collection.instance()
 
 def anki_collection() -> Collection: return col().anki_collection
 def anki_db() -> DBProxy: return non_optional(col().anki_collection.db)
-def anki_scheduler() -> Scheduler: return checked_cast(Scheduler, col().anki_collection.sched)
-def main_window() -> AnkiQt: return non_optional(mw)
-def get_ui_utils() -> IUIUtils: return UIUtils(main_window())
+def anki_scheduler() -> Scheduler:
+    from anki.scheduler.v3 import Scheduler
+    return checked_cast(Scheduler, col().anki_collection.sched)
+
+def main_window() -> AnkiQt:
+    from aqt import mw
+    return non_optional(mw)
+
+def get_ui_utils() -> IUIUtils:
+    from ankiutils.ui_utils import UIUtils
+    return UIUtils(main_window())
 
 def _collection_closed(_collection: Collection, _downgrade: bool = False) -> None:
     for hook in _collection_closed_hooks:
         hook()
 
-Collection.close = anki.hooks.wrap(Collection.close, _collection_closed, "before")  # type: ignore
-gui_hooks.profile_will_close.append(_profile_closing)
-gui_hooks.profile_did_open.append(_profile_opened)
+def _wrap_collection_close() -> None:
+    import anki
+    from anki.collection import Collection
+    Collection.close = anki.hooks.wrap(Collection.close, _collection_closed, "before")
+
+_wrap_collection_close()
+
+def _setup_gui_hooks() -> None:
+    from aqt import gui_hooks
+    gui_hooks.profile_will_close.append(_profile_closing)
+    gui_hooks.profile_did_open.append(_profile_opened)
+
+_setup_gui_hooks()
