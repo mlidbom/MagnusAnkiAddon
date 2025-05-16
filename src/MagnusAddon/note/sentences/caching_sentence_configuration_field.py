5e925ae0cc65a905104a0cb3c47507fe6c04794a
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-16 23:40:56 +0200

    cruft

--- src/MagnusAddon/note/sentences/caching_sentence_configuration_field.py
+++ src/MagnusAddon/note/sentences/caching_sentence_configuration_field.py
@@ -6,11 +6,11 @@ from note.note_constants import SentenceNoteFields
 from note.notefields.string_field import StringField
 from note.sentences.sentence_configuration import SentenceConfiguration
 from sysutils.lazy import Lazy
+from sysutils.weak_ref import WeakRef
 
 if TYPE_CHECKING:
     from note.sentences.sentencenote import SentenceNote
     from note.sentences.word_exclusion_set import WordExclusionSet
-    from sysutils.weak_ref import WeakRef
 
 class CachingSentenceConfigurationField:
     def __init__(self, sentence: WeakRef[SentenceNote]) -> None:
