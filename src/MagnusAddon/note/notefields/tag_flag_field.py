5aed4c3e321d5befd512647112e331df0d572ea9
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-16 22:57:54 +0200

    Oops. this should be a WeakRef

--- src/MagnusAddon/note/notefields/tag_flag_field.py
+++ src/MagnusAddon/note/notefields/tag_flag_field.py
@@ -3,19 +3,20 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from sysutils.object_instance_tracker import ObjectInstanceTracker
+from sysutils.weak_ref import WeakRef
 
 if TYPE_CHECKING:
     from note.jpnote import JPNote
 
 class TagFlagField:
-    def __init__(self, note: JPNote, tag: str) -> None:
+    def __init__(self, note: WeakRef[JPNote], tag: str) -> None:
         self._instance_tracker: ObjectInstanceTracker = ObjectInstanceTracker(self.__class__)
         self._note = note
         self.tag = tag
 
     @property
-    def is_set(self) -> bool: return self._note.has_tag(self.tag)
+    def is_set(self) -> bool: return self._note().has_tag(self.tag)
 
     def set_to(self, set_: bool) -> None:
-        if set_: self._note.set_tag(self.tag)
-        else: self._note.remove_tag(self.tag)
+        if set_: self._note().set_tag(self.tag)
+        else: self._note().remove_tag(self.tag)
--- src/MagnusAddon/note/vocabulary/vocabnote_flags.py
+++ src/MagnusAddon/note/vocabulary/vocabnote_flags.py
@@ -13,9 +13,9 @@ if TYPE_CHECKING:
 class VocabNoteFlags:
     def __init__(self, vocab: WeakRef[VocabNote]) -> None:
         self._vocab: WeakRef[VocabNote] = vocab
-        self.requires_exact_match: TagFlagField = TagFlagField(vocab(), Mine.Tags.requires_exact_match)
-        self.question_overrides_form: TagFlagField = TagFlagField(vocab(),  Mine.Tags.question_overrides_form)
-        self.requires_a_stem: TagFlagField = TagFlagField(vocab(),  Mine.Tags.requires_a_stem)
-        self.requires_e_stem: TagFlagField = TagFlagField(vocab(),  Mine.Tags.requires_e_stem)
-        self.match_with_preceding_character: TagFlagField = TagFlagField(vocab(), Mine.Tags.match_with_preceding_character)
-        self.match_with_preceding_vowel: TagFlagField = TagFlagField(vocab(), Mine.Tags.match_with_preceding_vowel)
+        self.requires_exact_match: TagFlagField = TagFlagField(vocab, Mine.Tags.requires_exact_match)
+        self.question_overrides_form: TagFlagField = TagFlagField(vocab,  Mine.Tags.question_overrides_form)
+        self.requires_a_stem: TagFlagField = TagFlagField(vocab,  Mine.Tags.requires_a_stem)
+        self.requires_e_stem: TagFlagField = TagFlagField(vocab,  Mine.Tags.requires_e_stem)
+        self.match_with_preceding_character: TagFlagField = TagFlagField(vocab, Mine.Tags.match_with_preceding_character)
+        self.match_with_preceding_vowel: TagFlagField = TagFlagField(vocab, Mine.Tags.match_with_preceding_vowel)
