de793aaf30cafbc271e3129dae4b20134edd779f
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-22 00:33:24 +0200

    checkpoint

--- src/MagnusAddonTests/tests/batches_tests/test_local_note_updater_smoke_tests_only.py
+++ src/MagnusAddonTests/tests/batches_tests/test_local_note_updater_smoke_tests_only.py
@@ -3,17 +3,11 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import pytest
-
-# from ankiutils import app
 from fixtures.collection_factory import inject_anki_collection_with_all_sample_data
 from fixtures.stub_factory import stub_ui_dependencies
 
 if TYPE_CHECKING:
     from collections.abc import Generator
-
-# from language_services.janome_ex.word_extraction.candidate_word import CandidateWord
-# from language_services.janome_ex.word_extraction.text_analysis import TextAnalysis
-# from note.vocabnote import VocabNote
 
 # noinspection PyUnusedFunction
 @pytest.fixture(scope="function")
@@ -21,11 +15,6 @@ def setup() -> Generator[None, None, None]:
     with (stub_ui_dependencies(), inject_anki_collection_with_all_sample_data()):
         yield
 
-
-def test_smoke_update_all(setup:object) -> None:
+def test_smoke_full_rebuild(setup: object) -> None:
     from batches import local_note_updater
-    local_note_updater.update_all()
-
-
-def test_memory_leak(setup:object) -> None:
-    pass
+    local_note_updater.full_rebuild()
