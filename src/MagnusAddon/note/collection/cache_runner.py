a0c923fa00c8c9a929452cf1d8652ba76ff330aa
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-05-23 11:32:28 +0200

    Whoops, without the dedicated thread, we do need some synchronization....

--- src/MagnusAddon/note/collection/cache_runner.py
+++ src/MagnusAddon/note/collection/cache_runner.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import threading
 import time
 from typing import TYPE_CHECKING, Callable, cast
 
@@ -19,7 +20,6 @@ if TYPE_CHECKING:
     from anki.decks import DeckId
     from anki.notes import Note, NoteId
 
-
 class CacheRunner(Slots):
     def __init__(self, anki_collection: Collection) -> None:
         self._pause_data_generation: bool = False
@@ -31,6 +31,7 @@ class CacheRunner(Slots):
         self._destructors: list[Callable[[], None]] = []
         self._anki_collection: Collection = anki_collection
         self._running: bool = False
+        self._lock = threading.RLock()
 
         model_manager: ModelManager = anki_collection.models
         all_note_types: list[NoteTypeEx] = [NoteTypeEx.from_dict(model) for model in model_manager.all()]
@@ -42,27 +43,29 @@ class CacheRunner(Slots):
         hooks.note_will_flush.append(self._on_will_flush)
 
     def start(self) -> None:
-        assert not self._running
-        self._running = True
-        app_thread_pool.pool.submit(self._check_for_changed_schemas)
+        with self._lock:
+            assert not self._running
+            self._running = True
+            app_thread_pool.pool.submit(self._run_periodic_flushes)
 
-    def _check_for_changed_schemas(self) -> None:
+    def _run_periodic_flushes(self) -> None:
         while self._running:
-            self._check_for_updated_note_types_and_reset_app_if_found()
+            self.flush_updates()
             time.sleep(0.1)
 
     def destruct(self) -> None:
-        self._running = False
-        self._internal_flush_updates()
+        with self._lock:
+            self._running = False
+            self._internal_flush_updates()
 
-        hooks.notes_will_be_deleted.remove(self._on_will_be_removed)
-        hooks.note_will_be_added.remove(self._on_will_be_added)
-        hooks.note_will_flush.remove(self._on_will_flush)
+            hooks.notes_will_be_deleted.remove(self._on_will_be_removed)
+            hooks.note_will_be_added.remove(self._on_will_be_added)
+            hooks.note_will_flush.remove(self._on_will_flush)
 
-        for destructor in self._destructors: destructor()
+            for destructor in self._destructors: destructor()
 
-        from note import noteutils
-        noteutils.clear_studying_cache()
+            from note import noteutils
+            noteutils.clear_studying_cache()
 
     def _internal_flush_updates(self) -> None:
         for subscriber in self._merge_pending_subscribers: subscriber()
@@ -71,39 +74,51 @@ class CacheRunner(Slots):
         for callback in self._generate_data_subscribers: callback()
 
     def flush_updates(self) -> None:
-        self._internal_flush_updates()
+        with self._lock:
+            self._check_for_updated_note_types_and_reset_app_if_found()
+            self._internal_flush_updates()
 
     def _on_will_be_added(self, _collection: Collection, backend_note: Note, _deck_id: DeckId) -> None:
-        for subscriber in self._will_add_subscribers: subscriber(backend_note)
+        with self._lock:
+            for subscriber in self._will_add_subscribers: subscriber(backend_note)
 
     def _on_will_flush(self, backend_note: Note) -> None:
-        for subscriber in self._will_flush_subscribers: subscriber(backend_note)
+        with self._lock:
+            for subscriber in self._will_flush_subscribers: subscriber(backend_note)
 
     def _on_will_be_removed(self, _: Collection, note_ids: Sequence[NoteId]) -> None:
-        for subscriber in self._will_remove_subscribers: subscriber(note_ids)
+        with self._lock:
+            for subscriber in self._will_remove_subscribers: subscriber(note_ids)
 
     def pause_data_generation(self) -> None:
-        assert not self._pause_data_generation
-        self._pause_data_generation = True
+        with self._lock:
+            assert not self._pause_data_generation
+            self._pause_data_generation = True
 
     def resume_data_generation(self) -> None:
-        assert self._pause_data_generation
-        self._pause_data_generation = False
+        with self._lock:
+            assert self._pause_data_generation
+            self._pause_data_generation = False
 
     def connect_generate_data_timer(self, flush_updates: Callable[[], None]) -> None:
-        self._generate_data_subscribers.append(flush_updates)
+        with self._lock:
+            self._generate_data_subscribers.append(flush_updates)
 
     def connect_merge_pending_adds(self, _merge_pending_added_notes: Callable[[], None]) -> None:
-        self._merge_pending_subscribers.append(_merge_pending_added_notes)
+        with self._lock:
+            self._merge_pending_subscribers.append(_merge_pending_added_notes)
 
     def connect_will_add(self, _merge_pending_added_notes: Callable[[Note], None]) -> None:
-        self._will_add_subscribers.append(_merge_pending_added_notes)
+        with self._lock:
+            self._will_add_subscribers.append(_merge_pending_added_notes)
 
     def connect_will_flush(self, _merge_pending_added_notes: Callable[[Note], None]) -> None:
-        self._will_flush_subscribers.append(_merge_pending_added_notes)
+        with self._lock:
+            self._will_flush_subscribers.append(_merge_pending_added_notes)
 
     def connect_will_remove(self, _merge_pending_added_notes: Callable[[Sequence[NoteId]], None]) -> None:
-        self._will_remove_subscribers.append(_merge_pending_added_notes)
+        with self._lock:
+            self._will_remove_subscribers.append(_merge_pending_added_notes)
 
     def _check_for_updated_note_types_and_reset_app_if_found(self) -> None:
         for cached_note_type in self._note_types:
@@ -112,4 +127,4 @@ class CacheRunner(Slots):
             try:
                 current.assert_schema_matches(cached_note_type)
             except AssertionError:
-                app_thread_pool.pool.submit(lambda: app.reset())#We are running on the thread that will be killed by the reset...
+                app_thread_pool.pool.submit(lambda: app.reset())  # We are running on the thread that will be killed by the reset...
