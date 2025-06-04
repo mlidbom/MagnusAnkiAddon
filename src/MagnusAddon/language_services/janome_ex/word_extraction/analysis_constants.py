e71f0b19c033ef6b6d161cfcfb869c3d90b8daa2
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-06-04 22:18:13 +0200

    add ～ to noise characters

--- src/MagnusAddon/language_services/janome_ex/word_extraction/analysis_constants.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/analysis_constants.py
@@ -2,5 +2,5 @@ from __future__ import annotations
 
 from sysutils import ex_str
 
-noise_characters = {".", ",", ":", ";", "/", "|", "。", "、", "?", "!", ex_str.invisible_space}
+noise_characters = {".", ",", ":", ";", "/", "|", "。", "、", "?", "!", "～", ex_str.invisible_space}
 non_word_characters = noise_characters | {" ", "\t"}
