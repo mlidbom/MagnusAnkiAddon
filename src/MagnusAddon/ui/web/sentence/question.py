38bd0b73b27ba376527e1c9f4d96579a6b7f77f0
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-06-07 18:02:16 +0200

    and let's make it really visible

--- src/MagnusAddon/ui/web/sentence/question.py
+++ src/MagnusAddon/ui/web/sentence/question.py
@@ -7,15 +7,16 @@ from note.sentences.sentencenote import SentenceNote
 from ui.web.web_utils.content_renderer import PrerenderingAnswerContentRenderer
 
 
+def render_wbr(question: str) -> str:
+    return question.replace(SentenceQuestionField.word_break_tag, "<span class='wbr_tag'>&lt;wbr&gt;</span>") \
+        if app.config().show_sentence_breakdown_in_edit_mode.get_value() \
+        else question
+
 def render_user_question(note: SentenceNote) -> str:
-    return note.user.question.get_raw() \
-        if not app.config().show_sentence_breakdown_in_edit_mode.get_value() \
-        else note.user.question.get_raw().replace(SentenceQuestionField.word_break_tag, "&lt;wbr&gt;")
+    return render_wbr(note.user.question.get_raw())
 
 def render_source_question(note: SentenceNote) -> str:
-    return note.source_question.get_raw() \
-        if not app.config().show_sentence_breakdown_in_edit_mode.get_value() \
-        else note.source_question.get_raw().replace(SentenceQuestionField.word_break_tag, "&lt;wbr&gt;")
+    return render_wbr(note.source_question.get_raw())
 
 def init() -> None:
     gui_hooks.card_will_show.append(PrerenderingAnswerContentRenderer(SentenceNote, {
