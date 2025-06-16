783f9fc9a5937abe283bafe8b8fe7477fd3a8cb2
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-06-16 12:45:05 +0200

    let's actually implement forbids_sentence_end

--- src/MagnusAddon/language_services/janome_ex/word_extraction/tail_requirements.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/tail_requirements.py
@@ -18,11 +18,15 @@ class TailRequirements(Slots):
         self.fulfills_requires_sentence_end_requirement = (not self.config.sentence_end.is_required
                                                            or (tail is None or tail().token.is_non_word_character or tail().token.surface in _quote_characters))
 
-        self.are_fulfilled = self.fulfills_requires_sentence_end_requirement
+        self.fulfills_forbids_sentence_end_requirement = (not self.config.sentence_end.is_forbidden
+                                                          or (tail is not None and not tail().token.is_non_word_character and tail().token.surface not in _quote_characters))
+
+        self.are_fulfilled = self.fulfills_requires_sentence_end_requirement and self.fulfills_forbids_sentence_end_requirement
 
     def failure_reasons(self) -> set[str]:
         return (SimpleStringListBuilder()
                 .append_if(not self.fulfills_requires_sentence_end_requirement, "requires_sentence_end")
+                .append_if(not self.fulfills_forbids_sentence_end_requirement, "forbids_sentence_end")
                 .as_set())
 
     def __repr__(self) -> str: return " ".join(self.failure_reasons())
