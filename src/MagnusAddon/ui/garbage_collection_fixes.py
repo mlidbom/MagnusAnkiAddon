fb99e362cf9222770eea2f8bae8ad41bfc8c6d13
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-08-01 09:30:26 +0200

    pop up a message when garbage collection runs.

--- src/MagnusAddon/ui/garbage_collection_fixes.py
+++ src/MagnusAddon/ui/garbage_collection_fixes.py
@@ -1,10 +1,19 @@
+import gc
+
 import aqt
 from PyQt6.QtWidgets import QDialog
 from aqt import qconnect
+
+from ankiutils import app
 
 def noop_gc_on_dialog_finish(self:aqt.main.AnkiQt, dialog: QDialog) -> None:
     # Fix anki hanging for several seconds every time a window closes
     qconnect(dialog.finished, lambda: dialog.deleteLater())
 
+def visible_garbage_collection(self:aqt.main.AnkiQt) -> None:
+    app.get_ui_utils().tool_tip("running garbage collection")
+    gc.collect()
+
 def init() -> None:
     aqt.main.AnkiQt.garbage_collect_on_dialog_finish = noop_gc_on_dialog_finish  # type: ignore
+    aqt.main.AnkiQt.garbage_collect_now = visible_garbage_collection  # type: ignore
