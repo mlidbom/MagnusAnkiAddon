db0737f74abc8e1955a5cafca1d3470650957a33
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-05 06:38:28 +0200

    checkpoint

--- src/MagnusAddon/language_services/janome_ex/word_extraction/head_requirements.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/head_requirements.py
@@ -17,7 +17,7 @@ class HeadRequirements(Slots):
         rules = vocab.matching_rules
         self.config = rules
 
-        self.fulfills_requires_prefix = not rules.is_strictly_suffix or end_of_stem is not None
+        self.fulfills_is_strictly_suffix = not rules.is_strictly_suffix.is_set() or end_of_stem is not None
 
         self.has_a_stem = end_of_stem is not None and end_of_stem().token.surface[-1] in conjugator.a_stem_characters
         self.fulfills_forbids_a_stem_requirement = not rules.a_stem.is_forbidden or not self.has_a_stem
@@ -30,13 +30,15 @@ class HeadRequirements(Slots):
         self.fulfills_requires_e_stem_requirement = not rules.e_stem.is_required or self.has_e_stem
         self.fulfills_forbids_e_stem_requirement = not rules.e_stem.is_forbidden or not self.has_e_stem
 
-        self.are_fulfilled = (self.fulfills_forbids_a_stem_requirement
+        self.are_fulfilled = (self.fulfills_is_strictly_suffix
+                              and self.fulfills_forbids_a_stem_requirement
                               and self.fulfills_requires_a_stem
                               and self.fulfills_requires_e_stem_requirement
                               and self.fulfills_forbids_e_stem_requirement)
 
     def failure_reasons(self) -> set[str]:
         return (SimpleStringListBuilder()
+                .append_if(not self.fulfills_is_strictly_suffix, "is_strictly_suffix")
                 .append_if(not self.fulfills_forbids_a_stem_requirement, "forbids_a_stem")
                 .append_if(not self.fulfills_requires_a_stem, "requires_a_stem")
                 .append_if(not self.fulfills_forbids_e_stem_requirement, "forbids_e_stem")
