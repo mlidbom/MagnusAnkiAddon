0d89d001a1e6324d895a618eab4942d7a903abee
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-09 12:04:42 +0200

    make it clear that it is not OK to change the value

--- src/MagnusAddon/note/notefields/auto_save_wrappers/set_wrapper.py
+++ src/MagnusAddon/note/notefields/auto_save_wrappers/set_wrapper.py
@@ -11,30 +11,30 @@ TValue = TypeVar("TValue")
 
 class FieldSetWrapper(Generic[TValue], Slots):
     _secret = "aoeulrcaboeusthb"
-    def __init__(self, save_callback: Callable[[], None], value: set[TValue], secret: str) -> None:
+    def __init__(self, save_callback: Callable[[], None], value: Callable[[], set[TValue]], secret: str) -> None:
         if FieldSetWrapper._secret != secret: raise ValueError("use the factory methods, not this private constructor")
         self._save: Callable[[], None] = save_callback
-        self._value: set[TValue] = value #never replace _value or the save method will stop working...
+        self._value: Callable[[], set[TValue]] = value #never replace _value or the save method will stop working...
 
-    def get(self) -> set[TValue]: return self._value
+    def get(self) -> set[TValue]: return self._value()
 
     def add(self, value: TValue) -> None:
-        self._value.add(value)
+        self._value().add(value)
         self._save()
 
     def remove(self, key: TValue) -> None:
-        self._value.remove(key)
+        self._value().remove(key)
         self._save()
 
     def overwrite_with(self, other: FieldSetWrapper[TValue]) -> None:
-        self._value.clear()
-        self._value.update(other.get())
+        self._value().clear()
+        self._value().update(other.get())
         self._save()
 
-    def is_empty(self) -> bool: return len(self._value) == 0
+    def is_empty(self) -> bool: return len(self._value()) == 0
 
     @classmethod
     def for_json_object_field(cls, field: JsonObjectField[Any], value: set[TValue]) -> FieldSetWrapper[TValue]:
-        return cls(lambda: field.save(), value, FieldSetWrapper._secret)
+        return cls(lambda: field.save(), lambda: value, FieldSetWrapper._secret)
 
-    def __repr__(self) -> str: return self._value.__repr__() if self._value else "{}"
+    def __repr__(self) -> str: return self._value().__repr__() if self._value() else "{}"
