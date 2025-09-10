eddc378778b9487ecdf4b27341dee33852da56c0
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-10 09:57:09 +0200

    make note actions available via the string menu

--- src/MagnusAddon/ui/menus/common.py
+++ src/MagnusAddon/ui/menus/common.py
@@ -92,15 +92,24 @@ def build_string_menu(menu: QMenu, string: str, string_note_menu_factory: typing
     build_create_note_menu(non_optional(menu.addMenu(shortcutfinger.up1(f"Create: {string[:40]}"))), string)
     add_ui_action(menu, shortcutfinger.down1("Reparse matching sentences"), lambda: local_note_updater.reparse_matching_sentences(string))
 
-def build_universal_note_actions_menu(note_actions_menu: QMenu, note: JPNote | None) -> None:
+def build_universal_note_actions_menu(universal_actions_menu: QMenu, note: JPNote | None) -> None:
     if not note: return
 
-    note_actions_menu.addAction(shortcutfinger.home1("Open in previewer"), search_executor.lookup_and_show_previewer_promise(lambda: query_builder.notes_lookup([note])))
-    note_actions_menu.addAction(shortcutfinger.home2("Find in browser"), search_executor.lookup_promise(lambda: query_builder.notes_lookup([note])))
+    universal_actions_menu.addAction(shortcutfinger.home1("Open in previewer"), search_executor.lookup_and_show_previewer_promise(lambda: query_builder.notes_lookup([note])))
+    universal_actions_menu.addAction(shortcutfinger.home2("Find in browser"), search_executor.lookup_promise(lambda: query_builder.notes_lookup([note])))
 
-    add_ui_action(note_actions_menu, shortcutfinger.home3("Unsuspend all cards"), note.unsuspend_all_cards, note.has_suspended_cards())
-    add_ui_action(note_actions_menu, shortcutfinger.home4("Suspend all cards"), note.suspend_all_cards, note.has_active_cards())
-    add_ui_action(note_actions_menu, shortcutfinger.up1("Unsuspend all cards and dependencies' cards"), note.unsuspend_all_cards_and_dependencies, confirm=True, enabled=note.has_suspended_cards_or_depencies_suspended_cards())
+    add_ui_action(universal_actions_menu, shortcutfinger.home3("Unsuspend all cards"), note.unsuspend_all_cards, note.has_suspended_cards())
+    add_ui_action(universal_actions_menu, shortcutfinger.home4("Suspend all cards"), note.suspend_all_cards, note.has_active_cards())
+    add_ui_action(universal_actions_menu, shortcutfinger.up1("Unsuspend all cards and dependencies' cards"), note.unsuspend_all_cards_and_dependencies, confirm=True, enabled=note.has_suspended_cards_or_depencies_suspended_cards())
+
+    note_actions_menu = non_optional(universal_actions_menu.addMenu(shortcutfinger.home5("Note actions")))
+    if note:
+        if isinstance(note, KanjiNote):
+            menus.notes.kanji.main.build_note_menu(note_actions_menu, note)
+        elif isinstance(note, VocabNote):
+            menus.notes.vocab.main.setup_note_menu(note_actions_menu, note, "TODO:selection", "TODO:clipboard")
+        elif isinstance(note, SentenceNote):
+            menus.notes.sentence.main.build_note_menu(note_actions_menu, note)
 
 def null_op_factory(_menu: QMenu, _string: str) -> None:
     pass
