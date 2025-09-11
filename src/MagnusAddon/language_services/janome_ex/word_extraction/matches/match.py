be823ee369d1f266bc8f7fdaacfee2299e04b209
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-11 07:52:43 +0200

    checkpoint

--- src/MagnusAddon/language_services/janome_ex/word_extraction/matches/match.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/matches/match.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING
 
 from autoslot import Slots
 from sysutils.simple_string_list_builder import SimpleStringListBuilder
@@ -8,13 +8,13 @@ from sysutils.weak_ref import WeakRefable
 
 if TYPE_CHECKING:
     from language_services.janome_ex.word_extraction.candidate_word import CandidateWord
-    from language_services.janome_ex.word_extraction.candidate_word_variant import CandidateWordBaseVariant, CandidateWordVariant
+    from language_services.janome_ex.word_extraction.candidate_word_variant import CandidateWordVariant
     from note.vocabulary.vocabnote_matching_rules import VocabNoteMatching
     from sysutils.weak_ref import WeakRef
 
 class Match(WeakRefable, Slots):
     def __init__(self, word_variant: WeakRef[CandidateWordVariant], rules: VocabNoteMatching | None) -> None:
-        self.variant: WeakRef[CandidateWordVariant] = word_variant
+        self._variant: WeakRef[CandidateWordVariant] = word_variant
         self.tokenized_form: str = word_variant().form
         self.parsed_form: str = self.tokenized_form
         self.match_form: str = self.tokenized_form
@@ -24,64 +24,52 @@ class Match(WeakRefable, Slots):
         self.is_configured_hidden = word_variant().configuration.hidden_matches.excludes_at_index(self.tokenized_form, word_variant().start_index)
         self.is_configured_incorrect = word_variant().configuration.incorrect_matches.excludes_at_index(self.tokenized_form, word_variant().start_index)
 
-    @property
-    def is_valid(self) -> bool:
-        return self._is_valid_internal or self.is_highlighted
 
     @property
-    def surface_form(self) -> str: return self.variant().word().surface.form
+    def variant(self) -> CandidateWordVariant: return self._variant()
 
     @property
-    def _is_valid_internal(self) -> bool:
-        return not self.is_configured_incorrect
-
+    def is_valid(self) -> bool: return self._is_valid_internal or self.is_highlighted
     @property
-    def is_highlighted(self) -> bool: return self.match_form in self.variant().configuration.highlighted_words
-
+    def surface_form(self) -> str: return self.variant.word().surface.form
+    @property
+    def _is_valid_internal(self) -> bool: return not self.is_configured_incorrect
+    @property
+    def is_highlighted(self) -> bool: return self.match_form in self.variant.configuration.highlighted_words
     @property
     def is_secondary_match(self) -> bool: raise NotImplementedError()
-
     @property
-    def is_displayed(self) -> bool: return (self.is_valid_for_display
-                                            or self.is_emergency_displayed)
-
+    def is_displayed(self) -> bool: return self.is_valid_for_display or self.is_emergency_displayed
     @property
-    def start_index(self) -> int: return self.variant().start_index
-
+    def start_index(self) -> int: return self.variant.start_index
     @property
     def is_indexed(self) -> bool: return self.is_valid or self.is_emergency_displayed
-
     @property
-    def is_valid_for_display(self) -> bool:
-        return self.is_valid and not self.is_configured_hidden and not self.is_shadowed
+    def is_valid_for_display(self) -> bool: return self.is_valid and not self.is_configured_hidden and not self.is_shadowed
 
     @property
     def is_emergency_displayed(self) -> bool:
         return (self.surface_is_seemingly_valid_single_token
-                and self.variant().is_surface
+                and self.variant.is_surface
                 and not self._base_is_valid_word
                 and not self._has_valid_for_display_sibling)
 
     @property
     def _has_valid_for_display_sibling(self) -> bool: return any(other_match for other_match in self._sibling_matches if other_match.is_valid_for_display)
     @property
-    def _sibling_matches(self) -> list[Match]: return [match for match in self.variant().matches if match != self]
+    def _sibling_matches(self) -> list[Match]: return [match for match in self.variant.matches if match != self]
     @property
     def _base_is_valid_word(self) -> bool:
-        base = self.variant().word().base
-        return base is not None and base.is_valid_candidate
-
-    def _has_base_where(self, condition: Callable[[CandidateWordBaseVariant], bool]) -> bool:
-        base = self.variant().word().base
-        return base is not None and condition(base)
+        base = self.word.base
+        return self.word.base is not None and self.word.base.is_valid_candidate
 
     @property
-    def word(self) -> CandidateWord: return self.variant().word()
+    def word(self) -> CandidateWord: return self.variant.word()
     @property
-    def surface_is_seemingly_valid_single_token(self) -> bool: return self.variant().word().surface_is_seemingly_valid_single_token()
+    def surface_is_seemingly_valid_single_token(self) -> bool: return self.word.surface_is_seemingly_valid_single_token()
 
     @property
-    def is_shadowed(self) -> bool: return self.variant().is_shadowed
+    def is_shadowed(self) -> bool: return self.variant.is_shadowed
 
     @property
     def failure_reasons(self) -> set[str]:
@@ -93,7 +81,7 @@ class Match(WeakRefable, Slots):
     def hiding_reasons(self) -> set[str]:
         return (SimpleStringListBuilder()
                 .append_if(self.is_configured_hidden, "configured_hidden")
-                .append_if_lambda(self.is_shadowed, lambda: f"shadowed_by:{self.variant().shadowed_by_text}")
+                .append_if_lambda(self.is_shadowed, lambda: f"shadowed_by:{self.variant.shadowed_by_text}")
                 .as_set())
 
     def __repr__(self) -> str: return f"""{self.parsed_form}, {self.match_form[:10]}: {" ".join(self.failure_reasons)} {" ".join(self.hiding_reasons)}"""
