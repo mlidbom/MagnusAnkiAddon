782ec95f4664f2a4ad6d3635f0e95c61b19866d9
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-12 08:28:35 +0200

    checkpoint

--- src/MagnusAddon/ui/web/vocab/vocab_sentences.py
+++ src/MagnusAddon/ui/web/vocab/vocab_sentences.py
@@ -1,88 +1,13 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from aqt import gui_hooks
-from autoslot import Slots
 from note.note_constants import Tags
 from note.vocabulary.vocabnote import VocabNote
-from sysutils import ex_str, kana_utils
+from sysutils import kana_utils
 from sysutils.ex_str import newline
+from ui.web.vocab.vocab_sentences_vocab_sentence_view_model import VocabSentenceViewModel
 from ui.web.web_utils.content_renderer import PrerenderingAnswerContentRenderer
 
-if TYPE_CHECKING:
-    from note.sentences.parsed_word import ParsedMatch
-    from note.sentences.sentencenote import SentenceNote
-
-class VocabSentenceViewModel(Slots):
-    def __init__(self, _vocab_note: VocabNote, sentence_note: SentenceNote) -> None:
-        self.vocab: VocabNote = _vocab_note
-        self.sentence: SentenceNote = sentence_note
-        self.result = sentence_note.parsing_result.get()
-        self.matches = [match for match in self.result.parsed_words if match.vocab_id == _vocab_note.get_id()]
-        self.displayed_matches = [match for match in self.matches if match.is_displayed]
-        self.highlighted_sentences = set(_vocab_note.sentences.user_highlighted())
-
-    def format_sentence(self) -> str:
-        result = self.result
-        conjugations = self.vocab.forms.conjugations
-
-        def highlight_displayed_match(match: ParsedMatch, class_name: str) -> str:
-            head = result.sentence[:match.start_index]
-            hit_range = result.sentence[match.start_index:match.end_index]
-            tail = result.sentence[match.end_index:]
-            return f"""{head}<span class="vocabInContext {class_name}">{hit_range}</span>{tail}"""
-
-        matches = [match for match in result.parsed_words if match.vocab_id == self.vocab.get_id()]
-        displayed_matches = [match for match in matches if match.is_displayed]
-        if displayed_matches:
-            match = displayed_matches[0]
-            if any(form for form in conjugations.secondary_forms_containing_primary_form_forms if form.startswith(match.parsed_form)):
-                return highlight_displayed_match(match, "secondaryForm")
-            if any(form for form in conjugations.secondary_forms_forms if form.startswith(match.parsed_form)):
-                return highlight_displayed_match(match, "secondaryForm")
-
-            return highlight_displayed_match(match, "primaryForm")
-
-        shaded_matches = [match for match in matches if not match.is_displayed]
-        first_shaded_match = shaded_matches[0]
-        match_shading_our_match = [match for match in reversed(result.parsed_words) if match.is_displayed and match.start_index <= first_shaded_match.start_index][0]
-        if match_shading_our_match.vocab_id in conjugations.derived_compound_ids:
-            if any(form for form in conjugations.secondary_forms_derived_compounds_forms if form.startswith(match_shading_our_match.parsed_form)):
-                return highlight_displayed_match(match_shading_our_match, "secondaryFormDerivedCompoundForm")
-            return highlight_displayed_match(match_shading_our_match, "derivedCompoundForm")
-
-        return highlight_displayed_match(first_shaded_match, "undisplayedMatch")
-
-    def is_highlighted(self) -> bool: return self.sentence in self.highlighted_sentences
-
-    def sentence_classes(self) -> str:
-        classes = ""
-        if self.sentence in self.highlighted_sentences: classes += "highlighted "
-        classes += " ".join(self.sentence.get_meta_tags())
-        return classes
-
-    def contains_secondary_form_with_its_own_vocabulary_note(self) -> bool:
-        return any(base_form for base_form in self.vocab.forms.conjugations.secondary_forms_with_their_own_vocab_forms
-                   if base_form in self.result.sentence)
-
-    def contains_primary_form(self) -> bool:
-        return (not any(covering_secondary_form for covering_secondary_form
-                        in self.vocab.forms.conjugations.secondary_forms_containing_primary_form_forms
-                        if covering_secondary_form in self.result.sentence)
-                and any(base_form for base_form
-                        in self.vocab.forms.conjugations.primary_form_forms
-                        if base_form in self.result.sentence))
-
-    def contains_secondary_form(self) -> bool:
-        return any(base_forms for base_forms
-                   in self.vocab.forms.conjugations.secondary_forms_forms
-                   if any(base_form for base_form in base_forms
-                          if base_form in self.result.sentence))
-
-    def contains_derived_compound(self) -> bool:
-        return any(stem for stem in self.vocab.forms.conjugations.derived_compounds_forms
-                   if stem in self.result.sentence)
 
 def generate_sentences_list_html(_vocab_note: VocabNote) -> str:
     primary_form = _vocab_note.question.without_noise_characters
@@ -91,35 +16,36 @@ def generate_sentences_list_html(_vocab_note: VocabNote) -> str:
     def sort_sentences(_sentences: list[VocabSentenceViewModel]) -> list[VocabSentenceViewModel]:
         is_low_reliability_matching = kana_utils.is_only_kana(primary_form) and len(primary_form) <= 2
 
-        def prefer_highlighted_for_low_reliability_matches(_sentence: VocabSentenceViewModel) -> int: return 0 if is_low_reliability_matching and _sentence.is_highlighted() else 1
+        def prefer_highlighted_for_low_reliability_matches(_sentence: VocabSentenceViewModel) -> int: return 0 if (is_low_reliability_matching and _sentence.is_highlighted()) else 1
         def prefer_highlighted(_sentence: VocabSentenceViewModel) -> int: return 0 if _sentence.is_highlighted() else 1
         def prefer_studying_read(_sentence: VocabSentenceViewModel) -> int: return 0 if _sentence.sentence.is_studying_read() else 1
         def prefer_studying_listening(_sentence: VocabSentenceViewModel) -> int: return 0 if _sentence.sentence.is_studying_listening() else 1
-        def dislike_secondary_form_with_vocab(_sentence: VocabSentenceViewModel) -> int: return 1 if not _sentence.contains_primary_form() and _sentence.contains_secondary_form_with_its_own_vocabulary_note() else 0
-        def prefer_primary_form(_sentence: VocabSentenceViewModel) -> int: return 0 if _sentence.contains_primary_form() else 1
+        def dislike_secondary_form_with_vocab(_sentence: VocabSentenceViewModel) -> int: return 1 if (not _sentence.primary_form_is_displayed() and _sentence.contains_secondary_form_with_its_own_vocabulary_note()) else 0
+        def prefer_primary_form(_sentence: VocabSentenceViewModel) -> int: return 0 if _sentence.primary_form_is_displayed() else 1
         def dislike_tts_sentences(_sentence: VocabSentenceViewModel) -> int: return 1 if _sentence.sentence.has_tag(Tags.TTSAudio) else 0
         def prefer_short_questions(_sentence: VocabSentenceViewModel) -> int: return len(_sentence.sentence.get_question())
         def prefer_lower_priority_tag_values(_sentence: VocabSentenceViewModel) -> int: return _sentence.sentence.priority_tag_value()
-        def dislike_no_translation(_sentence: VocabSentenceViewModel) -> int: return 1 if not _sentence.sentence.get_answer().strip() else 0
+        def dislike_no_translation(_sentence: VocabSentenceViewModel) -> int: return 1 if not _sentence.sentence.get_answer() else 0
 
         def dislike_sentences_containing_secondary_form(_sentence: VocabSentenceViewModel) -> int: return 1 if _sentence.contains_secondary_form() else 0
         def dislike_contains_derived_compound(_sentence: VocabSentenceViewModel) -> int: return 1 if _sentence.contains_derived_compound() else 0
 
-        return sorted(_sentences, key=lambda x: (dislike_secondary_form_with_vocab(x),
-                                                 prefer_studying_read(x),
-                                                 prefer_studying_listening(x),
-                                                 prefer_highlighted_for_low_reliability_matches(x),
-                                                 dislike_no_translation(x),
-                                                 prefer_lower_priority_tag_values(x),
-                                                 dislike_tts_sentences(x),
-                                                 prefer_primary_form(x),
-                                                 prefer_highlighted(x),
-                                                 dislike_contains_derived_compound(x),
-                                                 dislike_sentences_containing_secondary_form(x),
-                                                 prefer_short_questions(x)))
+        return sorted(_sentences,
+                      key=lambda x: (dislike_secondary_form_with_vocab(x),
+                                     prefer_studying_read(x),
+                                     prefer_studying_listening(x),
+                                     prefer_highlighted_for_low_reliability_matches(x),
+                                     dislike_no_translation(x),
+                                     prefer_lower_priority_tag_values(x),
+                                     dislike_tts_sentences(x),
+                                     prefer_primary_form(x),
+                                     prefer_highlighted(x),
+                                     dislike_contains_derived_compound(x),
+                                     dislike_sentences_containing_secondary_form(x),
+                                     prefer_short_questions(x)))
 
     sentences = sort_sentences([VocabSentenceViewModel(_vocab_note, sentence_note) for sentence_note in _vocab_note.sentences.all()])
-    primary_form_matches = len([x for x in sentences if x.contains_primary_form()])
+    primary_form_matches = len([x for x in sentences if x.primary_form_is_displayed()])
     sentences = sentences[:30]
 
     return f'''
---
+++ src/MagnusAddon/ui/web/vocab/vocab_sentences_vocab_sentence_view_model.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from autoslot import Slots
+
+if TYPE_CHECKING:
+    from note.sentences.parsed_word import ParsedMatch
+    from note.sentences.sentencenote import SentenceNote
+    from note.vocabulary.vocabnote import VocabNote
+
+class VocabSentenceMatchViewModel:
+    def __init__(self, match: ParsedMatch, sentence_view_model: VocabSentenceViewModel) -> None:
+        self.match = match
+        self.sentence_view_model = sentence_view_model
+
+    @property
+    def is_displayed(self) -> bool: return self.match.is_displayed
+    @property
+    def start_index(self) -> int: return self.match.start_index
+    @property
+    def end_index(self) -> int: return self.match.end_index
+    @property
+    def parsed_form(self) -> str: return self.match.parsed_form
+    @property
+    def shaded_by(self) -> ParsedMatch: return [match for match in reversed(self.sentence_view_model.result.parsed_words)
+                                                if match.is_displayed and match.start_index <= self.start_index][0]
+
+class VocabSentenceViewModel(Slots):
+    def __init__(self, _vocab_note: VocabNote, sentence_note: SentenceNote) -> None:
+        self.vocab: VocabNote = _vocab_note
+        self.sentence: SentenceNote = sentence_note
+        self.result = sentence_note.parsing_result.get()
+        self.matches = [VocabSentenceMatchViewModel(match, self) for match in self.result.parsed_words if match.vocab_id == _vocab_note.get_id()]
+        self.displayed_matches = [match for match in self.matches if match.is_displayed]
+        self.highlighted_sentences = set(_vocab_note.sentences.user_highlighted())
+        self.shaded_matches = [match for match in self.matches if not match.is_displayed]
+
+    @property
+    def first_shaded_match(self) -> VocabSentenceMatchViewModel: return self.shaded_matches[0]
+
+    @property
+    def first_match(self) -> VocabSentenceMatchViewModel: return self.matches[0]
+
+    def format_sentence(self) -> str:
+        result = self.result
+        conjugations = self.vocab.forms.conjugations
+
+        def highlight_displayed_match(match: VocabSentenceMatchViewModel, class_name: str) -> str:
+            head = result.sentence[:match.start_index]
+            hit_range = result.sentence[match.start_index:match.end_index]
+            tail = result.sentence[match.end_index:]
+            return f"""{head}<span class="vocabInContext {class_name}">{hit_range}</span>{tail}"""
+
+        def highlight_shaded_match(match: VocabSentenceMatchViewModel, shaded_by: ParsedMatch, inner_class_name: str, outer_class_name: str) -> str:
+            outer_head = result.sentence[:shaded_by.start_index]
+            outer_tail = result.sentence[shaded_by.end_index:]
+            inner_range = result.sentence[match.start_index:match.end_index]
+            formatted_match = f"""<span class="vocabInContext {inner_class_name}">{inner_range}</span>"""
+            return f"""{outer_head}<span class="vocabInContext {outer_class_name}">{formatted_match}</span>{outer_tail}"""
+
+        if self.first_match.is_displayed:
+            if any(form for form in conjugations.secondary_forms_containing_primary_form_forms if form.startswith(self.first_match.parsed_form)):
+                return highlight_displayed_match(self.first_match, "secondaryForm")
+            if any(form for form in conjugations.secondary_forms_forms if form.startswith(self.first_match.parsed_form)):
+                return highlight_displayed_match(self.first_match, "secondaryForm")
+
+            return highlight_displayed_match(self.first_match, "primaryForm")
+
+        match_shading_our_match = self.first_match.shaded_by
+        if match_shading_our_match.vocab_id in conjugations.derived_compound_ids:
+            if any(form for form in conjugations.secondary_forms_derived_compounds_forms if form.startswith(match_shading_our_match.parsed_form)):
+                return highlight_shaded_match(self.first_match, match_shading_our_match, "secondaryFormDerivedCompoundForm", "primaryForm")
+            return highlight_shaded_match(self.first_match, match_shading_our_match, "derivedCompoundForm", "primaryForm")
+
+        return highlight_displayed_match(self.first_shaded_match, "undisplayedMatch")
+
+    def is_highlighted(self) -> bool: return self.sentence in self.highlighted_sentences
+
+    def sentence_classes(self) -> str:
+        classes = ""
+        if self.sentence in self.highlighted_sentences: classes += "highlighted "
+        classes += " ".join(self.sentence.get_meta_tags())
+        return classes
+
+    def contains_secondary_form_with_its_own_vocabulary_note(self) -> bool:
+        return any(base_form for base_form in self.vocab.forms.conjugations.secondary_forms_with_their_own_vocab_forms
+                   if base_form in self.result.sentence)
+
+    def primary_form_is_displayed(self) -> bool:
+        return (not any(covering_secondary_form for covering_secondary_form
+                        in self.vocab.forms.conjugations.secondary_forms_containing_primary_form_forms
+                        if covering_secondary_form in self.result.sentence)
+                and any(base_form for base_form
+                        in self.vocab.forms.conjugations.primary_form_forms
+                        if base_form in self.result.sentence))
+
+    def contains_secondary_form(self) -> bool:
+        return any(base_forms for base_forms
+                   in self.vocab.forms.conjugations.secondary_forms_forms
+                   if any(base_form for base_form in base_forms
+                          if base_form in self.result.sentence))
+
+    def contains_derived_compound(self) -> bool:
+        return any(stem for stem in self.vocab.forms.conjugations.derived_compounds_forms
+                   if stem in self.result.sentence)
