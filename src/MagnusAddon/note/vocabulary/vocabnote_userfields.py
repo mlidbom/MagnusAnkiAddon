2a2018f8f26b1c661dfa9fccba8d2f77542ec126
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-15 12:22:48 +0200

    Woops, removed a bit to much a few commits back.

--- src/MagnusAddon/note/vocabulary/vocabnote_userfields.py
+++ src/MagnusAddon/note/vocabulary/vocabnote_userfields.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from autoslot import Slots
 from note.note_constants import NoteFields
-from note.notefields.mutable_string_field import MutableStringField
+from note.notefields.string_field import StringField
 
 if TYPE_CHECKING:
     from note.vocabulary.vocabnote import VocabNote
@@ -13,5 +13,7 @@ if TYPE_CHECKING:
 
 class VocabNoteUserfields(Slots):
     def __init__(self, vocab: WeakRef[VocabNote]) -> None:
-        self.mnemonic: MutableStringField = MutableStringField(vocab, NoteFields.Vocab.user_mnemonic)
-        self.answer: MutableStringField = MutableStringField(vocab, NoteFields.Vocab.user_answer)
+        self.mnemonic: StringField = StringField(vocab, NoteFields.Vocab.user_mnemonic)
+        self.answer: StringField = StringField(vocab, NoteFields.Vocab.user_answer)
+        self.explanation: StringField = StringField(vocab, NoteFields.Vocab.user_explanation)
+        self.explanation_long: StringField = StringField(vocab, NoteFields.Vocab.user_explanation_long)
