d52a107552fdd61845cadb791e964c7b843de597
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-17 15:45:20 +0200

    Fix autoslot typing problems with pyright by copying the library into the project.

--- src/MagnusAddon/ex_autoslot.py
+++ src/MagnusAddon/ex_autoslot.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from autoslot import SlotsPlusDict  # noqa: F401  # pyright: ignore
+from manually_copied_in_libraries.autoslot import SlotsPlusDict
 
 # todo prorably add some code here to only use PlusDict when profiling and save us that overhead when not profiling, like ProfilableAutoSlots = SlotsPlusDict if os.environ.get('LINE_PROFILE') else Slots
-ProfilableAutoSlots = SlotsPlusDict
+class ProfilableAutoSlots(SlotsPlusDict):
+    pass
