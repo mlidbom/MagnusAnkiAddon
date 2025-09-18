8ab930dd507363b822b21eef552a111407645dac
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-18 15:19:22 +0200

    refactoring

--- src/MagnusAddon/note/vocabulary/vocabnote_conjugator.py
+++ src/MagnusAddon/note/vocabulary/vocabnote_conjugator.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from ex_autoslot import ProfilableAutoSlots
 from language_services import conjugator
-from sysutils import ex_sequence
+from sysutils.collections.linq.q_iterable import QList, query
 
 if TYPE_CHECKING:
     from note.vocabulary.vocabnote import VocabNote
@@ -17,13 +17,13 @@ class VocabNoteConjugator(ProfilableAutoSlots):
     @property
     def _vocab(self) -> VocabNote: return self.__vocab()
 
-    def _get_stems_for_form(self, form: str) -> list[str]:
-        return [base for base in conjugator.get_word_stems(form,
-                                                           is_ichidan_verb=self._vocab.parts_of_speech.is_ichidan(),
-                                                           is_godan=self._vocab.parts_of_speech.is_godan()) if base != form]
+    def _get_stems_for_form(self, form: str) -> QList[str]:
+        return (query(conjugator.get_word_stems(form, is_ichidan_verb=self._vocab.parts_of_speech.is_ichidan(), is_godan=self._vocab.parts_of_speech.is_godan()))
+                .where(lambda stem: stem != form)
+                .to_list())
 
-    def get_stems_for_primary_form(self) -> list[str]:
-        return ex_sequence.remove_duplicates_while_retaining_order(self._get_stems_for_form(self._vocab.get_question()))
+    def get_stems_for_primary_form(self) -> QList[str]:
+        return self._get_stems_for_form(self._vocab.get_question()).unique()  # ex_sequence.remove_duplicates_while_retaining_order(self._get_stems_for_form(self._vocab.get_question()))
 
-    def get_stems_for_all_forms(self) -> list[str]:
-        return ex_sequence.flatten([self._get_stems_for_form(form) for form in self._vocab.forms.all_set()])
+    def get_stems_for_all_forms(self) -> QList[str]:
+        return self._vocab.forms.all_set().select_many(self._get_stems_for_form).to_list().unique()  # ex_sequence.flatten([self._get_stems_for_form(form) for form in self._vocab.forms.all_set()])
