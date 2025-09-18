fc8bfa87b1e91ba833b7003c972e17abd81c44a9
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-18 12:19:36 +0200

    refactoring

--- src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_sentence_analysis_viewmodel_common.py
+++ src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_sentence_analysis_viewmodel_common.py
@@ -18,26 +18,31 @@ def surface_and_match_form(match_vm: MatchViewModel) -> str:
 def assert_display_words_equal_and_that_failed_matches_have_proper_messages(sentence: str, excluded: list[WordExclusion], expected_output: list[str]) -> None:
 
     def run_note_assertions(message: str) -> None:
-        analysis = SentenceAnalysisViewModel(sentence_note)
-
         root_words = [surface_and_match_form(dm) for dm in analysis.displayed_matches]
         try:
             assert root_words == expected_output
-        except AssertionError as e:
+        except AssertionError:
             print(f"""
 
 ---####--- {message} ---####---""")
-            raise e
+            raise
 
     sentence_note = SentenceNote.create(sentence)
+    if len(excluded) != 0:
+        sentence_note.configuration._value = Lazy[SentenceConfiguration].from_value(SentenceConfiguration.from_hidden_matches(excluded))  # pyright: ignore[reportPrivateUsage]
+
+        run_note_assertions("running assertions with exclusions hidden")
+    analysis = SentenceAnalysisViewModel(sentence_note)
+
     if len(excluded) == 0:
         run_note_assertions("running assertions with no exclusions")
     else:
-        sentence_note.configuration._value = Lazy[SentenceConfiguration].from_value(SentenceConfiguration.from_hidden_matches(excluded))  # pyright: ignore[reportPrivateUsage]
+
         run_note_assertions("running assertions with exclusions hidden")
 
         sentence_note.configuration._value = Lazy[SentenceConfiguration].from_value(SentenceConfiguration.from_incorrect_matches(excluded))  # pyright: ignore[reportPrivateUsage]
         run_note_assertions("running assertions with exclusions marked as incorrect matches")
+
 
 def assert_all_words_equal(sentence: str, expected_output: list[str]) -> None:
     sentence_note = SentenceNote.create(sentence)
