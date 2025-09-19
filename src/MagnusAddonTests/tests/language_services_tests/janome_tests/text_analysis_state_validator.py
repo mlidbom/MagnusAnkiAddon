2dc011c9dd754bb4a946665ce230e399d81f4dbb
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-19 03:16:58 +0200

    assertion improvements

--- src/MagnusAddonTests/tests/language_services_tests/janome_tests/text_analysis_state_validator.py
+++ src/MagnusAddonTests/tests/language_services_tests/janome_tests/text_analysis_state_validator.py
@@ -11,13 +11,14 @@ class TextAnalysisStateValidator:
     def __init__(self, analysis: TextAnalysis) -> None:
         self.analysis = analysis
 
-        self.is_valid_true_matches:QSet[Match] = analysis.all_matches.where(lambda match: match.is_valid).to_set()
-        self.is_valid_true_is_displayed_false_matches:QSet[Match] = self.is_valid_true_matches.where(lambda match: not match.is_displayed).to_set()
-        self.is_displayed_true_matches:QSet[Match] = analysis.all_matches.where(lambda match: match.is_displayed).to_set()
-        self.is_valid_false_matches:QSet[Match] = analysis.all_matches.where(lambda match: not match.is_valid).to_set()
-        self.display_matches:QSet[Match] = analysis.display_matches.to_set()
-        self.valid_matches:QSet[Match] = analysis.valid_matches.to_set()
-        self.valid_variant_valid_matches:QSet[Match] = analysis.valid_word_variant_valid_matches.to_set()
+        self.is_valid_true_matches: QSet[Match] = analysis.all_matches.where(lambda match: match.is_valid).to_set()
+        self.is_valid_true_hiding_reasons_empty_matches: QSet[Match] = self.is_valid_true_matches.where(lambda match: len(match.hiding_reasons) == 0).to_set()
+        self.is_valid_true_is_displayed_false_matches: QSet[Match] = self.is_valid_true_matches.where(lambda match: not match.is_displayed).to_set()
+        self.is_displayed_true_matches: QSet[Match] = analysis.all_matches.where(lambda match: match.is_displayed).to_set()
+        self.is_valid_false_matches: QSet[Match] = analysis.all_matches.where(lambda match: not match.is_valid).to_set()
+        self.display_matches: QSet[Match] = analysis.display_matches.to_set()
+        self.valid_matches: QSet[Match] = analysis.valid_matches.to_set()
+        self.valid_variant_valid_matches: QSet[Match] = analysis.valid_word_variant_valid_matches.to_set()
 
     def assert_is_valid(self) -> None:
         print(self.analysis.text)
@@ -26,20 +27,24 @@ class TextAnalysisStateValidator:
         self.is_valid_false_matches_should_have_failure_reasons()
         self.is_valid_true_is_displayed_false_matches_should_have_hiding_reasons()
         self.is_valid_true_matches_should_be_in_valid_matches()
-        #self.is_valid_true_matches_should_be_in_valid_variant_valid_matches()
-        #self.valid_matches_and_valid_variant_matches_should_be_identical()
+        self.is_valid_true_hiding_reasons_empty_matches_should_have_is_displayed_true()
+        # self.is_valid_true_matches_should_be_in_valid_variant_valid_matches()
+        # self.valid_matches_and_valid_variant_matches_should_be_identical()
 
     def is_displayed_false_matches_should_not_be_in_display_matches_list(self) -> None:
         self.is_valid_true_is_displayed_false_matches.assert_each(lambda match: match not in self.display_matches, lambda match: f"""Match: {match} has is_displayed=False yet is displayed""")
+
+    def is_valid_true_is_displayed_false_matches_should_have_hiding_reasons(self) -> None:
+        self.is_valid_true_is_displayed_false_matches.assert_each(lambda match: len(match.hiding_reasons) > 0, lambda match: f"""Match: {match} has is_displayed=False yet has no hiding_reasons""")
+
+    def is_valid_true_hiding_reasons_empty_matches_should_have_is_displayed_true(self) -> None:
+        self.is_valid_true_hiding_reasons_empty_matches.assert_each(lambda match: match.is_displayed, lambda match: f"""Match: {match} has is_valid=True and hiding_reasons=Empty yet has is_displayed=False""")
 
     def is_valid_true_matches_should_be_in_valid_matches(self) -> None:
         self.is_valid_true_matches.assert_each(lambda match: match in self.valid_matches, lambda match: f"""Match: {match} has is_valid=True yet is not in valid_matches""")
 
     def is_displayed_true_matches_should_be_in_display_matches(self) -> None:
         self.is_displayed_true_matches.assert_each(lambda match: match in self.display_matches, lambda match: f"""Match: {match} has is_displayed=True yet is not in display_matches""")
-
-    def is_valid_true_is_displayed_false_matches_should_have_hiding_reasons(self) -> None:
-        self.is_valid_true_is_displayed_false_matches.assert_each(lambda match: len(match.hiding_reasons) > 0, lambda match: f"""Match: {match} has is_displayed=False yet has no hiding_reasons""")
 
     def is_valid_false_matches_should_have_failure_reasons(self) -> None:
         self.is_valid_false_matches.assert_each(lambda match: len(match.failure_reasons) > 0, lambda match: f"""Match: {match} has is_valid=False yet has no failure_reasons""")
