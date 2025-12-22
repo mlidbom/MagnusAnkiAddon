b0e15ea0afa9720f4a49143c0d7f9b1efa7efcc1
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-12-22 12:09:52 +0100

    checkpoint

--- src/MagnusAddon/sysutils/memory_usage/string_auto_interner.py
+++ src/MagnusAddon/sysutils/memory_usage/string_auto_interner.py
@@ -5,8 +5,12 @@ import sys
 import mylog
 from ankiutils import app
 from typed_linq_collections.collections.q_dict import QDict
+from typed_linq_collections.collections.string_interning import set_default_intern_func
 
 _is_enabled = app.config().enable_auto_string_interning.get_value()
+
+intern_hit_count = 0
+intern_miss_count = 0
 
 class StringWithUsage:
     def __init__(self, string: str) -> None:
@@ -17,9 +21,15 @@ class StringWithUsage:
         return self.usage_count > 100  # todo: should probably be some combination of length and usage count in the end
 
     def increment_usage_count(self) -> None:
+        global intern_hit_count, intern_miss_count
         self.usage_count += 1
         if self.is_worth_interning():
-            sys.intern(self.value)
+            original_id = id(self.value)
+            self.value = sys.intern(self.value)
+            if id(self.value) == original_id:
+                intern_hit_count += 1
+            else:
+                intern_miss_count += 1
             store.remove(self.value)
             interned_hashes.add(hash(self.value))
 
@@ -32,7 +42,7 @@ def auto_intern(string: str) -> str:  # replace every string placed in any memor
     if not _is_enabled: return string
     if hash(string) in interned_hashes: return sys.intern(string)
 
-    value = store.get_or_add(string, lambda: StringWithUsage(string))
+    value = store.get_or_add(string, StringWithUsage)
     value.increment_usage_count()
     return value.value
 
@@ -45,4 +55,6 @@ def auto_intern_list(strings: list[str]) -> None:
 def flush_store() -> None:  # call this after populating the application memory cache, thus discarding the unhelpful strings from interning and ensuring that the helpful ones stay using a single instance.
     non_interned = store.qcount()
     store.clear()
-    mylog.info(f"string_auto_interner: interned {len(interned_hashes)} strings, removed {non_interned}")
+    mylog.info(f"string_auto_interner: interned {len(interned_hashes)} strings, removed {non_interned}: hit_count={intern_hit_count}, miss_count={intern_miss_count}")
+
+set_default_intern_func(auto_intern)
