0beee8e8eb9389cd7c77346a4fdd9522f77b4d06
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-12-23 07:09:10 +0100

    Use sys.intern directly

--- src/MagnusAddon/sysutils/memory_usage/string_auto_interner.py
+++ src/MagnusAddon/sysutils/memory_usage/string_auto_interner.py
@@ -12,6 +12,7 @@ from typed_linq_collections.collections.string_interning import set_default_inte
 if TYPE_CHECKING:
     from typed_linq_collections.collections.q_list import QList
 
+use_sys_intern = False
 use_simple_interner = True
 
 _is_enabled = False
@@ -25,30 +26,53 @@ def try_enable() -> None:  # call this after populating the application memory c
 
 try_enable()
 
-if use_simple_interner:
-    simple_store = dict[str, str]()
+if use_sys_intern:  # noqa: SIM114
     def auto_intern(string: str) -> str:  # replace every string placed in any memory cache in the application with the value returned by this function
-        if not _is_enabled: return string
-        return simple_store.setdefault(string, string)
+        #if not _is_enabled: return string
+        return sys.intern(string)
 
     def auto_intern_list(strings: list[str]) -> list[str]:
-        if _is_enabled:
-            for index, value in enumerate(strings):
-                strings[index] = simple_store.setdefault(value, value)
+        #if _is_enabled:
+        for index, value in enumerate(strings):
+            strings[index] = sys.intern(value)
 
         return strings
 
     def auto_intern_qlist(strings: QList[str]) -> QList[str]:
-        if _is_enabled:
-            for index, value in enumerate(strings):
-                strings[index] = simple_store.setdefault(value, value)
+        #if _is_enabled:
+        for index, value in enumerate(strings):
+            strings[index] = sys.intern(value)
 
         return strings
 
     def flush_store_and_disable() -> None:  # call this after populating the application memory cache, thus discarding the unhelpful strings from interning and ensuring that the helpful ones stay using a single instance.
-        global _is_enabled
-        _is_enabled = False
-        simple_store.clear()
+        pass
+
+elif use_simple_interner:
+    simple_store = dict[str, str]()
+    def auto_intern(string: str) -> str:  # replace every string placed in any memory cache in the application with the value returned by this function
+        #if not _is_enabled: return string
+        return simple_store.setdefault(string, string)
+
+    def auto_intern_list(strings: list[str]) -> list[str]:
+        #if _is_enabled:
+        for index, value in enumerate(strings):
+            strings[index] = simple_store.setdefault(value, value)
+
+        return strings
+
+    def auto_intern_qlist(strings: QList[str]) -> QList[str]:
+        #if _is_enabled:
+        for index, value in enumerate(strings):
+            strings[index] = simple_store.setdefault(value, value)
+
+        return strings
+
+    def flush_store_and_disable() -> None:  # call this after populating the application memory cache, thus discarding the unhelpful strings from interning and ensuring that the helpful ones stay using a single instance.
+        pass
+        # global _is_enabled
+        # _is_enabled = False
+        # simple_store.clear()
 else:
     intern_hit_count = 0
     intern_miss_count = 0
