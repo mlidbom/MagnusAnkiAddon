0662b4356008e8374f35d6e04dbc9305534468ae
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-12-25 13:27:49 +0100

    browser menu should respect minimum sentences required to run garbage collection on batches

--- src/MagnusAddon/ui/menus/browser/main.py
+++ src/MagnusAddon/ui/menus/browser/main.py
@@ -54,7 +54,7 @@ def setup_browser_context_menu(browser: Browser, menu: QMenu) -> None:
     if selected_sentences:
         from batches import local_note_updater
 
-        magnus_menu.addAction("Reparse sentence words", lambda: local_note_updater.reparse_sentences(selected_sentences))  # pyright: ignore[reportUnknownMemberType]
+        magnus_menu.addAction("Reparse sentence words", lambda: local_note_updater.reparse_sentences(selected_sentences, run_gc_during_batch=True))  # pyright: ignore[reportUnknownMemberType]
 
 
 def init() -> None:
