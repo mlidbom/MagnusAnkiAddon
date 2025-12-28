3f084d2863477c9931b488de94e043ec267ccdb0
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-12-28 09:34:51 +0100

    rearrange tests

--- src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_sentence_analysis_viewmodel_with_select_data.py
+++ src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_sentence_analysis_viewmodel_with_select_data.py
@@ -17,7 +17,7 @@ def setup_collection_with_select_data() -> Iterator[None]:
         yield
 
 @pytest.mark.parametrize("sentence, expected_output", [
-        ("ここは清められ", ["ここ", "は", "清める", "られる"])
+
 ])
 def test_new_stuff(sentence: str, expected_output: list[str]) -> None:
     assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentence, [], expected_output)
@@ -35,6 +35,7 @@ def test_new_stuff(sentence: str, expected_output: list[str]) -> None:
         ("進めない", ["進める", "ない"]),
         ("さっさと傷を清めてこい", ["さっさと:[MISSING]", "傷", "を", "清める", "てこ", "い"]),
         ("清めの一波", ["清め", "の", "一波:[MISSING]"]),
+        ("ここは清められ", ["ここ", "は", "清める", "られる"])
 ])
 def test_godan_potential_and_imperative(sentence: str, expected_output: list[str]) -> None:
     assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentence, [], expected_output)
