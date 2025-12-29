b62c086f5671ee30f40ff10729d1c9dabcfef930
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-12-29 20:59:56 +0100

    improve test coverage

--- src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_sentence_analysis_viewmodel_with_select_data.py
+++ src/MagnusAddonTests/tests/language_services_tests/janome_tests/test_sentence_analysis_viewmodel_with_select_data.py
@@ -17,6 +17,7 @@ def setup_collection_with_select_data() -> Iterator[None]:
         yield
 
 @pytest.mark.parametrize("sentence, expected_output", [
+
 ])
 def test_new_stuff(sentence: str, expected_output: list[str]) -> None:
     assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentence, [], expected_output)
@@ -35,7 +36,8 @@ def test_new_stuff(sentence: str, expected_output: list[str]) -> None:
         ("さっさと傷を清めてこい", ["さっさと:[MISSING]", "傷", "を", "清める", "てこ", "い"]),
         ("清めの一波", ["清め", "の", "一波:[MISSING]"]),
         ("ここは清められ", ["ここ", "は", "清める", "られる"]),
-        ("その物陰に隠れろ", ["その", "物陰", "に", "隠れる", "ろ"])
+        ("その物陰に隠れろ", ["その", "物陰", "に", "隠れる", "ろ"]),
+        ("聞けよ", ["聞え", "よ"])
 ])
 def test_godan_potential_and_imperative(sentence: str, expected_output: list[str]) -> None:
     assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentence, [], expected_output)
