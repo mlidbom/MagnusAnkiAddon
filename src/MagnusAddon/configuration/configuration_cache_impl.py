13230a289279f89e3b28a37de7f92189fb98955f
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-01-03 22:01:28 +0100

    Warnings.

--- src/MagnusAddon/configuration/configuration_cache_impl.py
+++ src/MagnusAddon/configuration/configuration_cache_impl.py
@@ -5,10 +5,10 @@ from ankiutils import app
 
 class ConfigurationCache:
     _instance: ConfigurationCache | None = None
-    _initialized = False
+    _initialized: bool = False
     def __init__(self) -> None:
-        self._hide_transparent_compounds = app.config().hide_compositionally_transparent_compounds.get_value()
-        self._hide_all_compounds = app.config().hide_all_compounds.get_value()
+        self._hide_transparent_compounds: bool = app.config().hide_compositionally_transparent_compounds.get_value()
+        self._hide_all_compounds: bool = app.config().hide_all_compounds.get_value()
 
         if not ConfigurationCache._initialized:
             app.config().hide_compositionally_transparent_compounds.on_change(self._replace_instance)
@@ -27,5 +27,5 @@ class ConfigurationCache:
         return ConfigurationCache._instance
 
     @staticmethod
-    def _replace_instance(value: object) -> None:
+    def _replace_instance(_: object) -> None:
         ConfigurationCache._instance = ConfigurationCache()
