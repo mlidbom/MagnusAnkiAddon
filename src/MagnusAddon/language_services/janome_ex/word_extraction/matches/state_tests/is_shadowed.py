50971686b991dbbc1763bcb08a774e5c263ebb76
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-01-04 16:17:01 +0100

    Get rid of obsolety class.

--- src/MagnusAddon/language_services/janome_ex/word_extraction/matches/state_tests/is_shadowed.py
+++ src/MagnusAddon/language_services/janome_ex/word_extraction/matches/state_tests/is_shadowed.py
@@ -2,19 +2,22 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, override
 
-from autoslot import Slots  # pyright: ignore[reportMissingTypeStubs]
-from language_services.janome_ex.word_extraction.matches.requirements.match_custom_forbids_no_cache import MatchCustomForbidsNoCache
+from autoslot import Slots
+from language_services.janome_ex.word_extraction.matches.requirements.requirement import MatchRequirement
 
 if TYPE_CHECKING:
     from language_services.janome_ex.word_extraction.matches.requirements.match_inspector import MatchInspector
 
-class ForbidsIsShadowed(MatchCustomForbidsNoCache, Slots):
+class ForbidsIsShadowed(MatchRequirement, Slots):
     def __init__(self, inspector: MatchInspector) -> None:
-        super().__init__(inspector)
+        self.inspector: MatchInspector = inspector
 
     @property
     @override
-    def description(self) -> str: return f"shadowed_by:{self.inspector.match.word.shadowed_by_text}"
+    def is_fulfilled(self) -> bool:
+        return not self.inspector.match.is_shadowed
 
+    @property
     @override
-    def _internal_is_in_state(self) -> bool: return self.inspector.match.is_shadowed
+    def failure_reason(self) -> str:
+        return f"""forbids::shadowed_by:{self.inspector.match.word.shadowed_by_text}""" if not self.is_fulfilled else ""
