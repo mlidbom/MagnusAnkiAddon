6f6128a105aa3e4bf0cf9eec94670c15ee177d26
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-01-11 07:14:38 +0100

    refactoring

--- src/MagnusAddon/language_services/janome_ex/tokenizing/split_token.py
+++ src/MagnusAddon/language_services/janome_ex/tokenizing/split_token.py
@@ -3,14 +3,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, override
 
 from autoslot import Slots
-from language_services.janome_ex.tokenizing.analysis_token import IAnalysisToken
+from language_services.janome_ex.tokenizing.godan_dictionary_form_stem import SplitTokenBase
 
 if TYPE_CHECKING:
     from language_services.janome_ex.tokenizing.jn_token import JNToken
 
 # this is the interface that is actually used in the text analysis pipeline
 
-class SplitToken(IAnalysisToken, Slots):
+class SplitToken(SplitTokenBase, Slots):
     def __init__(self,
                  source: JNToken,
                  surface: str,
@@ -23,9 +23,8 @@ class SplitToken(IAnalysisToken, Slots):
                  is_godan_potential_inflection: bool = False,
                  is_godan_imperative_inflection: bool = False,
                  is_ichidan_imperative_inflection: bool = False) -> None:
-        self.source: JNToken = source
-        self._surface: str = surface
-        self._base_form: str = base
+        super().__init__(source, surface, base)
+
         self._is_inflectable_word: bool = is_inflectable_word
         self._is_non_word_character: bool = is_non_word_character
         self._is_godan_potential_stem: bool = is_godan_potential_stem
@@ -38,16 +37,7 @@ class SplitToken(IAnalysisToken, Slots):
     # <IAnalysisToken implementation>
     @property
     @override
-    def source_token(self) -> JNToken: return self.source
-    @property
-    @override
-    def surface(self) -> str: return self._surface
-    @property
-    @override
     def is_te_form_stem(self) -> bool: return self.is_godan_potential_inflection
-    @property
-    @override
-    def base_form(self) -> str: return self._base_form
     @property
     @override
     def is_inflectable_word(self) -> bool: return self._is_inflectable_word
@@ -79,7 +69,3 @@ class SplitToken(IAnalysisToken, Slots):
     @override
     def is_ichidan_imperative_inflection(self) -> bool: return self._is_ichidan_imperative_inflection
     # </IAnalysisToken implementation>
-
-    @override
-    def __repr__(self) -> str:
-        return f"ProcessedToken('{self.surface}', '{self.base_form}', {self.is_inflectable_word})"
