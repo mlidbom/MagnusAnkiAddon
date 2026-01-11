e38fad8bbeb3aab76969eb6851230545da92877b
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-01-11 07:30:39 +0100

    refactoring

--- src/MagnusAddon/language_services/janome_ex/tokenizing/analysis_token.py
+++ src/MagnusAddon/language_services/janome_ex/tokenizing/analysis_token.py
@@ -7,7 +7,6 @@ from autoslot import Slots
 if TYPE_CHECKING:
     from language_services.janome_ex.tokenizing.jn_token import JNToken
 
-
 # the interface that is used in the text analysis pipeline, defaults to returning False for all bool properties
 # and inheritors get to override whatever properties they can.
 class IAnalysisToken(Slots):
@@ -17,29 +16,32 @@ class IAnalysisToken(Slots):
     def base_form(self) -> str: raise NotImplementedError()
 
     @property
-    def is_ichidan_verb(self) -> bool: return False
+    def is_ichidan_verb(self) -> bool: return self.source_token.is_ichidan_verb
     @property
-    def is_godan_verb(self) -> bool: return False
+    def is_godan_verb(self) -> bool: return self.source_token.is_godan_verb
     @property
-    def is_past_tense_stem(self) -> bool: return False
+    def is_past_tense_stem(self) -> bool: return self.source_token.is_past_tense_stem
     @property
-    def is_ichidan_masu_stem(self) -> bool: return False
+    def is_ichidan_masu_stem(self) -> bool: return self.source_token.is_ichidan_masu_stem
     @property
-    def is_te_form_stem(self) -> bool: return False
+    def is_te_form_stem(self) -> bool: return self.source_token.is_te_form_stem
     @property
-    def is_past_tense_marker(self) -> bool: return False
+    def is_past_tense_marker(self) -> bool: return self.source_token.is_past_tense_marker
     @property
-    def is_special_nai_negative(self) -> bool: return False
+    def is_special_nai_negative(self) -> bool: return self.source_token.is_special_nai_negative
+    @property
+    def is_masu_stem(self) -> bool: return self.source_token.is_masu_stem
+    @property
+    def is_adverb(self) -> bool: return self.source_token.is_adverb
+    @property
+    def is_irrealis(self) -> bool: return self.source_token.is_irrealis
+    @property
+    def is_end_of_statement(self) -> bool: return self.source_token.is_end_of_statement
+
     @property
     def is_inflectable_word(self) -> bool: return False
     @property
     def is_non_word_character(self) -> bool: return False
-    @property
-    def is_masu_stem(self) -> bool: return False
-    @property
-    def is_adverb(self) -> bool: return False
-    @property
-    def is_irrealis(self) -> bool: return False
     @property
     def is_dictionary_verb_form_stem(self) -> bool: return False
     @property
@@ -59,9 +61,6 @@ class IAnalysisToken(Slots):
     @property
     def is_ichidan_imperative_inflection(self) -> bool: return False
     # </Only true for split tokens>
-
-    @property
-    def is_end_of_statement(self) -> bool: return self.source_token.is_end_of_statement
 
     @property
     def source_token(self) -> JNToken: raise NotImplementedError()
