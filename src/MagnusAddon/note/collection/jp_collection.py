a3711f7667642327e25b81e140f5341111b22765
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-01 10:11:30 +0100

    checkpoint

--- src/MagnusAddon/note/collection/jp_collection.py
+++ src/MagnusAddon/note/collection/jp_collection.py
@@ -7,8 +7,6 @@ from autoslot import Slots  # pyright: ignore[reportMissingTypeStubs]
 from note.collection.kanji_collection import KanjiCollection
 from note.collection.sentence_collection import SentenceCollection
 from note.collection.vocab_collection import VocabCollection
-from sysutils.memory_usage import string_auto_interner
-from sysutils.memory_usage.ex_trace_malloc import ex_trace_malloc_instance
 from sysutils.typed import non_optional
 from sysutils.weak_ref import WeakRefable
 
@@ -32,12 +30,6 @@ class JPCollection(WeakRefable, Slots):
         else:
             self._initialize()
 
-    def reshchedule_init_for(self, delay_seconds: float) -> None:
-        if self._initialization_started: return
-        if self._pending_init_timer is None: raise AssertionError("Pending init timer is None")
-        self._pending_init_timer.cancel()
-        self._pending_init_timer = threading.Timer(delay_seconds, self._initialize_wrapper)
-
     def _initialized_self(self) -> JPCollection:
         if not self._is_initialized:
             self._initialize()
@@ -50,18 +42,13 @@ class JPCollection(WeakRefable, Slots):
         if self._initialization_started:
             return
         self._initialization_started = True
-        string_auto_interner.try_enable()
         mylog.info("JPCollection.__init__")
         if self._pending_init_timer is not None:
             self._pending_init_timer.cancel()
-        ex_trace_malloc_instance.ensure_initialized()
 
         self._vocab = VocabCollection()
         self._sentences = SentenceCollection()
         self._kanji = KanjiCollection()
-
-        ex_trace_malloc_instance.log_memory_delta("Done loading add-on")
-        ex_trace_malloc_instance.stop()
 
         self._is_initialized = True
         JPCollection._is_inital_load = False
