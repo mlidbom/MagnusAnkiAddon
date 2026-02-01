2ae7161fc4fdf8d9dc1c4f33ae451026938247e8
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-01 15:35:48 +0100

    checkpoint

--- src/MagnusAddon/jaslib/task_runners/i_task_progress_runner.py
+++ src/MagnusAddon/jaslib/task_runners/i_task_progress_runner.py
@@ -10,10 +10,10 @@ class ITaskRunner:
     # noinspection PyUnusedFunction,Annotator
     def process_with_progress[TInput, TOutput](self, items: list[TInput], process_item: Callable[[TInput], TOutput], message: str, run_gc: bool = False, minimum_items_to_gc: int = 0) -> list[TOutput]: raise NotImplementedError()  # pyright: ignore[reportUnusedParameter]
     # noinspection Annotator
-    def set_label_text(self, text: str) -> None: raise NotImplementedError()  # pyright: ignore
+    def set_label_text(self, text: str) -> None: raise NotImplementedError()  # pyright: ignore[reportUnusedParameter]
     def close(self) -> None: raise NotImplementedError()
     # noinspection PyUnusedFunction, Annotator
-    def run_on_background_thread_with_spinning_progress_dialog[TResult](self, message: str, action: Callable[[], TResult]) -> TResult: raise NotImplementedError()  # pyright: ignore
+    def run_on_background_thread_with_spinning_progress_dialog[TResult](self, message: str, action: Callable[[], TResult]) -> TResult: raise NotImplementedError()  # pyright: ignore[reportUnusedParameter]
     def run_gc(self) -> None: pass
     # noinspection PyUnusedFunction
     def is_hidden(self) -> bool: return True
