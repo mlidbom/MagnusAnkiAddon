f99291cbb8f8bdf8c8ac8fb8126c6762e159ea88
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-03 19:36:21 +0100

    sonnet porting.

---
+++ src/src_dotnet/JAStudio.Core/Note/Vocabulary/VocabNoteFactory.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+namespace JAStudio.Core.Note.Vocabulary;
+
+public static class VocabNoteFactory
+{
+    public static VocabNote CreateWithDictionary(string question)
+    {
+        // TODO: Implement dictionary lookup when DictLookup is ported
+        // var lookupResult = DictLookup.LookupWord(question);
+        // if (!lookupResult.FoundWords())
+        // {
+        //     return Create(question, "", new List<string>());
+        // }
+        //
+        // var created = Create(question, lookupResult.FormatAnswer(), lookupResult.Readings());
+        // created.Tags.Set(Tags.Source.Jamdict);
+        // created.UpdateGeneratedData();
+        // return created;
+        
+        return Create(question, "", new List<string>());
+    }
+
+    public static VocabNote Create(string question, string answer, List<string> readings, Action<VocabNote>? initializer = null)
+    {
+        var note = new VocabNote();
+        note.Question.Set(question);
+        note.SetField(NoteFieldsConstants.Vocab.SourceAnswer, answer);
+        note.SetReadings(readings);
+        
+        if (initializer != null)
+        {
+            initializer(note);
+        }
+        
+        App.Col().Vocab.Add(note);
+        return note;
+    }
+
+    public static VocabNote CreateFromUserData(string question, string answer, List<string> readings, Action<VocabNote>? initializer = null)
+    {
+        var note = Create(question, answer, readings, initializer);
+        note.User.Answer.Set(note.GetField(NoteFieldsConstants.Vocab.SourceAnswer));
+        note.SetField(NoteFieldsConstants.Vocab.SourceAnswer, "");
+        
+        return note;
+    }
+}
