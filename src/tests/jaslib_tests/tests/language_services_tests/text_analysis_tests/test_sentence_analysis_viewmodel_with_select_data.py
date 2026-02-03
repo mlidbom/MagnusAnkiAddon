f1df64193999ae76df1c2602c0c301dd61e15569
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-03 15:58:11 +0100

    checkpoint

--- src/tests/jaslib_tests/tests/language_services_tests/text_analysis_tests/test_sentence_analysis_viewmodel_with_select_data.py
+++ src/tests/jaslib_tests/tests/language_services_tests/text_analysis_tests/test_sentence_analysis_viewmodel_with_select_data.py
@@ -6,7 +6,7 @@ import pytest
 from jaslib import app
 from jaslib.language_services.janome_ex.word_extraction.word_exclusion import WordExclusion
 from jaslib_tests.fixtures.collection_factory import inject_collection_with_select_data
-from jaslib_tests.tests.language_services_tests.janome_tests.test_sentence_analysis_viewmodel_common import assert_all_words_equal, assert_display_words_equal_and_that_analysis_internal_state_is_valid
+from jaslib_tests.tests.language_services_tests.text_analysis_tests.test_sentence_analysis_viewmodel_common import assert_all_words_equal, assert_display_words_equal_and_that_analysis_internal_state_is_valid
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
