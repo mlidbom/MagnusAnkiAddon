915a0df803e969fad5162cd686a0e51a409b3227
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-05 04:55:32 +0100

    checkpoint

--- src/tests/jaslib_tests/tests/language_services_tests/text_analysis_tests/test_sentence_analysis_viewmodel_common.py
+++ src/tests/jaslib_tests/tests/language_services_tests/text_analysis_tests/test_sentence_analysis_viewmodel_common.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from jaslib.note.sentences.sentence_configuration import SentenceConfiguration
 from jaslib.note.sentences.sentencenote import SentenceNote
 from jaslib.ui.web.sentence.sentence_viewmodel import SentenceViewModel
 
@@ -18,7 +17,7 @@ def surface_and_match_form(match_vm: MatchViewModel) -> str:
     emergency = ":emergency" if match_vm.match.is_emergency_displayed else ""
     return f"""{form_to_display}:{match_vm.vocab_form}{emergency}""" if match_vm.display_vocab_form else f"{form_to_display}{emergency}"
 
-def assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentence: str, excluded: list[WordExclusion], expected_output: list[str]) -> None:
+def assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentence: str, incorrect: list[WordExclusion], hidden: list[WordExclusion], expected_output: list[str]) -> None:
     def run_note_assertions(message: str) -> None:
         root_words = [surface_and_match_form(dm) for dm in sentence_view_model.displayed_matches]
         try:
@@ -30,19 +29,19 @@ def assert_display_words_equal_and_that_analysis_internal_state_is_valid(sentenc
             raise
 
     sentence_note = SentenceNote.create(sentence)
-    if len(excluded) != 0:
-        sentence_note.configuration._value = SentenceConfiguration.from_hidden_matches(excluded)  # pyright: ignore[reportPrivateUsage]
+    for exclusion in hidden:
+        sentence_note.configuration.hidden_matches.add(exclusion)
+    for exclusion in incorrect:
+        sentence_note.configuration.incorrect_matches.add(exclusion)
+
 
     sentence_view_model = SentenceViewModel(sentence_note)
 
 
-    if len(excluded) == 0:
+    if len(incorrect) == 0:
         run_note_assertions("running assertions with no exclusions")
     else:
-        run_note_assertions("running assertions with exclusions hidden")
-
-        sentence_note.configuration._value = SentenceConfiguration.from_incorrect_matches(excluded)  # pyright: ignore[reportPrivateUsage]
-        run_note_assertions("running assertions with exclusions marked as incorrect matches")
+        run_note_assertions("running assertions with exclusions")
 
 def assert_all_words_equal(sentence: str, expected_output: list[str]) -> None:
     sentence_note = SentenceNote.create(sentence)
