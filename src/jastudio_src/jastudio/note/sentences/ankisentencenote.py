b60714b73d1cada06c80e21c0f70e22fff6cfd95
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-07 20:49:32 +0100

    checkpoint

--- src/jastudio_src/jastudio/note/sentences/ankisentencenote.py
+++ src/jastudio_src/jastudio/note/sentences/ankisentencenote.py
@@ -3,25 +3,25 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from autoslot import Slots
-from jaslib.note.note_constants import ImmersionKitSentenceNoteFields
-from jaslib.note.sentences.sentencenote import SentenceNote
-from jaslib.note.tags import Tags
 from jastudio.note.ankijpnote import AnkiJPNote
-from typed_linq_collections.collections.q_set import QSet
 
 if TYPE_CHECKING:
     from anki.notes import Note
+    from JAStudio.Core.Note import SentenceNote
 
 class AnkiSentenceNote(AnkiJPNote, Slots):
     @classmethod
     def import_immersion_kit_sentence(cls, immersion_kit_note: Note) -> SentenceNote:
-        created = SentenceNote.add_sentence(question=immersion_kit_note[ImmersionKitSentenceNoteFields.question],
-                                            answer=immersion_kit_note[ImmersionKitSentenceNoteFields.answer],
-                                            audio=immersion_kit_note[ImmersionKitSentenceNoteFields.audio],
-                                            screenshot=immersion_kit_note[ImmersionKitSentenceNoteFields.screenshot],
-                                            tags=QSet([Tags.Source.immersion_kit]))
-
-        created.id.set(immersion_kit_note[ImmersionKitSentenceNoteFields.id])
-        created.reading.set(immersion_kit_note[ImmersionKitSentenceNoteFields.reading])
-
-        return created
+        raise NotImplementedError()
+        #todo migration
+        # created = SentenceNote.AddSentence(dotnet_ui_root.Services.NoteServices,
+        #                                    question=immersion_kit_note[ImmersionKitSentenceNoteFields.question],
+        #                                    answer=immersion_kit_note[ImmersionKitSentenceNoteFields.answer],
+        #                                    audio=immersion_kit_note[ImmersionKitSentenceNoteFields.audio],
+        #                                    screenshot=immersion_kit_note[ImmersionKitSentenceNoteFields.screenshot],
+        #                                    tags=QSet([Tags.Source.ImmersionKit]))
+        #
+        # created.id.set(immersion_kit_note[ImmersionKitSentenceNoteFields.id])
+        # created.reading.set(immersion_kit_note[ImmersionKitSentenceNoteFields.reading])
+        #
+        # return created
