4df97ad1a281de6b649871b236af2ef32e4d48c6
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-07 19:45:58 +0100

    checkpoint

--- src/jastudio_src/jastudio/ui/hooks/custom_auto_advance_timings.py
+++ src/jastudio_src/jastudio/ui/hooks/custom_auto_advance_timings.py
@@ -5,11 +5,8 @@ from typing import TYPE_CHECKING
 from aqt import gui_hooks, mw
 from aqt.reviewer import Reviewer
 from jaslib.note.difficulty_calculator import DifficultyCalculator
-from jaslib.note.kanjinote import KanjiNote
-from jaslib.note.note_constants import CardTypes
-from jaslib.note.sentences.sentencenote import SentenceNote
-from jaslib.note.vocabulary.vocabnote import VocabNote
 from jaspythonutils.sysutils.typed import non_optional
+from JAStudio.Core.Note import CardTypes, KanjiNote, SentenceNote, VocabNote
 
 from jastudio.anki_extentions.card_ex import CardEx
 from jastudio.ankiutils import app, ui_utils
@@ -20,7 +17,7 @@ if TYPE_CHECKING:
 _real_auto_advance_to_answer_if_enabled = Reviewer._auto_advance_to_answer_if_enabled  # noqa  # pyright: ignore[reportPrivateUsage]
 
 def is_handled_card(card: CardEx) -> bool:
-    if card.type().name != CardTypes.reading:
+    if card.type().name != CardTypes.Reading:
         return False
 
     import jastudio.note.ankijpnote
@@ -35,7 +32,7 @@ def seconds_to_show_question(card: CardEx) -> float:
             return DifficultyCalculator(starting_seconds=app.config().autoadvance_sentence_starting_seconds.get_value(),
                                         hiragana_seconds=app.config().autoadvance_sentence_hiragana_seconds.get_value(),
                                         katakata_seconds=app.config().autoadvance_sentence_katakana_seconds.get_value(),
-                                        kanji_seconds=app.config().autoadvance_sentence_kanji_seconds.get_value()).allowed_seconds(note.question.without_invisible_space())
+                                        kanji_seconds=app.config().autoadvance_sentence_kanji_seconds.get_value()).allowed_seconds(note.Question.WithoutInvisibleSpace())
         if isinstance(note, VocabNote):
             return DifficultyCalculator(starting_seconds=app.config().autoadvance_vocab_starting_seconds.get_value(),
                                         hiragana_seconds=app.config().autoadvance_vocab_hiragana_seconds.get_value(),
