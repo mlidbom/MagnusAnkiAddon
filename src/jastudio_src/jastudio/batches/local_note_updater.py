207ddbe7497dcd1e2463f08cab17539aadc72ec0
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-07 18:34:31 +0100

    checkpoint

--- src/jastudio_src/jastudio/batches/local_note_updater.py
+++ src/jastudio_src/jastudio/batches/local_note_updater.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import gc
 from typing import TYPE_CHECKING
 
 from jaslib.task_runners.task_progress_runner import TaskRunner
@@ -21,13 +20,3 @@ def convert_immersion_kit_sentences() -> None:
     with TaskRunner.current("Converting immersion kit sentences", inhibit_gc=True) as runner:
         immersion_kit_sences = list(app.anki_collection().find_notes(query_builder.immersion_kit_sentences()))
         runner.process_with_progress(immersion_kit_sences, convert_note, "Converting immersion kit sentences", run_gc=True, minimum_items_to_gc=100)
-
-def print_gc_status_and_collect() -> None:
-    from jastudio.sysutils import object_instance_tracker
-    object_instance_tracker.print_instance_counts()
-
-    app.get_ui_utils().tool_tip(f"Gc.isenabled(): {gc.isenabled()}, Collecting ...", 10000)
-
-    instances = gc.collect()
-    app.get_ui_utils().tool_tip(f"collected: {instances} instances", 10000)
-    object_instance_tracker.print_instance_counts()
