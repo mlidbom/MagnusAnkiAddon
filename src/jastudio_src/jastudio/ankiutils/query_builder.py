f2d49cd07ec0c3c880f3176de1a178ce7ba8c597
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-07 19:47:04 +0100

    checkpoint

--- src/jastudio_src/jastudio/ankiutils/query_builder.py
+++ src/jastudio_src/jastudio/ankiutils/query_builder.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from jaslib.note.note_constants import Builtin, NoteTypes
+from JAStudio.Core.Note import Builtin, NoteTypes
 
 if TYPE_CHECKING:
     from anki.cards import CardId
@@ -13,7 +13,7 @@ note_vocab = f"{Builtin.Note}:{NoteTypes.Vocab}"
 note_vocab = note_vocab
 
 def immersion_kit_sentences() -> str:
-    return f'''"{Builtin.Note}:{NoteTypes.immersion_kit}"'''
+    return f'''"{Builtin.Note}:{NoteTypes.ImmersionKit}"'''
 
 def open_card_by_id(card_id: CardId) -> str:
     return f"cid:{card_id}"
