7e6ea8273f657634ac50e5d0134ab004b41a6737
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-10 17:16:12 +0100

    Fix bug where UI setting influenced persistent parsing result.

--- src/src_dotnet/JAStudio.Core/LanguageServices/JanomeEx/WordExtraction/Matches/StateTests/Tail/ForbidsHasDisplayedOverlappingFollowingCompound.cs
+++ src/src_dotnet/JAStudio.Core/LanguageServices/JanomeEx/WordExtraction/Matches/StateTests/Tail/ForbidsHasDisplayedOverlappingFollowingCompound.cs
@@ -4,51 +4,74 @@ namespace JAStudio.Core.LanguageServices.JanomeEx.WordExtraction.Matches.StateTe
 
 public sealed class ForbidsHasDisplayedOverlappingFollowingCompound : CustomForbidsNoCache
 {
-    private ForbidsHasDisplayedOverlappingFollowingCompound(VocabMatchInspector inspector) : base(inspector)
-    {
-    }
+   ForbidsHasDisplayedOverlappingFollowingCompound(VocabMatchInspector inspector) : base(inspector) {}
 
-    public static ForbidsHasDisplayedOverlappingFollowingCompound? ApplyTo(VocabMatchInspector inspector)
-    {
-        return inspector.RequiresForbids.YieldLastToken.IsRequired && !inspector.Match.IsHighlighted
-            ? new ForbidsHasDisplayedOverlappingFollowingCompound(inspector)
-            : null;
-    }
+   public static ForbidsHasDisplayedOverlappingFollowingCompound? ApplyTo(VocabMatchInspector inspector)
+   {
+      if(inspector.Match.IsHighlighted) return null;
 
-    public override string Description => "has_displayed_following_overlapping_compound";
+      if(ShouldYield(inspector))
+         return new ForbidsHasDisplayedOverlappingFollowingCompound(inspector);
 
-    protected override bool InternalIsInState()
-    {
-        // todo: this is a problematic reference to display_words. That collection is initialized using this class,
-        // so this class will return different results depending on whether it is used after or before display_words is first initialized. Ouch
+      return null;
+   }
 
-        var endLocation = Inspector.EndLocation;
-        var tailLocation = endLocation;
-        // The dictionary verb inflection is a special case that is shown as a separate token after the word in which it is part, so it does not by itself cover/shadow anything
-        // thus, we skip such tokens as possible locations for following compounds to overlap, since that words starting with that token are shown after this compound anyway, and are not hidden by this compound.
-        // this logic is mirrored in is_shadowed logic
-        if (endLocation.Token.IsDictionaryVerbInflection)
-        {
-            if (endLocation.Previous == null)
+   static bool ShouldYield(VocabMatchInspector inspector)
+   {
+      var yieldLastToken = inspector.RequiresForbids.YieldLastToken;
+      if(yieldLastToken.IsRequired) return true;
+
+      if(!inspector.Word.Analysis.ForUI || yieldLastToken.IsForbidden) 
+         return false;
+
+      var pos = inspector.Match.Vocab.PartsOfSpeech;
+      var settings = inspector.Settings;
+
+      if(settings.AutomaticallyYieldLastTokenInSuruVerbCompoundsToOverlappingCompound() && pos.IsSuruVerbIncluded() && !pos.IsNiSuruGaSuruKuSuruCompound())
+         return true;
+      if(settings.AutomaticallyYieldLastTokenInPassiveVerbCompoundsToOverlappingCompound() && pos.IsPassiveVerbCompound())
+         return true;
+      if(settings.AutomaticallyYieldLastTokenInCausativeVerbCompoundsToOverlappingCompound() && pos.IsCausativeVerbCompound())
+         return true;
+
+      return false;
+   }
+
+   public override string Description => "has_displayed_following_overlapping_compound";
+
+   protected override bool InternalIsInState()
+   {
+      // todo: this is a problematic reference to display_words. That collection is initialized using this class,
+      // so this class will return different results depending on whether it is used after or before display_words is first initialized. Ouch
+
+      var endLocation = Inspector.EndLocation;
+      var tailLocation = endLocation;
+      // The dictionary verb inflection is a special case that is shown as a separate token after the word in which it is part, so it does not by itself cover/shadow anything
+      // thus, we skip such tokens as possible locations for following compounds to overlap, since that words starting with that token are shown after this compound anyway, and are not hidden by this compound.
+      // this logic is mirrored in is_shadowed logic
+      if(endLocation.Token.IsDictionaryVerbInflection)
+      {
+         if(endLocation.Previous == null)
+         {
+            return false;
+         }
+
+         tailLocation = endLocation.Previous;
+      }
+
+      while(tailLocation != Inspector.Word.StartLocation)
+      {
+         foreach(var displayWord in tailLocation.DisplayWords)
+         {
+            if(displayWord.EndLocation.TokenIndex > endLocation.TokenIndex)
             {
-                return false;
+               return true;
             }
-            tailLocation = endLocation.Previous;
-        }
+         }
 
-        while (tailLocation != Inspector.Word.StartLocation)
-        {
-            foreach (var displayWord in tailLocation.DisplayWords)
-            {
-                if (displayWord.EndLocation.TokenIndex > endLocation.TokenIndex)
-                {
-                    return true;
-                }
-            }
+         tailLocation = tailLocation.Previous!;
+      }
 
-            tailLocation = tailLocation.Previous!;
-        }
-
-        return false;
-    }
+      return false;
+   }
 }
--- src/src_dotnet/JAStudio.Core/Note/Vocabulary/YieldLastTokenToOverlappingCompound.cs
+++ src/src_dotnet/JAStudio.Core/Note/Vocabulary/YieldLastTokenToOverlappingCompound.cs
@@ -4,7 +4,6 @@ namespace JAStudio.Core.Note.Vocabulary;
 
 public class YieldLastTokenToOverlappingCompound : RequireForbidFlagField
 {
-    private readonly VocabNote _vocab;
     private readonly VocabNotePartsOfSpeech _pos;
 
     public YieldLastTokenToOverlappingCompound(VocabNote vocab)
@@ -12,28 +11,13 @@ public class YieldLastTokenToOverlappingCompound : RequireForbidFlagField
             Tags.Vocab.Matching.YieldLastTokenToOverlappingCompound, 
             Tags.Vocab.Matching.Forbids.AutoYielding)
     {
-        _vocab = vocab;
         _pos = vocab.PartsOfSpeech;
     }
 
-    private bool DecideIfRequired()
+    bool DecideIfRequired()
     {
         return base.IsRequired
-            || (!IsForbidden
-                && (
-                    // na adjectives
-                    _pos.IsCompleteNaAdjective()
-                    // suru verb compounds
-                    || (_vocab.Services.Settings.AutomaticallyYieldLastTokenInSuruVerbCompoundsToOverlappingCompound()
-                        && _pos.IsSuruVerbIncluded()
-                        && !_pos.IsNiSuruGaSuruKuSuruCompound())
-                    // passive verb compounds
-                    || (_vocab.Services.Settings.AutomaticallyYieldLastTokenInPassiveVerbCompoundsToOverlappingCompound()
-                        && _pos.IsPassiveVerbCompound())
-                    // causative verb compounds
-                    || (_vocab.Services.Settings.AutomaticallyYieldLastTokenInCausativeVerbCompoundsToOverlappingCompound()
-                        && _pos.IsCausativeVerbCompound())
-                ));
+            || (!IsForbidden && _pos.IsCompleteNaAdjective());
     }
 
     public override bool IsRequired => DecideIfRequired();
