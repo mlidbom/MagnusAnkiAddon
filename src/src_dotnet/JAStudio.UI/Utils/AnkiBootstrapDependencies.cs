da4c272c1d0505c1acb4d35ce7171d504f3d3a23
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-17 21:44:49 +0100

    checkpoint

--- src/src_dotnet/JAStudio.UI/Utils/AnkiBootstrapDependencies.cs
+++ src/src_dotnet/JAStudio.UI/Utils/AnkiBootstrapDependencies.cs
@@ -11,19 +11,14 @@ namespace JAStudio.UI.Utils;
 /// <summary>
 /// Production dependencies for running inside Anki with Avalonia UI.
 /// </summary>
-class AnkiBootstrapDependencies : IBootstrapDependencies
+class AnkiBootstrapDependencies(string configJson, Action<string> configUpdateCallback) : IBootstrapDependencies
 {
-   public AnkiBootstrapDependencies(string configJson, Action<string> configUpdateCallback)
-   {
-      ConfigDictSource = new AnkiConfigDictSource(configJson, configUpdateCallback);
-   }
-
    public IEnvironmentPaths EnvironmentPaths { get; } = new AnkiEnvironmentPaths();
    public IBackendNoteCreator BackendNoteCreator { get; } = new AnkiBackendNoteCreator();
    public IBackendDataLoader BackendDataLoader { get; } = new AnkiBackendDataLoader();
    public IFatalErrorHandler FatalErrorHandler { get; } = new AvaloniaFatalErrorHandler();
    public ITaskProgressUI TaskProgressUI { get; } = new AvaloniaTaskProgressUI();
-   public IConfigDictSource ConfigDictSource { get; }
+   public IConfigDictSource ConfigDictSource { get; } = new AnkiConfigDictSource(configJson, configUpdateCallback);
    public IReadingsMappingsSource? ReadingsMappingsSource => null;
    public Func<ExternalNoteIdMap, ICardOperations> CardOperationsFactory { get; } = idMap => new AnkiCardOperationsImpl(idMap);
 }
