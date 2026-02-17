ac454edc6f2e329ca1c891977cbe6391fc947bcf
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-17 05:32:35 +0100

    checkpoint

--- src/src_dotnet/JAStudio.UI/AppDialogs.cs
+++ src/src_dotnet/JAStudio.UI/AppDialogs.cs
@@ -6,6 +6,8 @@ using JAStudio.UI.Views;
 
 namespace JAStudio.UI;
 
+// ReSharper disable MemberCanBeInternal used from python
+// ReSharper disable UnusedMember.Global used from python
 public class AnkiDialogs
 {
    readonly CoreApp _coreApp;
@@ -13,26 +15,18 @@ public class AnkiDialogs
 
    internal AnkiDialogs(CoreApp coreApp) => _coreApp = coreApp;
 
-   // ReSharper disable once MemberCanBeInternal used from python
-   // ReSharper disable once UnusedMember.Global used from python
    internal void ShowReadingsMappingsDialog()
    {
       this.Log().Info("ShowReadingsMappingsDialog() called");
       Dispatcher.UIThread.Invoke(() => new ReadingsMappingsDialog(Services).ShowNearCursor());
    }
 
-   // ReSharper disable once MemberCanBeInternal used from python
-   // ReSharper disable once UnusedMember.Global used from python
    public void ToggleNoteSearchDialog()
    {
       this.Log().Info("ToggleNoteSearchDialog() called");
-      Dispatcher.UIThread.Invoke(() =>
-      {
-         NoteSearchDialog.ToggleVisibility(Services);
-      });
+      Dispatcher.UIThread.Invoke(() => NoteSearchDialog.ToggleVisibility(Services));
    }
 
-   // ReSharper disable once UnusedMember.Global - Called from Python (global_shortcuts.py)
    public void ToggleEnglishWordSearchDialog()
    {
       this.Log().Info("ToggleEnglishWordSearchDialog() called");
