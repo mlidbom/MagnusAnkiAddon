4fcaec9e5d9abba71294a79997fcd052401ba29b
Magnus Lidbom <magnus.lidbom@gmail.com>
2026-02-18 23:44:54 +0100

    Make our iframe take up 100% of the window size.

--- src/src_dotnet/JAStudio.Core/UI/Web/Kanji/KanjiNoteRenderer.cs
+++ src/src_dotnet/JAStudio.Core/UI/Web/Kanji/KanjiNoteRenderer.cs
@@ -19,6 +19,6 @@ public class KanjiNoteRenderer
       var baseUrl = CardServerUrl.BaseUrl;
       if(baseUrl == null) return "<!-- CardServer not running -->";
       var externalId = note.Collection.GetExternalNoteId(note.GetId());
-      return $"""<iframe src="{baseUrl}/card/kanji/{side}?NoteId={externalId}" style="width:100%;height:100%;{extraStyle}" frameborder="0"></iframe>""";
+      return $"""<iframe src="{baseUrl}/card/kanji/{side}?NoteId={externalId}" style="position:fixed;inset:0;width:100%;height:100%;border:none;{extraStyle}" frameborder="0"></iframe>""";
    }
 }
